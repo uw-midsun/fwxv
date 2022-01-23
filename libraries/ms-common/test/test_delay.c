c411f678acb10889a6f90c68ad592f3a00895b81
Bafran <aachinoy@gmail.com>
2022-01-23 16:29:05 -0500

    Fix Delay Test

--- libraries/ms-common/test/test_delay.c
+++ libraries/ms-common/test/test_delay.c
@@ -9,7 +9,26 @@ void setup_test(void) {}
 
 void teardown_test(void) {}
 
+static StackType_t s_task_stack;
+static StaticTask_t s_task_tcb;
+
+static void delay_task() {
+  LOG_DEBUG("Begin Delay\n");
+  delay_us(10000);
+  LOG_DEBUG("End Delay\n");
+  vTaskEndScheduler();
+}
+
 void test_delay_us(void) {
-  LOG_DEBUG("This is a message\n");
-  delay_us(1000);
+  xTaskCreateStatic(
+        delay_task,
+        "Delay",
+        configMINIMAL_STACK_SIZE,
+        NULL,
+        tskIDLE_PRIORITY + 1,
+        &s_task_stack,
+        &s_task_tcb
+    );
+  
+  vTaskStartScheduler();
 }
