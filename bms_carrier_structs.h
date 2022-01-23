193a38967919094567c4e4843ddd5f7b7e252f87
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-23 18:33:32 +0000

    Add jinja template for bms_carrier_getters.h

--- bms_carrier_structs.h
+++ bms_carrier_structs.h
@@ -4,19 +4,19 @@ typedef struct {
     uint8_t signal2;
     uint16_t signal3;
     uint32_t signal4;
-    uint8_t signal1;
-    uint8_t signal1;
-    uint8_t signal1;
-    uint8_t signal1;
-    uint16_t signal1;
-    uint8_t signal1;
-    uint64_t signal1;
-    uint8_t signal1;
-    uint8_t signal1;
-    uint16_t signal1;
-    uint8_t signal1;
-    uint32_t signal1;
-    uint8_t signal1;
+    uint8_t signal5;
+    uint8_t signal6;
+    uint8_t signal7;
+    uint8_t signal8;
+    uint16_t signal9;
+    uint8_t signal10;
+    uint64_t signal11;
+    uint8_t signal12;
+    uint8_t signal13;
+    uint16_t signal14;
+    uint8_t signal15;
+    uint32_t signal16;
+    uint8_t signal17;
 } receive_struct;
 
 
