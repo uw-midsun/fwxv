7b75dcaf0fab23e8239d042192089d037e2dc4b8
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-24 00:06:10 +0000

    Add function get_file_name to generator.py

--- libraries/codegen/generator.py
+++ libraries/codegen/generator.py
@@ -6,19 +6,20 @@ def read_yaml(yaml_file):
     with open(yaml_file, "r") as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
         return data
-    
-def write_template(env, template_name, yaml_path, data, dest="./"):
+
+def get_file_name(template_name, yaml_path, data):
     # get the name of the yaml file from the filepath
     file_prefix = yaml_path.split("/")[len(yaml_path.split("/"))-1].split(".")[0]
     data['FilePrefix'] = file_prefix
     # get the name of the jinja file from the filepath
-    jinja_prefix = options.template[:-6]
+    jinja_prefix = template_name[:-6]
     # files that start with _ are generic and we want to append the specific name in front
-    file = dest + (file_prefix + jinja_prefix if jinja_prefix[0] == "_" else jinja_prefix)
-
+    return file_prefix + jinja_prefix if jinja_prefix[0] == "_" else jinja_prefix
+    
+def write_template(env, template_name, yaml_path, data, dest="./"):
+    file = dest + get_file_name(template_name, yaml_path, data)
     template = env.get_template(template_name)
     output = template.render(data=data)
-
     with open(file, "w") as f:
         f.write(output)
 
