af35ccb40219fc71efa762d35bdea71cef5f0d6b
Daniel Qu <67433232+danielq987@users.noreply.github.com>
2022-02-09 21:37:51 -0500

    Format all files (#14)
    Global `scons format`.

--- projects/example/test/test_func.c
+++ projects/example/test/test_func.c
@@ -6,6 +6,6 @@ void setup_test(void) {}
 void teardown_test(void) {}
 
 void test_func(void) {
-    int ret = get5();
-    TEST_ASSERT_EQUAL(5, ret);
+  int ret = get5();
+  TEST_ASSERT_EQUAL(5, ret);
 }
