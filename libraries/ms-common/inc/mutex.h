8a1fe04c4a14e17e7e3f9c7ccf0d1eaffd784e50
Mitchell Ostler <mlostler@uwaterloo.ca>
2022-02-26 11:49:31 -0500

    Remove ISR behaviour, cleanup functions

--- libraries/ms-common/inc/mutex.h
+++ libraries/ms-common/inc/mutex.h
@@ -8,11 +8,6 @@
 
 #define BLOCK_INDEFINITELY UINT16_MAX
 
-typedef enum MutexCaller {
-  MUTEX_CALLER_TASK = 0,
-  MUTEX_CALLER_ISR,
-} MutexCaller;
-
 // Mutex handle is used to access Mutex
 // Mutex Objects must be declared statically 
 typedef struct Mutex {
@@ -33,5 +28,5 @@ StatusCode mutex_lock(Mutex *mutex, uint16_t ms_to_wait);
 // Unlocks a Mutex. The task which locks the mutex MUST also be the one to unlock it
 // Caller specifies whether the method is being called from a task or interrupt handler
 // higher_priority_task_woken informs the caller whether a higher priority task has become unblocked
-// Returns STATUS_CODE_OK on success, STATUS_CODE_INTERNTAL_ERROR on failure
-StatusCode mutex_unlock(Mutex *mutex, MutexCaller caller, bool *higher_priority_task_woken);
+// Returns STATUS_CODE_OK on success, STATUS_CODE_INTERNAL_ERROR on failure
+StatusCode mutex_unlock(Mutex *mutex);
--- libraries/ms-common/src/mutex.c
+++ libraries/ms-common/src/mutex.c
@@ -11,6 +11,9 @@ StatusCode mutex_init(Mutex *mutex) {
 
 StatusCode mutex_lock(Mutex *mutex, uint16_t ms_to_wait) {
   TickType_t ticks_to_wait;
+  if (mutex == NULL) {
+      return STATUS_CODE_INVALID_ARGS;
+  }
   if(ms_to_wait == BLOCK_INDEFINITELY) {
     ticks_to_wait = portMAX_DELAY;
   } else {
@@ -22,20 +25,12 @@ StatusCode mutex_lock(Mutex *mutex, uint16_t ms_to_wait) {
   return STATUS_CODE_OK;
 }
 
-StatusCode mutex_unlock(Mutex *mutex, MutexCaller caller, bool *higher_priority_task_woken) {
-  BaseType_t result, higher_priority_woken = pdFALSE;
-  if (higher_priority_task_woken == NULL) {
+StatusCode mutex_unlock(Mutex *mutex) {
+  if (mutex == NULL) {
       return STATUS_CODE_INVALID_ARGS;
   }
-  if(caller == MUTEX_CALLER_ISR) {
-    // Use ISR specific method
-    result = xSemaphoreGiveFromISR(mutex->handle, &higher_priority_woken);
-    *higher_priority_task_woken = higher_priority_woken;
-  } else {
-    *higher_priority_task_woken = false;
-    result = xSemaphoreGive(mutex->handle);
-  }
-  if (result == pdFALSE) {
+
+  if (xSemaphoreGive(mutex->handle) == pdFALSE) {
     return STATUS_CODE_INTERNAL_ERROR;
   }
   return STATUS_CODE_OK;
