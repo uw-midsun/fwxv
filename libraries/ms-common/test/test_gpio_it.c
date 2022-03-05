a6adf667217105ff44756d03713cd7afeff03bba
ShiCheng-Lu <shichenglu0708@gmail.com>
2022-03-05 19:06:11 +0000

    test2

--- libraries/ms-common/test/test_gpio_it.c
+++ libraries/ms-common/test/test_gpio_it.c
@@ -11,6 +11,15 @@ void setup_test(void) {}
 
 void teardown_test(void) {}
 
+
+
+
+
+
+
+
+
+
 static StackType_t s_task_stack;
 static StaticTask_t s_task_tcb;
 static StackType_t s_task_stack_2;
@@ -23,22 +32,21 @@ static void interrupt_handler() {
   uint32_t value;
   while (true) {
     xTaskNotifyWait(0, UINT32_MAX, &value, portMAX_DELAY);
-
     triggered = true;
   }
 }
 
 static GpioAddress addr = { .port = GPIO_PORT_A, .pin = 5 };
+static GpioAddress addr2 = { .port = GPIO_PORT_A, .pin = 5 };
 static InterruptSettings settings = {
   .priority = INTERRUPT_PRIORITY_NORMAL,
   .type = INTERRUPT_TYPE_INTERRUPT,
 };
 
-static void prv_test_trigger() {
+static void prv_test_trigger(void) {
   for (int i = 0; i < 5; ++i) {
-    delay_ms(500);
+    delay_ms(50);
     gpio_it_trigger_interrupt(&addr);
-
     TEST_ASSERT(triggered);
 
     triggered = false;
@@ -59,3 +67,36 @@ void test_gpio_it() {
 
   vTaskStartScheduler();
 }
+
+//
+// TEST 2
+//
+
+void prv_test_2() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+void test_gpio_it_0() {
+  TaskHandle_t handler = xTaskCreateStatic(interrupt_handler, "RECEIVE", configMINIMAL_STACK_SIZE, NULL,
+                              tskIDLE_PRIORITY + 1, &s_task_stack, &s_task_tcb);
+  LOG_DEBUG("%p\n", handler);
+  TaskHandle_t send_handle =
+      xTaskCreateStatic(prv_test_trigger, "SEND", configMINIMAL_STACK_SIZE, NULL,
+                        tskIDLE_PRIORITY + 1, &s_task_stack_2, &s_task_tcb_2);
+  interrupt_init();
+  gpio_it_init();
+  gpio_it_register_interrupt(&addr, &settings, INTERRUPT_EDGE_RISING_FALLING, handler, 0);
+
+  vTaskStartScheduler();
+}
+
