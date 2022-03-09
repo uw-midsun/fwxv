8a51546b47e96b7180c157b860dbacac70c9fcb8
Mitchell Ostler <ostlermitchell44@gmail.com>
2022-03-08 21:12:33 -0500

    Update queues.h

--- libraries/ms-common/inc/queues.h
+++ libraries/ms-common/inc/queues.h
@@ -6,12 +6,13 @@
 
 #define QUEUE_DELAY_BLOCKING portMAX_DELAY
 
+// Queue storage and access struct. Must be declared statically
 typedef struct {
-    uint32_t size;
-    uint32_t item_size;
-    uint8_t *storage_buf;
-    StaticQueue_t queue;
-    QueueHandle_t handle;
+    uint32_t num_items;   // Number of items the queue can hold
+    uint32_t item_size;   // Size of each item
+    uint8_t *storage_buf; // Must be declared statically, and have size num_items*item_size
+    StaticQueue_t queue;  // Internal Queue storage
+    QueueHandle_t handle; // Handle used for all queue operations
 } Queue;
 
 // Create a queue with the parameters specified in settings. Returns STATUS_CODE_OK if successful, STATUS_CODE_INVALID_ARGS otherwise.
