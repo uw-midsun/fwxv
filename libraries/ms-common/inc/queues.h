c86e2106cdc154100aa1e3596613e7c4e8fe1992
Hewitt McGaughey <hewittmcgaughey@gmail.com>
2022-03-12 20:59:07 +0000

    [SOFT-608] format queues.h/queues.c

--- libraries/ms-common/inc/queues.h
+++ libraries/ms-common/inc/queues.h
@@ -8,14 +8,15 @@
 
 // Queue storage and access struct. Must be declared statically
 typedef struct {
-    uint32_t num_items;   // Number of items the queue can hold
-    uint32_t item_size;   // Size of each item
-    uint8_t *storage_buf; // Must be declared statically, and have size num_items*item_size
-    StaticQueue_t queue;  // Internal Queue storage
-    QueueHandle_t handle; // Handle used for all queue operations
+  uint32_t num_items;    // Number of items the queue can hold
+  uint32_t item_size;    // Size of each item
+  uint8_t *storage_buf;  // Must be declared statically, and have size num_items*item_size
+  StaticQueue_t queue;   // Internal Queue storage
+  QueueHandle_t handle;  // Handle used for all queue operations
 } Queue;
 
-// Create a queue with the parameters specified in settings. Returns STATUS_CODE_OK if successful, STATUS_CODE_INVALID_ARGS otherwise.
+// Create a queue with the parameters specified in settings. Returns STATUS_CODE_OK if successful,
+// STATUS_CODE_INVALID_ARGS otherwise.
 StatusCode queue_init(Queue *queue);
 
 // Attempt to place an item into the queue, delaying for delay_ms in ms before timing out.
@@ -24,7 +25,8 @@ StatusCode queue_send(Queue *queue, const void *item, uint32_t delay_ms);
 // Attempt to receive an item from the queue, delaying for delay_ms in ms before timing out.
 StatusCode queue_receive(Queue *queue, void *buf, uint32_t delay_ms);
 
-// Attempt to receive an item from the queue without removing it from the queue, delaying for delay_ms in ms before timing out.
+// Attempt to receive an item from the queue without removing it from the queue, delaying for
+// delay_ms in ms before timing out.
 StatusCode queue_peek(Queue *queue, void *buf, uint32_t delay_ms);
 
 // Return number of items the queue can hold.
--- libraries/ms-common/src/queues.c
+++ libraries/ms-common/src/queues.c
@@ -2,45 +2,46 @@
 #include <string.h>
 
 StatusCode queue_init(Queue *queue) {
-    queue->handle = xQueueCreateStatic(queue->num_items, queue->item_size, queue->storage_buf, &queue->queue);
+  queue->handle =
+      xQueueCreateStatic(queue->num_items, queue->item_size, queue->storage_buf, &queue->queue);
 
-    if(queue->handle == NULL) {
-        return STATUS_CODE_INVALID_ARGS;
-    }
+  if (queue->handle == NULL) {
+    return STATUS_CODE_INVALID_ARGS;
+  }
 
-    return STATUS_CODE_OK;
+  return STATUS_CODE_OK;
 }
 
 StatusCode queue_send(Queue *queue, const void *item, uint32_t delay_ms) {
-    BaseType_t ret = xQueueSend(queue->handle, item, pdMS_TO_TICKS(delay_ms));
+  BaseType_t ret = xQueueSend(queue->handle, item, pdMS_TO_TICKS(delay_ms));
 
-    if(ret == errQUEUE_FULL) {
-        return STATUS_CODE_RESOURCE_EXHAUSTED;
-    }
+  if (ret == errQUEUE_FULL) {
+    return STATUS_CODE_RESOURCE_EXHAUSTED;
+  }
 
-    return STATUS_CODE_OK;
+  return STATUS_CODE_OK;
 }
 
 StatusCode queue_receive(Queue *queue, void *buf, uint32_t delay_ms) {
-    BaseType_t ret = xQueueReceive(queue->handle, buf, pdMS_TO_TICKS(delay_ms));
+  BaseType_t ret = xQueueReceive(queue->handle, buf, pdMS_TO_TICKS(delay_ms));
 
-    if(ret == pdFALSE) {
-        return STATUS_CODE_EMPTY;
-    }
+  if (ret == pdFALSE) {
+    return STATUS_CODE_EMPTY;
+  }
 
-    return STATUS_CODE_OK;
+  return STATUS_CODE_OK;
 }
 
 StatusCode queue_peek(Queue *queue, void *buf, uint32_t delay_ms) {
-    BaseType_t ret = xQueuePeek(queue->handle, buf, pdMS_TO_TICKS(delay_ms));
+  BaseType_t ret = xQueuePeek(queue->handle, buf, pdMS_TO_TICKS(delay_ms));
 
-    if(ret == pdFALSE) {
-        return STATUS_CODE_EMPTY;
-    }
+  if (ret == pdFALSE) {
+    return STATUS_CODE_EMPTY;
+  }
 
-    return STATUS_CODE_OK;
+  return STATUS_CODE_OK;
 }
 
 uint32_t queue_get_num_items(Queue *queue) {
-    return queue->num_items;
+  return queue->num_items;
 }
