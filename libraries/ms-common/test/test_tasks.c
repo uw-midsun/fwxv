54c853ac5ba2fb88129a9224e0eae0928715e148
ShiCheng-Lu <shichenglu0708@gmail.com>
2022-03-12 23:56:06 +0000

    added test for soft_timer

--- libraries/ms-common/test/test_tasks.c
+++ libraries/ms-common/test/test_tasks.c
@@ -6,6 +6,13 @@
 #include "tasks.h"
 #include "unity.h"
 
+enum test {
+  T1,
+  MAX,
+};
+
+int tests[MAX];
+
 // These are tests for tasks.h, but also an example of using the task_test_helpers.h library.
 
 static bool s_task_started;
@@ -27,6 +34,23 @@ TASK(my_task, TASK_STACK_512) {
 }
 
 TASK_TEST(test_running_task, TASK_STACK_512) {
+  LOG_DEBUG("started first test\n");
+  // Start the task: note no need to call tasks_start() because we're inside a test task and
+  // FreeRTOS is already running.
+  tasks_init_task(my_task, TASK_PRIORITY(1), NULL);
+
+  // The task doesn't start immediately because the test task has the highest priority.
+  TEST_ASSERT_FALSE(s_task_started);
+
+  // To let it run, use a delay.
+  delay_ms(20);
+
+  // The task should have run.
+  TEST_ASSERT_TRUE(s_task_started);
+}
+
+TASK_TEST(test_running_task_2, TASK_STACK_512) {
+  LOG_DEBUG("started second test\n");
   // Start the task: note no need to call tasks_start() because we're inside a test task and
   // FreeRTOS is already running.
   tasks_init_task(my_task, TASK_PRIORITY(1), NULL);
