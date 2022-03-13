4d7bdf49a13035cd10ce8c0c6296824af51a896c
ShiCheng-Lu <shichenglu0708@gmail.com>
2022-03-13 18:07:19 +0000

    better definition

--- libraries/ms-common/inc/watchdog.h
+++ libraries/ms-common/inc/watchdog.h
@@ -4,13 +4,12 @@
 #include "status.h"
 
 // declare a watchdog, watchdog is refered to by name
-#define DECLARE_WATCH_DOG(name) DECLARE_SOFT_TIMER(_watchdog_##name)
+typedef SoftTimer WatchDog;
 
 // start watchdog
-#define watchdog_start(name, timeout_ms, callback) \
-  soft_timer_start(timeout_ms, callback, _watchdog_##name)
+#define watchdog_start(name, timeout_ms, callback) soft_timer_start(timeout_ms, callback, name)
 
 // :)
-#define watchdog_kick(name) soft_timer_reset(_watchdog_##name)
+#define watchdog_kick(name) soft_timer_reset(name)
 
-#define watchdog_cancel(name) soft_timer_cancel(_watchdog_##name)
+#define watchdog_cancel(name) soft_timer_cancel(name)
