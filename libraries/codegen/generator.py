d80e4e69d15a64d869b2944a255828072119740c
Logan Yu <loganyu439@gmail.com>
2022-03-15 23:25:16 -0400

    Add new jinja filters in generator.py to format _pack.h.jinja

--- libraries/codegen/generator.py
+++ libraries/codegen/generator.py
@@ -9,56 +9,36 @@ def read_yaml(yaml_file):
         data = yaml.load(f, Loader=yaml.FullLoader)
         return data
 
-def format_pack_h(env, template_name, file_path, data):
-    # get the template as a string and split data into list
-    template = env.get_template(template_name)
-    raw_data = template.render(data=data).split()
+def format_function_parameters(template):
+    # Custom jinja filter that takes in the function parameter string
+    # and format it to indent to a new line whenever string length 
+    # reaches 100 characters.
+
+    template = template.split()
     letter_count = 0
-    
-    # traverse through the list to format the code to a correct header file format
-    for i in range(len(raw_data)):
-        letter_count += len(raw_data[i])
+    for i in range(len(template)):
+        letter_count += len(template[i]) + 1
         if letter_count >= 100:
-            letter_count = 0
-            raw_data[i] = ("\n"+(" "*(spacing_legnth + 1)) + raw_data[i])
-            letter_count += len(raw_data[i])
+            template[i] = "\n" + (" "*(len(template[0]) + len(template[1]) - 9)) + template[i]
+            letter_count = len(template[i])
+    template = " ".join(template)
+    return (template)
 
-        # checking for include statements
-        if raw_data[i].startswith("#"):
-            if raw_data[i] == "#pragma":
-                raw_data[i + 1] += "\n\n"
-                letter_count = 0
-            elif raw_data[i] == "#include":
-                raw_data[i + 1] += "\n"
-                letter_count = 0
-
-        # checking for the start of a function definition
-        if raw_data[i] == "void" and raw_data[i + 1].startswith("can_pack"):
-            letter_count = 1
-            spacing_legnth = len(raw_data[i] + raw_data[i + 1]) - 9
-            raw_data[i] = ("\n" + raw_data[i])
-            letter_count += len(raw_data[i])
-
-        # checking for when spacing would be needed/independent words to account for the space
-        elif raw_data[i].endswith(",") or raw_data[i].endswith("_t") :
-            letter_count += 1
-
-        # check for when going to the function body
-        if raw_data[i].endswith("{"):
-            spacing_legnth = len(raw_data[i + 1]) - 8
-            raw_data[i] += ("\n"+" "*2)
-            letter_count = 2
-
-        # check for when function body ends
-        elif raw_data[i].endswith("}"):
-            raw_data[i] = ("\n" + raw_data[i] + "\n")
-            letter_count = 0
+def format_function_body(template):
+    # Custom jinja filter that takes in the function body string
+    # and format it to indent to a new line whenever string length 
+    # reaches 100 characters.
     
-    raw_data = " ".join(raw_data).replace("\n ", "\n")
-    # writes data to .h template
-    with open(file_path, "w") as f:
-       f.write(raw_data)
-    
+    template = template.split()
+    template[0] = '  ' + template[0] 
+    letter_count = 0
+    for i in range(len(template)):
+        letter_count += len(template[i]) + 1
+        if letter_count >= 100:
+            template[i] = "\n" + (" " *(len(template[0]) - 10)) + template[i]
+            letter_count = len(template[i])
+    template = " ".join(template)
+    return (template)
 
 def get_board_name(yaml_path):
     return yaml_path.split("/")[len(yaml_path.split("/"))-1].split(".")[0]
@@ -173,6 +153,8 @@ if __name__ == "__main__":
         template_name = options.template.split('/')[-1]
         templateLoader = jinja2.FileSystemLoader(searchpath=codegen_dir)
         env = jinja2.Environment(loader=templateLoader)
+        env.filters["format_function_parameters"] = format_function_parameters
+        env.filters["format_function_body"] = format_function_body
         file_path = options.file_path + "/" + get_file_name(template_name, options.board)
 
         if "system_can" in template_name:
@@ -183,10 +165,7 @@ if __name__ == "__main__":
         elif options.board and options.yaml_file:
             for y in options.yaml_file:
                 data = read_yaml(y)
-                if "pack" in template_name:
-                    format_pack_h(env, template_name, file_path, data)
-                else:
-                    write_template(env, template_name, file_path, data)
+                write_template(env, template_name, file_path, data)
         elif options.board:
             data = parse_board_yaml_files(options.board)
             write_template(env, template_name, file_path, data)
