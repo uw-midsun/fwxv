210d750bfa15fd29523c07f5e92808aa4942ce2c
Kyle <chankyle9@gmail.com>
2022-03-26 17:04:08 +0000

    [SOFT-605] formatted log test

--- libraries/ms-common/test/test_log.c
+++ libraries/ms-common/test/test_log.c
@@ -28,14 +28,14 @@ TASK_TEST(test_log, TASK_STACK_512) {
 
   delay_ms(1);
 
-  for(int i = 1; i <= 33; i++){
+  for (int i = 1; i <= 33; i++) {
     LOG_DEBUG("%i \n", i);
     // should not print 33
   }
 
   delay_ms(50);
 
-  for(int i = 1; i <= 33; i++){
+  for (int i = 1; i <= 33; i++) {
     LOG_CRITICAL("%i \n", i);
     // should not print 33
     // 1 should be at the back
@@ -45,31 +45,31 @@ TASK_TEST(test_log, TASK_STACK_512) {
 
   // string log
   char trash[] = "Shichengs office hours";
-  LOG_DEBUG("This is a string log: %s \n", trash );
+  LOG_DEBUG("This is a string log: %s \n", trash);
 
   // int log
-  LOG_DEBUG("This is a positive int log: %i \n", 69 );
-  LOG_DEBUG("This is a negative int log: %i \n", -420 );
+  LOG_DEBUG("This is a positive int log: %i \n", 69);
+  LOG_DEBUG("This is a negative int log: %i \n", -420);
 
   // double log
-  LOG_DEBUG("This is a positive double log: %f \n", 69.420 );
-  LOG_DEBUG("This is a negative double log: %f \n", -420.69 );
+  LOG_DEBUG("This is a positive double log: %f \n", 69.420);
+  LOG_DEBUG("This is a negative double log: %f \n", -420.69);
   LOG_DEBUG("This is a capped (at 3 decimals) double log: %.3f \n", -3.454234);
 
   // boolean log
-  LOG_DEBUG("The statement below me is true: %i \n", true ); // actually true
-  LOG_DEBUG("The statement above me is false: %i \n", false ); // actually false
+  LOG_DEBUG("The statement below me is true: %i \n", true);    // actually true
+  LOG_DEBUG("The statement above me is false: %i \n", false);  // actually false
 
   // long int log
-  LOG_DEBUG("This is a positive long int log: %li \n", 69311489273489724 );
-  LOG_DEBUG("This is a negative long int log: %li \n", -69311489273489724 );
+  LOG_DEBUG("This is a positive long int log: %li \n", 69311489273489724);
+  LOG_DEBUG("This is a negative long int log: %li \n", -69311489273489724);
 
   // pointer log
-  LOG_DEBUG("This is a pointer: %p \n", trash );
+  LOG_DEBUG("This is a pointer: %p \n", trash);
 
   // testing multiple arguments
-  LOG_DEBUG("This is everything: %s, %i, %.3f, %li, %p \n", trash, 69, 69.420, 69311489273489724, trash );
+  LOG_DEBUG("This is everything: %s, %i, %.3f, %li, %p \n", trash, 69, 69.420, 69311489273489724,
+            trash);
 
   delay_ms(50);
-
 }
