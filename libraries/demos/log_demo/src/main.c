8a704f1c25430bb27fe9803f25e4481215b405f7
Kyle <chankyle9@gmail.com>
2022-03-26 20:05:13 +0000

    [SOFT-605] formatted log_demo

--- libraries/demos/log_demo/src/main.c
+++ libraries/demos/log_demo/src/main.c
@@ -6,17 +6,20 @@
 #include "misc.h"
 #include "tasks.h"
 
-
 TASK(break_task, TASK_STACK_512) {
-  while(true){
-    LOG_DEBUG("THIS IS THE FIRST TASK AND IT SHOULD NOT BE INTERRUPTED AND IT IS IN ALL CAPS, AND THIS SHOULD RUN\r\n");
+  while (true) {
+    LOG_DEBUG(
+        "THIS IS THE FIRST TASK AND IT SHOULD NOT BE INTERRUPTED AND IT IS IN ALL CAPS, AND THIS "
+        "SHOULD RUN\r\n");
     delay_ms(10);
   }
 }
 
 TASK(break2_task, TASK_STACK_512) {
-  while(true){
-    LOG_DEBUG("this is the second task and it should also not be interrupted and it is not in all caps, and this should run\r\n");
+  while (true) {
+    LOG_DEBUG(
+        "this is the second task and it should also not be interrupted and it is not in all caps, "
+        "and this should run\r\n");
     delay_ms(10);
   }
 }
