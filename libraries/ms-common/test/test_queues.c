2735e7ce8b48cfe2a5e64b0fe4bb23ad32f2183c
Bafran <aachinoy@gmail.com>
2022-03-30 22:29:09 -0400

    Queues tests

--- libraries/ms-common/test/test_queues.c
+++ libraries/ms-common/test/test_queues.c
@@ -36,7 +36,8 @@ void setup_test(void) {
 
   BaseType_t status;
   status = queue_init(&queue);
-  if(status == STATUS_CODE_OK) LOG_DEBUG("Q created with %d item capacity\n", queue_get_num_items(&queue));
+  if(status == STATUS_CODE_OK)
+    LOG_DEBUG("Q created with %d item capacity\n", queue_get_num_items(&queue));
 }
 
 void teardown_test(void) {}
@@ -48,19 +49,16 @@ TASK(sendMessages, TASK_STACK_512) {
   volatile TickType_t tick_count;
 
   while (true) {
-
-    status = queue_send(&queue, (void *) &s_list[index], 100);
-    tick_count = xTaskGetTickCount();
-    while(status == STATUS_CODE_OK)
-    {
-      LOG_DEBUG("Sent int: %d at tick %lu\n", s_list[index], tick_count);
+    do {
       status = queue_send(&queue, (void *) &s_list[index], 100);
       tick_count = xTaskGetTickCount();
+      if (status==STATUS_CODE_OK)
+        LOG_DEBUG("Sent int: %d at tick %lu\n", s_list[index], tick_count);
       if (++index == 5) {
         index = 0;
       }
-    }
-    LOG_DEBUG("Delayed until tick %lu and failed\n", tick_count);
+    } while(status == STATUS_CODE_OK);
+    LOG_DEBUG("Delayed until tick %lu and failed to send\n", tick_count);
     delay_ms(2000);
   }
 }
@@ -76,7 +74,7 @@ TASK(receiveMessages, TASK_STACK_512) {
       tick_count = xTaskGetTickCount();
       if (status != STATUS_CODE_OK) {
         LOG_DEBUG("Error reading from queue at tick %lu\n", tick_count);
-
+        delay_ms(100);
         continue;
       }
       LOG_DEBUG("Received int: %d at tick %lu\n", outstr, tick_count);
@@ -89,30 +87,46 @@ TASK(peekMessages, TASK_STACK_512) {
     volatile TickType_t tick_count;
 
     while(true) {
-      delay_ms(100);
-      status = queue_peek(&queue, &outstr, 50);
+      status = queue_peek(&queue, &outstr, 200);
       tick_count = xTaskGetTickCount();
       if (status != STATUS_CODE_OK) {
         LOG_DEBUG("Failed to peek at tick %lu\n", tick_count);
       } else {
         LOG_DEBUG("Peeked item %d at %lu\n", outstr, tick_count);
+        delay_ms(500);
       }
     }
 }
 
 TASK_TEST(test_running_task, TASK_STACK_512) {
-  // Start the task: note no need to call tasks_start() because we're inside a test task and
-  // FreeRTOS is already running.
+  // Send
   tasks_init_task(sendMessages, TASK_PRIORITY(3), NULL);
-  tasks_init_task(receiveMessages, TASK_PRIORITY(2), NULL);
-  // tasks_init_task(peekMessages, TASK_PRIORITY(1), NULL);
-
-  // The task doesn't start immediately because the test task has the highest priority.
+  
   TEST_ASSERT_FALSE(s_task_started);
+  delay_ms(100);
+  TEST_ASSERT_TRUE(s_task_started);
 
-  // To let it run, use a delay.
+  // Queue should be full
+  bool status;
+  uint8_t num = 1;
+  if(queue_send(&queue, (void *) &num, 0) != STATUS_CODE_OK)
+    status = true;
+  else
+    status = false;
+  TEST_ASSERT_TRUE(status);
+
+  // Peek and receive
+  tasks_init_task(peekMessages, TASK_PRIORITY(2), NULL);
+  tasks_init_task(receiveMessages, TASK_PRIORITY(1), NULL);
+
   delay_ms(1000);
 
-  // The task should have run.
-  TEST_ASSERT_TRUE(s_task_started);
+  // Queue is empty
+  const uint8_t outstr = 0;
+  if(queue_peek(&queue, &outstr, 0) != STATUS_CODE_OK)
+    status = true;
+  else
+    status = false;
+  
+  TEST_ASSERT_TRUE(status);
 }
