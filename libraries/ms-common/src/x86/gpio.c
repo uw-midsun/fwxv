6198f05ad1dcbda1f38d6a0d1310fc1a55ab2bab
Adrian Jendo <ajendo@uwaterloo.ca>
2022-04-02 13:03:01 -0700

    Add mutex logic to gpio.c

--- libraries/ms-common/src/x86/gpio.c
+++ libraries/ms-common/src/x86/gpio.c
@@ -5,9 +5,11 @@
 
 #include "log.h"
 #include "status.h"
+#include "mutex.h"
 
 static GpioSettings s_pin_settings[GPIO_TOTAL_PINS];
 static uint8_t s_gpio_pin_input_value[GPIO_TOTAL_PINS];
+static Mutex s_gpioMutex;
 
 static uint32_t prv_get_index(const GpioAddress *address) {
   return address->port * (uint32_t)GPIO_PINS_PER_PORT + address->pin;
@@ -24,34 +26,51 @@ StatusCode gpio_init(void) {
     s_pin_settings[i] = default_settings;
     s_gpio_pin_input_value[i] = 0;
   }
+  StatusCode status = mutex_init(&s_gpioMutex);
 
   return STATUS_CODE_OK;
 }
 
 StatusCode gpio_init_pin(const GpioAddress *address, const GpioSettings *settings) {
-  if (address->port >= NUM_GPIO_PORTS || address->pin >= GPIO_PINS_PER_PORT ||
+  StatusCode status = mutex_lock(&s_gpioMutex, BLOCK_INDEFINITELY);
+
+  if (status != STATUS_CODE_OK) {
+    return status_code(status);
+  } else if (address->port >= NUM_GPIO_PORTS || address->pin >= GPIO_PINS_PER_PORT ||
       settings->direction >= NUM_GPIO_DIRS || settings->state >= NUM_GPIO_STATES ||
       settings->resistor >= NUM_GPIO_RESES || settings->alt_function >= NUM_GPIO_ALTFNS) {
+    mutex_unlock(&s_gpioMutex);
     return status_code(STATUS_CODE_INVALID_ARGS);
   }
 
   s_pin_settings[prv_get_index(address)] = *settings;
+  mutex_unlock(&s_gpioMutex);
   return STATUS_CODE_OK;
 }
 
 StatusCode gpio_set_state(const GpioAddress *address, GpioState state) {
-  if (address->port >= NUM_GPIO_PORTS || address->pin >= GPIO_PINS_PER_PORT ||
-      state >= NUM_GPIO_STATES) {
+  StatusCode status = mutex_lock(&s_gpioMutex, BLOCK_INDEFINITELY);
+
+  if (status != STATUS_CODE_OK) {
+    return status_code(status);
+  } else if (address->port >= NUM_GPIO_PORTS || address->pin >= GPIO_PINS_PER_PORT ||
+    state >= NUM_GPIO_STATES) {
+    mutex_unlock(&s_gpioMutex);
     return status_code(STATUS_CODE_INVALID_ARGS);
   }
 
   s_pin_settings[prv_get_index(address)].state = state;
-
+  mutex_unlock(&s_gpioMutex);
   return STATUS_CODE_OK;
 }
 
 StatusCode gpio_toggle_state(const GpioAddress *address) {
-  if (address->port >= NUM_GPIO_PORTS || address->pin >= GPIO_PINS_PER_PORT) {
+  StatusCode status = mutex_lock(&s_gpioMutex, BLOCK_INDEFINITELY);
+
+  if (status != STATUS_CODE_OK) {
+    return status_code(status);
+  } else if (address->port >= NUM_GPIO_PORTS || address->pin >= GPIO_PINS_PER_PORT) {
+    mutex_unlock(&s_gpioMutex);
     return status_code(STATUS_CODE_INVALID_ARGS);
   }
 
@@ -62,11 +81,17 @@ StatusCode gpio_toggle_state(const GpioAddress *address) {
     s_pin_settings[index].state = GPIO_STATE_LOW;
   }
 
+  mutex_unlock(&s_gpioMutex);
   return STATUS_CODE_OK;
 }
 
 StatusCode gpio_get_state(const GpioAddress *address, GpioState *state) {
-  if (address->port >= NUM_GPIO_PORTS || address->pin >= GPIO_PINS_PER_PORT) {
+  StatusCode status = mutex_lock(&s_gpioMutex, BLOCK_INDEFINITELY);
+
+  if (status != STATUS_CODE_OK) {
+    return status_code(status);
+  } else if (address->port >= NUM_GPIO_PORTS || address->pin >= GPIO_PINS_PER_PORT) {
+    mutex_unlock(&s_gpioMutex);
     return status_code(STATUS_CODE_INVALID_ARGS);
   }
 
@@ -78,5 +103,7 @@ StatusCode gpio_get_state(const GpioAddress *address, GpioState *state) {
   } else {
     *state = s_gpio_pin_input_value[index];
   }
+
+  mutex_unlock(&s_gpioMutex);
   return STATUS_CODE_OK;
 }
