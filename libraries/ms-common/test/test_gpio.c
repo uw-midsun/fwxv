c15eb8e64f9f28f122a9c980912174c48ebd4011
Adrian Jendo <ajendo@uwaterloo.ca>
2022-04-02 17:42:49 -0700

    Add tests

--- libraries/ms-common/test/test_gpio.c
+++ libraries/ms-common/test/test_gpio.c
@@ -26,44 +26,56 @@ void teardown_test(void) {}
 // Create two tasks here
 TASK(task1, TASK_STACK_512) {
   LOG_DEBUG("task1 started\n");
-  // GpioState state;
+  GpioState state;
+  StatusCode status;
 
   // Loop forever, because tasks should not exit.
   while (true) {
-    // gpio_set_state(&s_gpio_addr, GPIO_STATE_LOW);
-    // gpio_get_state(&s_gpio_addr, &state);
-    // TEST_ASSERT_TRUE(state == GPIO_STATE_LOW);
-    LOG_DEBUG("ABC\n");
-    delay_ms(20);
+    status = gpio_set_state(&s_gpio_addr, GPIO_STATE_LOW);
+    TEST_ASSERT_TRUE(status == STATUS_CODE_OK);
+    status = gpio_get_state(&s_gpio_addr, &state);
+    TEST_ASSERT_TRUE(status == STATUS_CODE_OK);
+    TEST_ASSERT_TRUE(state == GPIO_STATE_LOW);
+    delay_ms(200);
   }
 }
 
 TASK(task2, TASK_STACK_512) {
   LOG_DEBUG("task2 started\n");
+  GpioState state;
+  StatusCode status;
 
   // Loop forever, because tasks should not exit.
-  while (true) {
-    LOG_DEBUG("123");
-    // gpio_set_state(s_gpio_addr, GPIO_STATE_LOW);
-    // GpioState state;
-    // gpio_get_state(s_gpio_addr, &state);
-    // TEST_ASSERT_TRUE(state == GPIO_STATE_LOW)
-    // delay_ms(10);
+  while (true) {  
+    status = gpio_set_state(&s_gpio_addr, GPIO_STATE_HIGH);
+    TEST_ASSERT_TRUE(status == STATUS_CODE_OK);
+    status = gpio_get_state(&s_gpio_addr, &state);
+    TEST_ASSERT_TRUE(status == STATUS_CODE_OK);
+    TEST_ASSERT_TRUE(state == GPIO_STATE_HIGH);
+    delay_ms(100);
+    status = gpio_get_state(&s_gpio_addr, &state);
+    TEST_ASSERT_TRUE(status == STATUS_CODE_OK);
+    status = gpio_toggle_state(&s_gpio_addr);
+    TEST_ASSERT_TRUE(status == STATUS_CODE_OK);
+    if (state == GPIO_STATE_HIGH) {
+      status = gpio_get_state(&s_gpio_addr, &state);
+      TEST_ASSERT_TRUE(status == STATUS_CODE_OK);
+      TEST_ASSERT_TRUE(state == GPIO_STATE_LOW);
+    } else {
+      status = gpio_get_state(&s_gpio_addr, &state);
+      TEST_ASSERT_TRUE(status == STATUS_CODE_OK);
+      TEST_ASSERT_TRUE(state == GPIO_STATE_HIGH);
+    }
+    delay_ms(100);
   }
 }
 
 TASK_TEST(test_running_task, TASK_STACK_512) {
   // Start the task: note no need to call tasks_start() because we're inside a test task and
   // FreeRTOS is already running.
-  tasks_init_task(task1, TASK_PRIORITY(2), NULL);
-  tasks_init_task(task2, TASK_PRIORITY(1), NULL); // higher priority
-
-  // The task doesn't start immediately because the test task has the highest priority.
-  TEST_ASSERT_FALSE(false);
+  tasks_init_task(task1, TASK_PRIORITY(2), NULL); // higher priority
+  tasks_init_task(task2, TASK_PRIORITY(1), NULL);
 
   // To let it run, use a delay.
   delay_ms(5000);
-
-  // The tasks should have run.
-  TEST_ASSERT_TRUE(true);
 }
