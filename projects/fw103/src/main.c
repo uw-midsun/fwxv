1cf69888c0fc38cf3b8ab6289b99af322e4a25d5
Harsh Patel <its.hp3@gmail.com>
2022-05-26 00:03:27 +0000

    harshs tasks commit

---
+++ projects/fw103/src/main.c
@@ -0,0 +1,49 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "delay.h"
+#include "log.h"
+#include "misc.h"
+#include "queues.h"
+#include "status.h"
+#include "tasks.h"
+
+// Non blocking delay. Simply consumes cpu cycles until a given time has passed
+static void prv_delay(const TickType_t delay_ms) {
+  TickType_t curr_tick = xTaskGetTickCount();
+  while (xTaskGetTickCount() - curr_tick < pdMS_TO_TICKS(delay_ms)) {
+  }
+}
+
+TASK(task1, TASK_STACK_512) {
+  int counter1 = 0;
+  while (true) {
+    LOG_DEBUG("Task: %s | Counter: %d\n", "task1", counter1);
+    counter1++;
+    prv_delay(2000);
+  }
+}
+
+TASK(task2, TASK_STACK_512) {
+  int counter2 = 0;
+  while (true) {
+    LOG_DEBUG("Task: %s | Counter: %d\n", "task2", counter2);
+    counter2++;
+    delay_ms(1000);  // task is in blocked state for 1s before
+    // prv_delay(1000);
+  }
+}
+
+int main(void) {
+  log_init();
+  // Create tasks here
+  tasks_init_task(task1, TASK_PRIORITY(1), NULL);
+  tasks_init_task(task2, TASK_PRIORITY(2), NULL);
+
+  LOG_DEBUG("Program start...\n");
+  // Start the scheduler
+  tasks_start();
+
+  return 0;
+}
