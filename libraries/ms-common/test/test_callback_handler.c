8ad8f34215e200817bb0eabe405b00723a2d9b24
Daniel Qu <danielq987@gmail.com>
2022-05-28 12:35:45 -0400

    Add brief tests for callback handler

--- libraries/ms-common/test/test_callback_handler.c
+++ libraries/ms-common/test/test_callback_handler.c
@@ -13,30 +13,61 @@ void setup_test(void) {
 
 void teardown_test(void) {}
 
-void hello_world(void) {
-  LOG_DEBUG("Hello\n");
-  delay_ms(50);
-  LOG_DEBUG("world\n");
+void print_num(void *context) {
+  uint8_t *num = context;
+  LOG_DEBUG("%d has been printed.\n", *num);
 }
 
-TASK(basic_callback, TASK_STACK_512) {
-  callback cb = &hello_world;
+// Register maximum number of callbacks, then trigger all of them successively.
+TASK(register_max_callbacks, TASK_STACK_512) {
+  CallbackFn cb = &print_num;
 
-  Event event = register_callback(cb);
+  Event registered_events[MAX_CALLBACKS] = { 0 };
+  uint8_t test_numbers[MAX_CALLBACKS] = { 0 };
 
-  LOG_DEBUG("Callback %d registered: waiting 300ms\n", event);
-
-  delay_ms(300);
-
-  notify(callback_task->handle, event);
-  while (true) {
+  for (uint8_t i = 0; i < MAX_CALLBACKS; ++i) {
+    test_numbers[i] = i;
+    Event event = register_callback(cb, test_numbers + i);
+    registered_events[i] = event;
+    TEST_ASSERT_LESS_OR_EQUAL(MAX_CALLBACKS, event);
   }
+
+  // Wait for callback_overflow test to finish running
+  delay_ms(50);
+
+  for (uint8_t i = 0; i < MAX_CALLBACKS; ++i) {
+    StatusCode result = notify(callback_task->handle, registered_events[i]);
+    TEST_ASSERT(result);
+    delay_ms(10);
+  }
+
+  while (1);
 }
+
+// Try registering a callback when MAX_CALLBACKS has been reached
+// Callback slots get filled from register_max_callbacks task
+TASK(callback_overflow, TASK_STACK_512) {
+  uint32_t *num = malloc(sizeof(uint32_t));
+
+  Event event = register_callback(print_num, num);
+  TEST_ASSERT_EQUAL(event, INVALID_EVENT);
+
+  // Try notifying callback task with invalid event
+  StatusCode status = notify(callback_task->handle, event);
+  TEST_ASSERT_EQUAL(status, STATUS_CODE_INVALID_ARGS);
+
+  free(num);
+
+  while (1);
+}
+
 
 TASK_TEST(test_callbacks, TASK_STACK_512) {
   delay_ms(1);
   LOG_DEBUG("test_callbacks\n");
-  tasks_init_task(basic_callback, TASK_PRIORITY(1), NULL);
+  tasks_init_task(register_max_callbacks, TASK_PRIORITY(1), NULL);
+  delay_ms(1);
+  tasks_init_task(callback_overflow, TASK_PRIORITY(1), NULL);
 
-  delay_ms(1000);
+  delay_ms(3000);
 }
