084e7b32bb515671d6f131f96f36cfbc18bb593b
awaterston01 <73081562+awaterston01@users.noreply.github.com>
2022-07-06 19:34:18 -0400

    FWXV-109, log critical on stack overflow (#69)
    Co-authored-by: vagrant <vagrant@midsunbox>

--- libraries/ms-freertos/inc/FreeRTOSConfig.h
+++ libraries/ms-freertos/inc/FreeRTOSConfig.h
@@ -59,7 +59,7 @@ extern uint32_t SystemCoreClock;
 // Hook function related definitions
 #define configUSE_IDLE_HOOK 0
 #define configUSE_TICK_HOOK 0
-#define configCHECK_FOR_STACK_OVERFLOW 0
+#define configCHECK_FOR_STACK_OVERFLOW 2
 #define configUSE_MALLOC_FAILED_HOOK 0
 #define configUSE_DAEMON_TASK_STARTUP_HOOK 0
 
