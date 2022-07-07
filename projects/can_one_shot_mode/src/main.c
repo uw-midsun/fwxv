7438d1451899edb73d09fbe85f2f09df1b40edb3
vagrant <vagrant@midsunbox>
2022-07-07 00:01:34 +0000

    initial commit for polling implementation

--- projects/can_one_shot_mode/src/main.c
+++ projects/can_one_shot_mode/src/main.c
@@ -6,13 +6,6 @@
 #include "log.h"
 #include "tasks.h"
 
-// Dsiable all threads
-// pack message
-// transmit message
-// receive messahe
-// unpack message
-// all needed functions are in the slides
-
 static CanStorage s_can_storage = { 0 };
 const CanSettings can_settings = {
   .device_id = 0x1,
@@ -20,24 +13,23 @@ const CanSettings can_settings = {
   .tx = { GPIO_PORT_A, 12 },
   .rx = { GPIO_PORT_A, 11 },
   .loopback = true,
+  .mode = 1,
 };
 
 TASK(testing_task, TASK_MIN_STACK_SIZE) {
-  int counter = 0;
+  CanMessage msg = { 0 };
   while (true) {
-    if (counter == 5) {
-      //   set_transmit_msg1_status(0xab);
+    if (can_receive(&msg, NULL) == STATUS_CODE_OK) {
+      LOG_DEBUG("can_one_received a message");
     }
-    counter++;
     vTaskDelay(pdMS_TO_TICKS(1000));
   }
 }
-
 int main() {
   log_init();
-  LOG_DEBUG("Welcome to CAN!\n");
+  LOG_DEBUG("Welcome to CAN_ONE_SHOT_MODE!\n");
   can_init(&s_can_storage, &can_settings);
-  //   can_add_filter(SYSTEM_CAN_MESSAGE_NEW_CAN_TRANSMIT_MSG1);
+  can_add_filter(SYSTEM_CAN_MESSAGE_NEW_CAN_TRANSMIT_MSG1);
 
   tasks_init_task(testing_task, TASK_PRIORITY(2), NULL);
 
