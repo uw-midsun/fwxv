4b6d6e2599ae2be9bac4ffae42251a3d3b9192a5
Elbert Chen <elbertchen5@gmail.com>
2022-07-16 01:19:18 +0000

    Complete task 2

---
+++ projects/FW_103/src/main.c
@@ -0,0 +1,90 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "tasks.h"
+#include "queues.h"
+#include "status.h"
+#include "delay.h"
+
+#include "log.h"
+#include "misc.h"
+
+
+#define ITEM_SZ 6
+#define QUEUE_LEN 5
+#define BUF_SIZE (QUEUE_LEN * ITEM_SZ)
+
+static const char s_list[QUEUE_LEN][ITEM_SZ] = {
+	"Item1",
+	"Item2",
+	"Item3",
+	"Item4",
+	"Item5"
+};
+
+// Task static entities
+static uint8_t s_queue1_buf[BUF_SIZE]; 
+static Queue s_queue1 = { 
+  .num_items = QUEUE_LEN,
+  .item_size = ITEM_SZ,
+  .storage_buf = s_queue1_buf,
+};
+
+
+TASK(task1, TASK_STACK_512) {
+  LOG_DEBUG("Task 1 initialized!\n");
+  StatusCode ret;
+  char word[ITEM_SZ];
+
+  while (true) {
+    for (int count = 0; count < QUEUE_LEN; count++)
+    {
+      for (int count1 = 0; count1 < ITEM_SZ - 1; count1++)
+      {
+        word[count1] = s_list[count][count1];
+      }
+      
+      ret = queue_send(&s_queue1, &word, 1000);
+
+      if (ret != STATUS_CODE_OK)
+      {
+        LOG_DEBUG("Write to Queue Failed \n");
+      }
+
+      delay_ms(100);
+    }
+  }
+}
+
+TASK(task2, TASK_STACK_512) {
+  LOG_DEBUG("Task 2 initialized!\n");
+  const char outstr[ITEM_SZ];
+  StatusCode ret;
+  while (true) {
+    ret = queue_receive(&s_queue1, &s_queue1_buf, 1000);
+
+    if (ret != STATUS_CODE_OK)
+    {
+      LOG_DEBUG("read from queue failed \n");
+    }
+
+    else 
+    {
+      LOG_DEBUG("%s \n", s_queue1_buf);
+    }
+  }
+}
+
+int main(void) {
+  log_init();
+  queue_init(&s_queue1);
+
+  tasks_init_task(task1, TASK_PRIORITY(2), NULL);
+  tasks_init_task(task2, TASK_PRIORITY(2), NULL);
+
+  LOG_DEBUG("Program start...\n");
+  tasks_start();
+
+  return 0;
+}
