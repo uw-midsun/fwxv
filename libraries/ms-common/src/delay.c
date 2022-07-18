1676801b9b856c1202fe8d0b9798f919a89f3dd1
Xi Zhu <x295zhu@uwaterloo.ca>
2022-07-18 22:16:31 +0000

    Added check for if scheduler not started in delay_ms

--- libraries/ms-common/src/delay.c
+++ libraries/ms-common/src/delay.c
@@ -1,10 +1,15 @@
 #include "delay.h"
 
 #include "FreeRTOS.h"
+#include "log.h"
 #include "task.h"
 
 void delay_ms(uint32_t t) {
-  vTaskDelay(pdMS_TO_TICKS(t));
+  if (xTaskGetSchedulerState() == taskSCHEDULER_NOT_STARTED) {
+    LOG_CRITICAL("Error: Scheduler is not running!");
+  } else {
+    vTaskDelay(pdMS_TO_TICKS(t));
+  }
 }
 
 void non_blocking_delay_ms(uint32_t t) {
