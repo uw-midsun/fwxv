b0afdf1ea9eb81ff6de3b806203ba314de5400db
Keegan Liu <keeganliu5678@gmail.com>
2022-07-23 02:04:42 +0000

    Completed get, give, and take functions for semaphores.

--- libraries/ms-common/inc/tests.h
+++ libraries/ms-common/inc/tests.h
@@ -5,3 +5,15 @@
 
 // Initializes test end and test start semaphore
 void tests_init(void);
+
+// Get for the test end and test start semaphores
+void test_start_get(void);
+void test_end_get(void);
+
+// Give for the test start and test end semaphores
+void test_start_give(void);
+void test_end_give(void);
+
+// Take for the test start anbd test end semaphores
+void test_start_take(void);
+void test_end_take(void);
--- libraries/ms-common/test/tests.c
+++ libraries/ms-common/test/tests.c
@@ -13,6 +13,9 @@ static SemaphoreHandle_t s_test_end_handle;
 // and the max number of tests that we begin with for test start semaphore
 #define MAX_NUM_TESTS 10
 
+// The number of ticks to delay for take functions
+TickType_t TICKS_TO_DELAY = 100;
+
 // Initialize start semaphore with max count 10, initial count 10, and pointer to variable of type
 // Static_Sempahore_t The semaphore can be accessed through the handle called s_test_start_handle
 // The value of the semaphore begins at 10 to indicate the number of tests that still need to run,
@@ -20,4 +23,28 @@ static SemaphoreHandle_t s_test_end_handle;
 void tests_init(void) {
   s_test_start_handle = xSemaphoreCreateCountingStatic(MAX_NUM_TESTS, 10, &s_test_start_sem);
   s_test_end_handle = xSemaphoreCreateCountingStatic(MAX_NUM_TESTS, 0, &s_test_end_sem);
+}
+
+void test_start_get(void) {
+  uxSemaphoreGetCount(s_test_start_handle);
+}
+
+void test_end_get(void) {
+  uxSemaphoreGetCount(s_test_end_handle);
+}
+
+void test_start_give(void) {
+  xSemaphoreGive(s_test_start_handle);
+}
+
+void test_end_give(void) {
+  xSemaphoreGive(s_test_end_handle);
+}
+
+void test_start_take(void) {
+  xSemaphoreTake(s_test_start_handle, TICKS_TO_DELAY);
+}
+
+void test_end_take(void) {
+  xSemaphoreTake(s_test_end_handle, TICKS_TO_DELAY);
 }
