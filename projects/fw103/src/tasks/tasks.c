d08d59dff24dcced4db5d53c1393f568d4e6ca4c
Preston <preston.dresser16@gmail.ca>
2022-09-23 22:41:04 -0400

    format fix

--- projects/fw103/src/tasks/tasks.c
+++ projects/fw103/src/tasks/tasks.c
@@ -1,19 +1,19 @@
+#include "tasks.h"
+
 #include <stdbool.h>
 #include <stdint.h>
 
 #include "FreeRTOS.h"
-#include "tasks.h"
-
+#include "delay.h"
 #include "gpio.h"
 #include "log.h"
 #include "misc.h"
-#include "delay.h"
 
 // Non blocking delay. Simply consumes cpu cycles until a given time has passed
 static void prv_delay(const TickType_t delay_ms) {
   TickType_t curr_tick = xTaskGetTickCount();
-  while(xTaskGetTickCount() - curr_tick < pdMS_TO_TICKS(delay_ms))
-  {}
+  while (xTaskGetTickCount() - curr_tick < pdMS_TO_TICKS(delay_ms)) {
+  }
 }
 
 TASK(task1, TASK_STACK_512) {
@@ -35,13 +35,13 @@ TASK(task2, TASK_STACK_512) {
 }
 
 int main(void) {
-    log_init();
-    // Create tasks here
-    tasks_init();
-    tasks_init_task(task1, TASK_PRIORITY(1), NULL);
-    tasks_init_task(task2, TASK_PRIORITY(0), NULL);
-    LOG_DEBUG("Program start...\n");
-    // Start the scheduler
-    tasks_start();
-    return 0;
+  log_init();
+  // Create tasks here
+  tasks_init();
+  tasks_init_task(task1, TASK_PRIORITY(1), NULL);
+  tasks_init_task(task2, TASK_PRIORITY(0), NULL);
+  LOG_DEBUG("Program start...\n");
+  // Start the scheduler
+  tasks_start();
+  return 0;
 }
