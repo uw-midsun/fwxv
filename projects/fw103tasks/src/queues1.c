70865999062fb5efcbd93d37a8a215d5c35ce869
Louis Sun <louis.sun3570@gmail.com>
2022-10-04 12:55:41 -0400

    Formatted FW103 Tasks

--- projects/fw103tasks/src/queues1.c
+++ projects/fw103tasks/src/queues1.c
@@ -2,35 +2,24 @@
 #include <stdint.h>
 
 #include "FreeRTOS.h"
-#include "tasks.h"
-#include "queues.h"
-#include "status.h"
 #include "delay.h"
-
 #include "log.h"
 #include "misc.h"
-
+#include "queues.h"
+#include "status.h"
+#include "tasks.h"
 
 #define ITEM_SZ 6
 #define QUEUE_LEN 5
 #define BUF_SIZE (QUEUE_LEN * ITEM_SZ)
 
-static const char s_list[QUEUE_LEN][ITEM_SZ] = {
-	"Item1",
-	"Item2",
-	"Item3",
-	"Item4",
-	"Item5"
-};
+static const char s_list[QUEUE_LEN][ITEM_SZ] = { "Item1", "Item2", "Item3", "Item4", "Item5" };
 
 // Task static entities
 static uint8_t s_queue1_buf[BUF_SIZE];
-static Queue s_queue1 = {
-  .num_items = QUEUE_LEN,
-  .item_size = ITEM_SZ,
-  .storage_buf = s_queue1_buf
-};
-
+static Queue s_queue1 = { .num_items = QUEUE_LEN,
+                          .item_size = ITEM_SZ,
+                          .storage_buf = s_queue1_buf };
 
 TASK(task1, TASK_STACK_512) {
   LOG_DEBUG("Task 1 initialized!\n");
--- projects/hello_world/src/main.c
+++ projects/hello_world/src/main.c
@@ -1,46 +1,47 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+
 #include "gpio.h"
 #include "interrupt.h"
 //#include "wait.h"
+#include "delay.h"
 #include "gpio_it.h"
 #include "soft_timer.h"
-#include "delay.h"
 
 static const GpioAddress leds[] = {
-   { .port = GPIO_PORT_B, .pin = 5 },   //
-   { .port = GPIO_PORT_B, .pin = 4 },   //
-   { .port = GPIO_PORT_B, .pin = 3 },   //
-   { .port = GPIO_PORT_A, .pin = 15 },  //
+  { .port = GPIO_PORT_B, .pin = 5 },   //
+  { .port = GPIO_PORT_B, .pin = 4 },   //
+  { .port = GPIO_PORT_B, .pin = 3 },   //
+  { .port = GPIO_PORT_A, .pin = 15 },  //
 };
 
 static GpioSettings led_settings = {
   .direction = GPIO_DIR_OUT,        //
-  .state = GPIO_STATE_LOW,         //
+  .state = GPIO_STATE_LOW,          //
   .alt_function = GPIO_ALTFN_NONE,  //
   .resistor = GPIO_RES_NONE,        //
 };
 
 static void prv_my_gpio_it_callback(SoftTimerId timer_id, void *context) {
-   GpioAddress *led = context;
-   gpio_toggle_state(led);
-   soft_timer_start(1000000, prv_my_gpio_it_callback, led);
+  GpioAddress *led = context;
+  gpio_toggle_state(led);
+  soft_timer_start(1000000, prv_my_gpio_it_callback, led);
 }
 
 int main(void) {
-   interrupt_init();
-   gpio_init();
-   gpio_it_init();
-   soft_timer_init();
+  interrupt_init();
+  gpio_init();
+  gpio_it_init();
+  soft_timer_init();
 
-   for (uint8_t i = 0; i < SIZEOF_ARRAY(leds); i++) {
-      gpio_init_pin(&leds[i], &led_settings);
-      soft_timer_start(1000000, prv_my_gpio_it_callback, &leds[i]);
-   }
+  for (uint8_t i = 0; i < SIZEOF_ARRAY(leds); i++) {
+    gpio_init_pin(&leds[i], &led_settings);
+    soft_timer_start(1000000, prv_my_gpio_it_callback, &leds[i]);
+  }
 
-   while(true) {
-      wait();
-   }
+  while (true) {
+    wait();
+  }
 
-   return 0;
+  return 0;
 }
