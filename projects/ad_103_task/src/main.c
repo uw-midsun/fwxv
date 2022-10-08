117456e8cb75136573ed078344e31cffc4cf31a5
devAdhiraj <adhiraj.bal@gmail.com>
2022-10-07 22:58:11 -0400

    adhiraj fw103 task

---
+++ projects/ad_103_task/src/main.c
@@ -0,0 +1,52 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "delay.h"
+#include "log.h"
+#include "misc.h"
+#include "tasks.h"
+
+// Non blocking delay. Simply consumes cpu cycles until a given time has passed
+static void prv_delay(const TickType_t delay_ms) {
+  TickType_t curr_tick = xTaskGetTickCount();
+  while (xTaskGetTickCount() - curr_tick < pdMS_TO_TICKS(delay_ms)) {
+  }
+}
+
+TASK(task1, TASK_STACK_512) {
+  int counter1 = 0;
+  while (true) {
+    // Your code here
+    LOG_DEBUG("task1 %d\n", counter1);
+    counter1++;
+    //  delay_ms(100);
+    prv_delay(100);
+  }
+}
+
+TASK(task2, TASK_STACK_512) {
+  int counter2 = 0;
+  while (true) {
+    // Your code here
+    LOG_DEBUG("task2 %d\n", counter2);
+    counter2++;
+    //  delay_ms(100);
+    prv_delay(100);
+  }
+}
+
+int main(void) {
+  log_init();
+  // Create tasks here
+  tasks_init();
+
+  tasks_init_task(task1, TASK_PRIORITY(1), NULL);
+  tasks_init_task(task2, TASK_PRIORITY(1), NULL);
+
+  LOG_DEBUG("Program start...\n");
+  // Start the scheduler
+  tasks_start();
+
+  return 0;
+}
---
+++ projects/led_blink_102/src/main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+#include "delay.h"
+#include "gpio.h"
+#include "log.h"
+#include "misc.h"
+#include "soft_timer.h"
+#include "tasks.h"
+
+#ifdef MS_PLATFORM_X86
+#define MASTER_MS_CYCLE_TIME 100
+#else
+#define MASTER_MS_CYCLE_TIME 1000
+#endif
+
+static uint8_t done_timer = 1;
+
+static const GpioAddress leds[] = {
+  { .port = GPIO_PORT_B, .pin = 5 },   //
+  { .port = GPIO_PORT_B, .pin = 4 },   //
+  { .port = GPIO_PORT_B, .pin = 3 },   //
+  { .port = GPIO_PORT_A, .pin = 15 },  //
+};
+
+static SoftTimer s_timer;
+
+void blink_leds(SoftTimerId id) {
+  for (uint8_t i = 0; i < 4; ++i) {
+    gpio_toggle_state(&leds[i]);
+  }
+  LOG_DEBUG("toggled leds\n");
+  // done_timer = 1;
+  // soft timer start
+  soft_timer_start(500, blink_leds, &s_timer);
+}
+
+TASK(master_task, TASK_MIN_STACK_SIZE) {
+  LOG_DEBUG("in master task 1\n");
+  const GpioSettings led_settings = {
+    .direction = GPIO_DIR_OUT,        //
+    .state = GPIO_STATE_HIGH,         //
+    .alt_function = GPIO_ALTFN_NONE,  //
+    .resistor = GPIO_RES_NONE,        //
+  };
+  LOG_DEBUG("in master task 2\n");
+  for (uint8_t i = 0; i < SIZEOF_ARRAY(leds); ++i) {
+    gpio_init_pin(&leds[0], &led_settings);
+  }
+
+  LOG_DEBUG("in master task 2\n");
+  soft_timer_start(500, blink_leds, &s_timer);
+  while (true) {
+    // delay_ms(100);
+    // non_blocking_delay_ms(100);
+  }
+
+  // while (true) {
+  //   // LOG_DEBUG("in while loop\n");
+  //   if(done_timer == 1) {
+  //     done_timer = 0;
+  //   }
+  // }
+}
+
+int main() {
+  log_init();
+  gpio_init();
+  tasks_init();
+
+  tasks_init_task(master_task, TASK_PRIORITY(1), NULL);
+
+  LOG_DEBUG("Welcome to led_blink_102!\n");
+
+  tasks_start();
+
+  LOG_DEBUG("done!\n");
+
+  return 0;
+}
