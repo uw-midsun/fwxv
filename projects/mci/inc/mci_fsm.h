b71592a1b6f3a1ab7c3747afeac8214e8c9ad39b
Adhiraj <adhiraj.bal@gmail.com>
2022-10-12 01:53:37 +0000

    added mci fsm transitions v1

--- projects/mci/inc/mci_fsm.h
+++ projects/mci/inc/mci_fsm.h
@@ -9,24 +9,39 @@
 
 #define NUM_MCI_FSM_STATES 4
 #define NUM_MCI_FSM_TRANSITIONS 9
-
+#define CRUISE_MAX_SPEED 45  // TODO: update these with actual numbers
+#define CRUISE_MIN_SPEED 12  // TODO: update these with actual numbers
 DECLARE_FSM(mci_fsm);
 
-typedef enum Fsm1StateId {
-  FSM1_STATE_0 = 0,
-  FSM1_STATE_1,
-  FSM1_STATE_2,
-} Fsm1StateId;
+typedef enum MciFsmStateId {
+  MCI_FSM_STATE_OFF = 0,
+  MCI_FSM_STATE_DRIVE,
+  MCI_FSM_STATE_REVERSE,
+  MCI_FSM_STATE_CRUISE,
+} MciFsmStateId;
 
-// Notifications to send to FSM 2
-typedef enum Fsm1OutputEvent {
-  FSM1_STATE_0_CMPL = 0,
-  FSM1_STATE_1_CMPL,
-  FSM1_STATE_2_CMPL,
-} Fsm1OutputEvent;
+// Events that MCI FSM Recieves?
+typedef enum MciFsmOutputEvent {
+  MCI_FSM_GOTO_OFF = 0,
+  MCI_FSM_GOTO_DRIVE,
+  MCI_FSM_GOTO_REVERSE,
+  MCI_FSM_GOTO_CRUISE,
+} MciFsmOutputEvent;
 
-typedef struct Fsm1Storage {
-  GpioAddress start_btn_address;
-} Fsm1Storage;
+typedef struct MciFsmStorage {
+  // updated every 200ms
+  uint8_t velocity;  // (m/s)
+  int16_t rpm;
+  uint16_t bus_voltage_v;
+  uint16_t bus_current_a;
+  uint8_t mc_limit_bitset;
+  uint8_t mc_error_bitset;
+  // updated every second
+  int8_t motor_temp_c;
+  int8_t heatsink_temp_c;
+  int8_t dsp_temp_c;
+  float motor_temp;      // Celsius
+  float heat_sink_temp;  // Celsius
+} MciFsmStorage;
 
 StatusCode init_mci_fsm(void);
