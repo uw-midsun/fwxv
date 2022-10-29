1a128f01e1ffac7f2513b0d69e23d22a38158983
Louis Sun <louis.sun3570@gmail.com>
2022-10-29 13:42:12 -0400

    debugging pedal_setters.h

--- projects/pedal/src/main.c
+++ projects/pedal/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+//#include "ads1015.h"
 #include "gpio.h"
 #include "i2c.h"
 #include "interrupt.h"
@@ -11,6 +12,7 @@
 #include "can_board_ids.h"
 #include "log.h"
 #include "tasks.h"
+// #include "pedal_setters.h"
 
 #ifdef MS_PLATFORM_X86
 #define MASTER_MS_CYCLE_TIME 100
@@ -27,6 +29,9 @@ const CanSettings can_settings = {
    .loopback = true,
 };
 
+// static Ads1015Storage s_ads1015_storage = { 0 };
+// static PedalCalibBlob s_calib_blob = { 0 };
+
 // Read any GPIOs and the ADC for throttle
 bool read_pedal_throttle() {
    return false;
@@ -39,7 +44,7 @@ bool read_pedal_brake() {
 
 // Read the ADC when the throttle and brake are pressed down/let go, and use that reading to set the upper and lower bounds of the pedal
 void pedal_calibrate() {
-
+   return;
 }
 
 void run_fast_cycle()
@@ -54,16 +59,16 @@ void init_pedal_controls() {
    interrupt_init();
    gpio_init(); 
    gpio_it_init();
-   //soft_timer_init();
 
    // setup ADC readings
    I2CSettings i2c_settings = {
       .speed = I2C_SPEED_FAST,
       .scl = { .port = GPIO_PORT_B, .pin = 10 },
-      .sda = { .port = GPIO_PORT_B, .pin = 11 },
+   .sda = { .port = GPIO_PORT_B, .pin = 11 },
    };
    i2c_init(I2C_PORT_2, &i2c_settings);
-   GpioAddress ready_pin = { .port = GPIO_PORT_B, .pin = 2 };   
+   GpioAddress ready_pin = { .port = GPIO_PORT_B, .pin = 2 };
+   //ads1015_init(&s_ads1015_storage, I2C_PORT_2, ADS1015_ADDRESS_GND, &ready_pin);
    adc_init(ADC_MODE_SINGLE);
 
    pedal_calibrate();
@@ -74,12 +79,20 @@ void run_medium_cycle()
    run_can_tx_cycle();
    wait_tasks(1);
 
+   // msg1 - brake
+   // msg2 - throttle
    if (read_pedal_brake()) {
       // Edit message to send 1 for brake, 0 for throttle (if brake is pressed, disable the throttle)
+      // set_transmit_msg1_status(1);
+      // set_transmit_msg2_signal(0);
    } else if (read_pedal_throttle()) {
       // Edit message to send 1 for throttle (send throttle data as normal)
+      // set_transmit_msg1_status(0);
+      // set_transmit_msg2_signal(1);
    } else {
       // Edit message to send 0 for both
+      // set_transmit_msg1_status(0);
+      // set_transmit_msg2_signal(0);
    }
 
     // transmit message: use pedal_can_setters.h file to set the correct data for the can message instead of using the struct
@@ -112,7 +125,7 @@ int main() {
    tasks_init();
    log_init();
 
-   LOG_DEBUG("Welcome to CAN!");
+   LOG_DEBUG("Welcome to CAN!\n");
    init_pedal_controls();
    can_init(&s_can_storage, &can_settings);
    can_add_filter_in(SYSTEM_CAN_MESSAGE_PEDAL_PEDAL_OUTPUT);
