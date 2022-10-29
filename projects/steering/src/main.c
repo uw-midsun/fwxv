44b15076fda884be68fa6d77cf3de3b2f0ed9692
Citrusboa <ivybao07@gmail.com>
2022-10-29 14:19:17 -0400

    formatted main.c

--- projects/steering/src/main.c
+++ projects/steering/src/main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "adc.h"
 #include "can.h"
@@ -19,65 +19,60 @@
 
 #define DEVICE_ID 0x02
 
-static CanStorage s_can_storage = {0};
+static CanStorage s_can_storage = { 0 };
 const CanSettings can_settings = {
-   .device_id = DEVICE_ID,
-   .bitrate = CAN_HW_BITRATE_125KBPS, 
-   .tx = {GPIO_PORT_A, 12},
-   .rx = {GPIO_PORT_A, 11},
-   .loopback = true,
+  .device_id = DEVICE_ID,
+  .bitrate = CAN_HW_BITRATE_125KBPS,
+  .tx = { GPIO_PORT_A, 12 },
+  .rx = { GPIO_PORT_A, 11 },
+  .loopback = true,
 };
 
 void run_fast_cycle() {}
 
-void run_medium_cycle()
-{
-   run_can_rx_cycle();
-   wait_tasks(1);
+void run_medium_cycle() {
+  run_can_rx_cycle();
+  wait_tasks(1);
 
-   //run_steering_analog_task();
-   //run_steering_digital_task();
-   
-   run_can_tx_cycle();
-   wait_tasks(1); 
+  // run_steering_analog_task();
+  // run_steering_digital_task();
 
+  run_can_tx_cycle();
+  wait_tasks(1);
 }
 
 void run_slow_cycle() {}
 
 TASK(master_task, TASK_MIN_STACK_SIZE) {
-   int counter = 0;
-   while (true) {
+  int counter = 0;
+  while (true) {
 #ifdef TEST
-   xSemaphoreTake(test_cycle_start_sem);
+    xSemaphoreTake(test_cycle_start_sem);
 #endif
-      run_fast_cycle();
-      if (!(counter%10))
-         run_medium_cycle();
-      if (!(counter%100))
-         run_slow_cycle();
+    run_fast_cycle();
+    if (!(counter % 10)) run_medium_cycle();
+    if (!(counter % 100)) run_slow_cycle();
 #ifdef TEST
-   xSemaphoreGive(test_cycle_end_sem);
+    xSemaphoreGive(test_cycle_end_sem);
 #endif
-      vTaskDelay(pdMS_TO_TICKS(1000));
-      ++counter;
-    }
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    ++counter;
+  }
 }
 
 int main() {
-   tasks_init();
-   log_init();
-   gpio_init();
-   gpio_it_init();
-   adc_init(ADC_MODE_SINGLE);
-   //steering_analog_adc_init();
-   //steering_digital_input_init();
-   can_init(&s_can_storage, &can_settings);
+  tasks_init();
+  log_init();
+  gpio_init();
+  gpio_it_init();
+  adc_init(ADC_MODE_SINGLE);
+  // steering_analog_adc_init();
+  // steering_digital_input_init();
+  can_init(&s_can_storage, &can_settings);
 
-   tasks_init_task(master_task, TASK_PRIORITY(2), NULL);
+  tasks_init_task(master_task, TASK_PRIORITY(2), NULL);
 
-   tasks_start();
+  tasks_start();
 
-   return 0;
+  return 0;
 }
-
