6cbfb7370150e7c1bf92ccfb97cef1d961157e07
Louis Sun <louis.sun3570@gmail.com>
2022-10-29 14:37:07 -0400

    formatted pedal main task

--- projects/pedal/src/main.c
+++ projects/pedal/src/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-//#include "ads1015.h"
+// #include "ads1015.h"
+#include "adc.h"
 #include "gpio.h"
+#include "gpio_it.h"
 #include "i2c.h"
 #include "interrupt.h"
-#include "gpio_it.h"
 #include "soft_timer.h"
-#include "adc.h"
-//#include "event_queue.h"
+// #include "event_queue.h"
 #include "can.h"
-#include "can_msg.h"
 #include "can_board_ids.h"
+#include "can_msg.h"
 #include "log.h"
-#include "tasks.h"
 #include "pedal_setters.h"
+#include "tasks.h"
 
 #ifdef MS_PLATFORM_X86
 #define MASTER_MS_CYCLE_TIME 100
@@ -22,110 +22,108 @@
 
 static CanStorage s_can_storage = { 0 };
 const CanSettings can_settings = {
-   .device_id = 0x1,
-   .bitrate = CAN_HW_BITRATE_500KBPS,
-   .tx = { GPIO_PORT_A, 12 },
-   .rx = { GPIO_PORT_A, 11 },
-   .loopback = true,
+  .device_id = 0x1,
+  .bitrate = CAN_HW_BITRATE_500KBPS,
+  .tx = { GPIO_PORT_A, 12 },
+  .rx = { GPIO_PORT_A, 11 },
+  .loopback = true,
 };
 
 // Read any GPIOs and the ADC for throttle
 bool read_pedal_throttle() {
-   return false;
+  return false;
 }
 
 // Read any GPIOs and the ADC for brake
 bool read_pedal_brake() {
-   return false;
+  return false;
 }
 
-// Read the ADC when the throttle and brake are pressed down/let go, and use that reading to set the upper and lower bounds of the pedal
+// Read the ADC when the throttle and brake are pressed down/let go, and use that reading to set the
+// upper and lower bounds of the pedal
 void pedal_calibrate() {
-   return;
+  return;
 }
 
-void run_fast_cycle()
-{
-   return;
+void run_fast_cycle() {
+  return;
 }
 
 // Initialize the GPIOs needed for the throttle
 // Initialize ADC for ADC readings
-// Calibrate the pedal upon initialization (set what we consider max value and min value for each pedal)
-void init_pedal_controls() { 
-   interrupt_init();
-   gpio_init(); 
-   gpio_it_init();
+// Calibrate the pedal upon initialization (set what we consider max value and min value for each
+// pedal)
+void init_pedal_controls() {
+  interrupt_init();
+  gpio_init();
+  gpio_it_init();
 
-   // setup ADC readings
-   I2CSettings i2c_settings = {
-      .speed = I2C_SPEED_FAST,
-      .scl = { .port = GPIO_PORT_B, .pin = 10 },
-   .sda = { .port = GPIO_PORT_B, .pin = 11 },
-   };
-   i2c_init(I2C_PORT_2, &i2c_settings);
-   GpioAddress ready_pin = { .port = GPIO_PORT_B, .pin = 2 };
-   adc_init(ADC_MODE_SINGLE);
+  // setup ADC readings
+  I2CSettings i2c_settings = {
+    .speed = I2C_SPEED_FAST,
+    .scl = { .port = GPIO_PORT_B, .pin = 10 },
+    .sda = { .port = GPIO_PORT_B, .pin = 11 },
+  };
+  i2c_init(I2C_PORT_2, &i2c_settings);
+  GpioAddress ready_pin = { .port = GPIO_PORT_B, .pin = 2 };
+  adc_init(ADC_MODE_SINGLE);
 
-   pedal_calibrate();
+  pedal_calibrate();
 }
 
-void run_medium_cycle()
-{
-   run_can_tx_cycle();
-   wait_tasks(1);
+void run_medium_cycle() {
+  run_can_tx_cycle();
+  wait_tasks(1);
 
-   // Sending messages
-   if (read_pedal_brake()) {
-      // Send 1 for brake, 0 for throttle (if brake is pressed, disable the throttle)
-      set_pedal_output_brake_output(1);
-      set_pedal_output_throttle_output(0);
-   } else if (read_pedal_throttle()) {
-      // Send 1 for throttle (send throttle data as normal)
-      set_pedal_output_brake_output(0);
-      set_pedal_output_throttle_output(1);
-   } else {
-      // Send 0 for both throttle and brake
-      set_pedal_output_brake_output(0);
-      set_pedal_output_throttle_output(0);
-   }
+  // Sending messages
+  if (read_pedal_brake()) {
+    // Send 1 for brake, 0 for throttle (if brake is pressed, disable the throttle)
+    set_pedal_output_brake_output(1);
+    set_pedal_output_throttle_output(0);
+  } else if (read_pedal_throttle()) {
+    // Send 1 for throttle (send throttle data as normal)
+    set_pedal_output_brake_output(0);
+    set_pedal_output_throttle_output(1);
+  } else {
+    // Send 0 for both throttle and brake
+    set_pedal_output_brake_output(0);
+    set_pedal_output_throttle_output(0);
+  }
 }
 
-void run_slow_cycle()
-{
-   return;
+void run_slow_cycle() {
+  return;
 }
 
 TASK(master_task, TASK_MIN_STACK_SIZE) {
-   int counter = 0;
-   while (true) {
+  int counter = 0;
+  while (true) {
 #ifdef TEST
-      xSemaphoreTake(test_cycle_start_sem);
+    xSemaphoreTake(test_cycle_start_sem);
 #endif
-      run_fast_cycle();
-      if (!(counter%10)) run_medium_cycle();
-      if (!(counter%100)) run_slow_cycle();
+    run_fast_cycle();
+    if (!(counter % 10)) run_medium_cycle();
+    if (!(counter % 100)) run_slow_cycle();
 
 #ifdef TEST
-      xSemaphoreGive(test_cycle_end_sem);
+    xSemaphoreGive(test_cycle_end_sem);
 #endif
-      vTaskDelay(pdMS_TO_TICKS(100));
-      ++counter;
-    }
+    vTaskDelay(pdMS_TO_TICKS(100));
+    ++counter;
+  }
 }
 
 int main() {
-   tasks_init();
-   log_init();
+  tasks_init();
+  log_init();
 
-   LOG_DEBUG("Welcome to CAN!\n");
-   init_pedal_controls();
-   can_init(&s_can_storage, &can_settings);
-   can_add_filter_in(SYSTEM_CAN_MESSAGE_PEDAL_PEDAL_OUTPUT);
+  LOG_DEBUG("Welcome to CAN!\n");
+  init_pedal_controls();
+  can_init(&s_can_storage, &can_settings);
+  can_add_filter_in(SYSTEM_CAN_MESSAGE_PEDAL_PEDAL_OUTPUT);
 
-   tasks_init_task(master_task, TASK_PRIORITY(2), NULL);
-   tasks_start();
-   LOG_DEBUG("exiting main?\n");
-   return 0;
+  tasks_init_task(master_task, TASK_PRIORITY(2), NULL);
+  tasks_start();
+  LOG_DEBUG("exiting main?\n");
+  return 0;
 }
-
