b37c2dba2649c26547eb6ba3aadbfed215f22fe7
Adhiraj <adhiraj.bal@gmail.com>
2022-11-06 22:34:24 +0000

    used standard new project template for motor controller main task

--- projects/motor_controller/src/main.c
+++ projects/motor_controller/src/main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "can.h"
 #include "can_board_ids.h"
 #include "delay.h"
@@ -8,6 +10,12 @@
 #include "soft_timer.h"
 #include "tasks.h"
 
+#ifdef MS_PLATFORM_X86
+#define MASTER_MS_CYCLE_TIME 100
+#else
+#define MASTER_MS_CYCLE_TIME 1000
+#endif
+
 static CanStorage s_can_storage = { 0 };
 const CanSettings can_settings = {
   .device_id = 0x1,
@@ -17,24 +25,34 @@ const CanSettings can_settings = {
   .loopback = true,
 };
 
-TASK(master_task, TASK_STACK_512) {
+void run_fast_cycle() {}
+
+void run_medium_cycle() {}
+
+void run_slow_cycle() {}
+
+TASK(master_task, TASK_MIN_STACK_SIZE) {
+  int counter = 0;
   while (true) {
-    fsm_run_cycle(mci_fsm);
-    delay_ms(250);
+    run_fast_cycle();
+    if (!(counter % 10)) run_medium_cycle();
+    if (!(counter % 100)) run_slow_cycle();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    ++counter;
   }
 }
 
-int main(void) {
+int main() {
   tasks_init();
   log_init();
-
   can_init(&s_can_storage, &can_settings);
-
   init_mci_fsm();
+  LOG_DEBUG("Motor Controller Task\n");
+
   tasks_init_task(master_task, TASK_PRIORITY(2), NULL);
 
-  LOG_DEBUG("Motor Controller Task...\n");
   tasks_start();
 
+  LOG_DEBUG("exiting main?");
   return 0;
 }
