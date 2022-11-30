36c99bbd1ef536054b2e771fde6d879f864625f3
Mitchell Ostler <mlostler@uwaterloo.ca>
2022-11-30 05:38:20 +0000

    i guess autogen doesn't format?

--- projects/power_distribution/src/main.c
+++ projects/power_distribution/src/main.c
@@ -9,44 +9,32 @@
 #define MASTER_MS_CYCLE_TIME 1000
 #endif
 
-void run_fast_cycle()
-{
+void run_fast_cycle() {}
 
-}
+void run_medium_cycle() {}
 
-void run_medium_cycle()
-{
-
-}
-
-void run_slow_cycle()
-{
-
-}
+void run_slow_cycle() {}
 
 TASK(master_task, TASK_MIN_STACK_SIZE) {
-   int counter = 0;
-   while (true) {
-      run_fast_cycle();
-      if (!(counter%10))
-         run_medium_cycle();
-      if (!(counter%100))
-         run_slow_cycle();
-      vTaskDelay(pdMS_TO_TICKS(1000));
-      ++counter;
-    }
+  int counter = 0;
+  while (true) {
+    run_fast_cycle();
+    if (!(counter % 10)) run_medium_cycle();
+    if (!(counter % 100)) run_slow_cycle();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    ++counter;
+  }
 }
 
 int main() {
-   tasks_init();
-   log_init();
-   LOG_DEBUG("Welcome to TEST!");
+  tasks_init();
+  log_init();
+  LOG_DEBUG("Welcome to TEST!");
 
-   tasks_init_task(master_task, TASK_PRIORITY(2), NULL);
+  tasks_init_task(master_task, TASK_PRIORITY(2), NULL);
 
-   tasks_start();
+  tasks_start();
 
-   LOG_DEBUG("exiting main?");
-   return 0;
+  LOG_DEBUG("exiting main?");
+  return 0;
 }
-
