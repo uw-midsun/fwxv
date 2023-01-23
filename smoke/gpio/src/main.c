65ae7a9c9d54d81ca99d18117dd0e74b6f870e01
AFailure <89050937+AFailure@users.noreply.github.com>
2023-01-23 17:15:52 -0500

    Second commit

--- smoke/gpio/src/main.c
+++ smoke/gpio/src/main.c
@@ -3,10 +3,141 @@
 
 #include "delay.h"
 #include "gpio.h"
+#include "gpio_it.h"
 #include "log.h"
 #include "misc.h"
-#include "smoke_gpio.h"
 #include "tasks.h"
+
+static const GpioAddress smoke_gpio[] = {
+  {
+      .port = GPIO_PORT_A,
+      .pin = 0,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 1,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 2,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 3,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 4,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 5,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 6,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 7,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 8,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 9,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 10,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 11,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 12,
+  },
+  {
+      .port = GPIO_PORT_A,
+      .pin = 15,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 0,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 1,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 3,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 4,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 5,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 6,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 7,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 8,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 9,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 10,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 11,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 12,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 13,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 14,
+  },
+  {
+      .port = GPIO_PORT_B,
+      .pin = 15,
+  },
+  {
+      .port = GPIO_PORT_C,
+      .pin = 13,
+  },
+  {
+      .port = GPIO_PORT_C,
+      .pin = 14,
+  },
+  {
+      .port = GPIO_PORT_C,
+      .pin = 15,
+  },
+};
 
 #ifdef MS_PLATFORM_X86
 #define MASTER_MS_CYCLE_TIME 100
@@ -36,6 +167,6 @@ int main(void) {
 
   tasks_start();
 
-  LOG_DEBUG("Pins Toggled\n");
+  LOG_DEBUG("Tasks Ended\n");
   return 0;
 }
