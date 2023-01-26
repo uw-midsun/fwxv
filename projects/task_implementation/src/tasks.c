cee6f8c37278ff19dabd61e0adc050766ca08a20
Kevin Song <kevin.kwsong@gmail.com>
2023-01-26 04:05:00 +0000

    Complete task 1

---
+++ projects/task_implementation/src/tasks.c
@@ -0,0 +1,50 @@
+#include "tasks.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "delay.h"
+#include "gpio.h"
+#include "log.h"
+#include "misc.h"
+
+// Non blocking delay. Simply consumes cpu cycles until a given time has passed
+static void prv_delay(const TickType_t delay_ms) {
+  TickType_t curr_tick = xTaskGetTickCount();
+  while (xTaskGetTickCount() - curr_tick < pdMS_TO_TICKS(delay_ms)) {
+  }
+}
+
+TASK(task1, TASK_STACK_512) {
+  int counter1 = 0;
+  while (true) {
+    // Your code here
+    LOG_DEBUG("%s %d \n", pcTaskGetName(NULL), counter1);
+    counter1++;
+    prv_delay(1000);
+  }
+}
+
+TASK(task2, TASK_STACK_512) {
+  int counter2 = 0;
+  while (true) {
+    // Your code here
+    LOG_DEBUG("%s %d \n", pcTaskGetName(NULL), counter2);
+    counter2++;
+    vTaskDelay(1000);
+  }
+}
+
+int main(void) {
+  log_init();
+  tasks_init();
+  // Create tasks here
+  tasks_init_task(task1, TASK_PRIORITY(0), NULL);
+  tasks_init_task(task2, TASK_PRIORITY(1), NULL);
+  LOG_DEBUG("Program start...\n");
+  // Start the scheduler
+  tasks_start();
+
+  return 0;
+}
