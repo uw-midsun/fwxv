66af4fbd62589629514090ec65d52fca0308c0f8
ShiCheng Lu <shichenglu0708@gmail.com>
2023-02-10 22:03:54 +0000

    fix data location mask

--- testjinja.py
+++ testjinja.py
@@ -1,11 +1,7 @@
 import jinja2
 
 x = jinja2.Template("""
-{{ '0x%0x' % ('1' * 8 + '0' * 8 + '1' * (64 - 8 - 8)) | int(base=2) }}
-
-asldfkjwef {#- comment -#}
-saldfkjwlea {#- comment -#}
-lkerew
+{{ '0x%016x' % ('1' * 0 + '0' * 8 + '1' * (64 - 8 - 8)) | int(base=2) }}
 """)
 
 print(x.render())
