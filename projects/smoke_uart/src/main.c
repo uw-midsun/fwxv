0c1d08f04b9fd300b9aab87f0bdcdc516d5d6881
Kevin Song <kevin.kwsong@gmail.com>
2023-02-11 00:43:22 +0000

    Trying to get uart_init to work

--- projects/smoke_uart/src/main.c
+++ projects/smoke_uart/src/main.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 
-#include "delay.h"
-#include "log.h"
-#include "misc.h"
+#include "gpio_it.h"
+#include "interrupt.h"
 #include "tasks.h"
 #include "uart.h"
-// const UartPort UART_PORT;
 
-UartSettings smoke_settings = { .tx = { .port = GPIO_PORT_A, .pin = 9 },
-                                .rx = { .port = GPIO_PORT_A, .pin = 10 },
-                                .baudrate = 9600 };
+static GpioAddress gpios[] = { { .port = GPIO_PORT_A, .pin = 10 },
+                               { .port = GPIO_PORT_A, .pin = 11 } };
+
 TASK(master_task, TASK_MIN_STACK_SIZE) {
-  uart_init(UART_PORT_2, &smoke_settings);
-  while (true) {
-    vTaskDelay(pdMS_TO_TICKS(1000));
-  }
+  // while (true) {
+  //   vTaskDelay(pdMS_TO_TICKS(1000));
+  // }
 }
 
 int main() {
-  tasks_init();
-  log_init();
-  LOG_DEBUG("Welcome to TEST!");
+  gpio_init();
+  gpio_it_init();
+  interrupt_init();
 
-  tasks_init_task(master_task, TASK_PRIORITY(2), NULL);
+  gpio_init_pin(&gpios[0], GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_HIGH);
+  gpio_init_pin(&gpios[1], GPIO_INPUT_PULL_DOWN, GPIO_STATE_HIGH);
+
+  UartSettings smoke_settings = { .tx = gpios[0], .rx = gpios[1], .baudrate = 9600 };
+  uart_init(UART_PORT_2, &smoke_settings);
+
+  tasks_init();
+
+  tasks_init_task(master_task, TASK_PRIORITY(0), NULL);
 
   tasks_start();
 
-  LOG_DEBUG("exiting main?");
   return 0;
 }
