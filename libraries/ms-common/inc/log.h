f7072dec0a6c9083aa2c5c37a3d3f43adc5db2b0
Bafran <48925842+Bafran@users.noreply.github.com>
2023-02-15 19:47:30 -0500

    Change queues to UART (#84)
    * Change queues to UART
    
    * Remove log.c
    
    * Changed memory for mutex and buffer
    
    * Format
    
    * Remove printf
    
    ---------
    
    Co-authored-by: vagrant <vagrant@midsunbox>

--- libraries/ms-common/inc/log.h
+++ libraries/ms-common/inc/log.h
@@ -4,9 +4,10 @@
 #include <stdlib.h>
 
 #include "FreeRTOS.h"
-#include "queue.h"
+#include "semaphore.h"
 #include "task.h"
 #include "tasks.h"
+#include "uart.h"
 
 // NOTE: Log debug cannot be used in core
 // NOTE: Please use %lli instead of %li
@@ -14,13 +15,15 @@
 DECLARE_TASK(log_task);
 
 #define MAX_LOG_SIZE (size_t)200
+#define LOG_TIMEOUT_MS 10
+
+#define UARTPORT UART_PORT_1
+#define TX_PIN 6
+#define RX_PIN 7
 
 #ifndef IN_ORDER_LOGS
-#define IN_ORDER_LOGS 0
+#define IN_ORDER_LOGS 1
 #endif
-
-// Global queue handle used in all log calls
-extern QueueHandle_t g_log_queue;
 
 typedef enum {
   LOG_LEVEL_DEBUG = 0,
@@ -29,44 +32,29 @@ typedef enum {
   NUM_LOG_LEVELS,
 } LogLevel;
 
-void log_init(void);
+extern char g_log_buffer[MAX_LOG_SIZE];
+extern Mutex s_log_mutex;
 
 #define LOG_DEBUG(fmt, ...) LOG(LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
 #define LOG_WARN(fmt, ...) LOG(LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
-#define LOG_CRITICAL(fmt, ...) LOG_C(LOG_LEVEL_CRITICAL, fmt, ##__VA_ARGS__)
+#define LOG_CRITICAL(fmt, ...) LOG(LOG_LEVEL_CRITICAL, fmt, ##__VA_ARGS__)
 
-#define LOG(level, fmt, ...)                                                            \
-  do {                                                                                  \
-    char log_buffer[MAX_LOG_SIZE];                                                      \
-    snprintf(log_buffer, MAX_LOG_SIZE, "[%u] %s:%u: " fmt, (level), __FILE__, __LINE__, \
-             ##__VA_ARGS__);                                                            \
-    if (xTaskGetSchedulerState() != taskSCHEDULER_RUNNING) {                            \
-      printf("%s", log_buffer);                                                         \
-    } else {                                                                            \
-      if (g_log_queue == NULL) {                                                        \
-        printf("Error: Logs init not initialized \n");                                  \
-        vTaskEndScheduler();                                                            \
-      }                                                                                 \
-      xQueueSendToBack(g_log_queue, log_buffer, 0);                                     \
-    }                                                                                   \
-  } while (0)
+#define log_init() mutex_init(&s_log_mutex)
 
-#define LOG_C(level, fmt, ...)                                                          \
-  do {                                                                                  \
-    char log_buffer[MAX_LOG_SIZE];                                                      \
-    snprintf(log_buffer, MAX_LOG_SIZE, "[%u] %s:%u: " fmt, (level), __FILE__, __LINE__, \
-             ##__VA_ARGS__);                                                            \
-    if (xTaskGetSchedulerState() != taskSCHEDULER_RUNNING) {                            \
-      printf("%s", log_buffer);                                                         \
-    } else {                                                                            \
-      if (g_log_queue == NULL) {                                                        \
-        printf("Error: Logs init not initialized \n");                                  \
-        vTaskEndScheduler();                                                            \
-      }                                                                                 \
-      if (IN_ORDER_LOGS) {                                                              \
-        xQueueSendToBack(g_log_queue, log_buffer, 0);                                   \
-      } else {                                                                          \
-        xQueueSendToFront(g_log_queue, log_buffer, 0);                                  \
-      }                                                                                 \
-    }                                                                                   \
+#ifdef MS_PLATFORM_X86
+#define LOG(level, fmt, ...) printf("[%u] %s:%u: " fmt, (level), __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define LOG(level, fmt, ...)                                                                      \
+  do {                                                                                            \
+    if (mutex_lock(&s_log_mutex, LOG_TIMEOUT_MS) == STATUS_CODE_OK) {                             \
+      size_t msg_size = (size_t)snprintf(g_log_buffer, MAX_LOG_SIZE, "[%u] %s:%u: " fmt, (level), \
+                                         __FILE__, __LINE__, ##__VA_ARGS__);                      \
+      if (xTaskGetSchedulerState() != taskSCHEDULER_RUNNING) {                                    \
+        printf("%s", g_log_buffer);                                                               \
+      } else {                                                                                    \
+        uart_tx(UARTPORT, (uint8_t *)g_log_buffer, &msg_size);                                    \
+      }                                                                                           \
+      mutex_unlock(&s_log_mutex);                                                                 \
+    }                                                                                             \
   } while (0)
+#endif
