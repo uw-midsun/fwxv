13ba39c236b3d67b0d5bb8af79638628b116d9e9
Kyle Chan <chankyle9@gmail.com>
2023-02-18 15:06:00 -0500

    Fixed LEDs and log

--- libraries/ms-common/src/log.c
+++ libraries/ms-common/src/log.c
@@ -3,3 +3,9 @@
 // Allocating memory for extern variables in .h files
 char g_log_buffer[MAX_LOG_SIZE];
 Mutex s_log_mutex;
+
+UartSettings log_uart_settings = {
+  .tx = {GPIO_PORT_A, TX_PIN},  // tx pin
+  .rx = {GPIO_PORT_A, RX_PIN},  // rx pin
+  .baudrate = 32000
+};
