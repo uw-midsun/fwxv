c34d8c5336b3746f0ddc20ca35f04f74d273226a
Kevin Song <kevin.kwsong@gmail.com>
2023-02-19 18:45:42 +0000

    Committing for review

--- projects/smoke_uart/src/main.c
+++ projects/smoke_uart/src/main.c
@@ -1,38 +1,62 @@
 #include <stdio.h>
 
-#include "delay.h"
-#include "gpio_it.h"
 #include "interrupt.h"
 #include "log.h"
-#include "misc.h"
-#include "soft_timer.h"
 #include "tasks.h"
 #include "uart.h"
 
+#define SHOULD_TRANSMIT true
+#define SHOULD_READ true
+
+#define UART_PORT_SEND UART_PORT_1
+#define UART_PORT_RECEIVE UART_PORT_2
+
+static UartSettings uart1_settings = { .tx = { .port = GPIO_PORT_A, .pin = 10 },
+                                       .rx = { .port = GPIO_PORT_A, .pin = 11 },
+                                       .baudrate = 9600 };
+
+static UartSettings uart2_settings = { .tx = { .port = GPIO_PORT_B, .pin = 4 },
+                                       .rx = { .port = GPIO_PORT_B, .pin = 5 },
+                                       .baudrate = 9600 };
 TASK(master_task, TASK_MIN_STACK_SIZE) {
-  // while (true) {
-  //   vTaskDelay(pdMS_TO_TICKS(1000));
-  // }
+  uart_init(UART_PORT_SEND, &uart1_settings);
+  uart_init(UART_PORT_RECEIVE, &uart2_settings);
+
+  uint8_t data_received = '1';
+  uint8_t data_sent = '1';
+  size_t len = sizeof(uint8_t);
+
+  while (true) {
+    StatusCode status;
+    if (SHOULD_TRANSMIT) {
+      status = uart_tx(UART_PORT_SEND, &data_sent, &len);
+      if (status == STATUS_CODE_OK) {
+        LOG_DEBUG("Successfully transmitted %u\n", data_sent);
+      } else {
+        LOG_DEBUG("Write failed: status code %d\n", status);
+      }
+    }
+    if (SHOULD_READ) {
+      status = uart_rex(UART_PORT_RECEIVE, &data_received, &len);
+      if (status == STATUS_CODE_OK) {
+        LOG_DEBUG("Successfully received %u\n", data_received);
+      } else {
+        LOG_DEBUG("Write failed: status code %d\n", status);
+      }
+    }
+  }
 }
 
 int main() {
   log_init();
   gpio_init();
-  gpio_it_init();
   interrupt_init();
-  UartSettings smoke_settings = { .tx = { .port = GPIO_PORT_A, .pin = 10 },
-                                  .rx = { .port = GPIO_PORT_A, .pin = 11 },
-                                  .baudrate = 9600 };
-  uart_init(UART_PORT_2, &smoke_settings);
 
   tasks_init();
 
-  LOG_DEBUG("Welcome to TEST!");
-
-  tasks_init_task(master_task, TASK_PRIORITY(0), NULL);
+  tasks_init_task(master_task, TASK_PRIORITY(1), NULL);
 
   tasks_start();
 
-  LOG_DEBUG("exiting main?");
   return 0;
 }
