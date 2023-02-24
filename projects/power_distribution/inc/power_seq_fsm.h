5071975fe87b9f2caaaca7f47b31a0edcbfaf89d
Harsh Patel <its.hp3@gmail.com>
2023-02-24 03:23:34 +0000

    set up power_seq_fsm files

---
+++ projects/power_distribution/inc/power_seq_fsm.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "can.h"
+#include "delay.h"
+#include "fsm.h"
+#include "gpio.h"
+#include "gpio_it.h"
+#include "log.h"
+#include "soft_timer.h"
+#include "status.h"
+#include "task.h"
+
+#define NUM_POWER_SEQ_STATES 5
+#define NUM_POWER_SEQ_TRANSITIONS 9
+
+DECLARE_FSM(power_seq);
+
+typedef enum PowerSeqStateId {
+  POWER_SEQ_INIT_STATE = 0,
+  POWER_SEQ_EVERYTHING_ON,
+  POWER_SEQ_DRIVER_CONTROLS,
+  POWER_SEQ_MAIN_OPERATION,
+  POWER_SEQ_AUX_POWER
+} PowerSeqStateId;
+
+StatusCode init_power_seq(void);
---
+++ projects/power_distribution/src/power_seq_fsm.c
@@ -0,0 +1,68 @@
+#include "power_seq_fsm.h"
+
+FSM(power_seq, NUM_POWER_SEQ_STATES);
+
+static void prv_init_state_input(Fsm *fsm, void *context) {}
+
+static void prv_init_state_output(void *context) {
+  LOG_DEBUG("Transitioned to INIT_STATE");
+}
+
+static void prv_everything_on_input(Fsm *fsm, void *context) {
+  fsm_transition(fsm, POWER_SEQ_MAIN_OPERATION);
+}
+
+static void prv_everything_on_output(void *context) {
+  LOG_DEBUG("Transitioned to EVERYTHING_ON");
+}
+
+static void prv_driver_controls_input(Fsm *fsm, void *context) {}
+
+static void prv_driver_controls_output(void *context) {
+  LOG_DEBUG("Transitioned to DRIVER_CONTROLS");
+}
+
+static void prv_main_operation_input(Fsm *fsm, void *context) {}
+
+static void prv_main_operation_output(void *context) {
+  LOG_DEBUG("Transitioned to MAIN_OPERATIONS");
+}
+
+static void prv_aux_power_input(Fsm *fsm, void *context) {}
+
+static void prv_aux_power_output(void *context) {
+  LOG_DEBUG("Transitioned to AUX_POWER");
+}
+
+// Power Sequence FSM declaration for states and transitions
+static FsmState s_power_seq_state_list[NUM_POWER_SEQ_STATES] = {
+  STATE(POWER_SEQ_INIT_STATE, prv_init_state_input, prv_init_state_output),
+  STATE(POWER_SEQ_EVERYTHING_ON, prv_everything_on_input, prv_everything_on_output),
+  STATE(POWER_SEQ_DRIVER_CONTROLS, prv_driver_controls_input, prv_driver_controls_output),
+  STATE(POWER_SEQ_MAIN_OPERATION, prv_main_operation_input, prv_main_operation_output),
+  STATE(POWER_SEQ_AUX_POWER, prv_aux_power_input, prv_aux_power_output),
+};
+
+static FsmTransition s_power_seq_transition_list[NUM_POWER_SEQ_TRANSITIONS] = {
+  TRANSITION(POWER_SEQ_INIT_STATE, POWER_SEQ_EVERYTHING_ON),
+  TRANSITION(POWER_SEQ_INIT_STATE, POWER_SEQ_DRIVER_CONTROLS),
+  TRANSITION(POWER_SEQ_EVERYTHING_ON, POWER_SEQ_MAIN_OPERATION),
+  TRANSITION(POWER_SEQ_DRIVER_CONTROLS, POWER_SEQ_INIT_STATE),
+  TRANSITION(POWER_SEQ_DRIVER_CONTROLS, POWER_SEQ_EVERYTHING_ON),
+  TRANSITION(POWER_SEQ_MAIN_OPERATION, POWER_SEQ_INIT_STATE),
+  TRANSITION(POWER_SEQ_MAIN_OPERATION, POWER_SEQ_AUX_POWER),
+  TRANSITION(POWER_SEQ_MAIN_OPERATION, POWER_SEQ_MAIN_OPERATION),
+  TRANSITION(POWER_SEQ_AUX_POWER, POWER_SEQ_INIT_STATE),
+};
+
+StatusCode init_power_seq(void) {
+  const FsmSettings power_seq_settings = {
+    .state_list = s_power_seq_state_list,
+    .transitions = s_power_seq_transition_list,
+    .num_transitions = NUM_POWER_SEQ_TRANSITIONS,
+    .initial_state = POWER_SEQ_INIT_STATE,
+  };
+
+  fsm_init(power_seq, power_seq_settings, NULL);
+  return STATUS_CODE_OK;
+}
