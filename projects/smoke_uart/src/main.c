a9c08ee6b6fbb9c70a9b813d7673a9671aea34d0
Kevin Song <kevin.kwsong@gmail.com>
2023-03-05 01:14:07 +0000

    made changes as requested

--- projects/smoke_uart/src/main.c
+++ projects/smoke_uart/src/main.c
@@ -1,49 +1,31 @@
 #include <stdio.h>
 
+#include "delay.h"
 #include "interrupt.h"
 #include "log.h"
 #include "tasks.h"
 #include "uart.h"
 
-#define SHOULD_TRANSMIT true
-#define SHOULD_READ true
+static const char test[] = "test uart\n";
 
-#define UART_PORT_SEND UART_PORT_1
-#define UART_PORT_RECEIVE UART_PORT_2
+static UartSettings uart_settings = { .tx = { .port = GPIO_PORT_A, .pin = 10 },
+                                      .rx = { .port = GPIO_PORT_A, .pin = 11 },
+                                      .baudrate = 9600 };
 
-static UartSettings uart1_settings = { .tx = { .port = GPIO_PORT_A, .pin = 10 },
-                                       .rx = { .port = GPIO_PORT_A, .pin = 11 },
-                                       .baudrate = 9600 };
-
-static UartSettings uart2_settings = { .tx = { .port = GPIO_PORT_B, .pin = 4 },
-                                       .rx = { .port = GPIO_PORT_B, .pin = 5 },
-                                       .baudrate = 9600 };
 TASK(master_task, TASK_MIN_STACK_SIZE) {
-  uart_init(UART_PORT_SEND, &uart1_settings);
-  uart_init(UART_PORT_RECEIVE, &uart2_settings);
+  uart_init(UART_PORT_1, &uart_settings);
 
-  uint8_t data_received = '1';
-  uint8_t data_sent = '1';
   size_t len = sizeof(uint8_t);
 
   while (true) {
     StatusCode status;
-    if (SHOULD_TRANSMIT) {
-      status = uart_tx(UART_PORT_SEND, &data_sent, &len);
-      if (status == STATUS_CODE_OK) {
-        LOG_DEBUG("Successfully transmitted %u\n", data_sent);
-      } else {
-        LOG_DEBUG("Write failed: status code %d\n", status);
-      }
+    status = uart_tx(UART_PORT_1, test, &len);
+    if (status == STATUS_CODE_OK) {
+      LOG_DEBUG("Successfully transmitted %s\n", test);
+    } else {
+      LOG_DEBUG("Write failed: status code %d\n", status);
     }
-    if (SHOULD_READ) {
-      status = uart_rx(UART_PORT_RECEIVE, &data_received, &len);
-      if (status == STATUS_CODE_OK) {
-        LOG_DEBUG("Successfully received %u\n", data_received);
-      } else {
-        LOG_DEBUG("Write failed: status code %d\n", status);
-      }
-    }
+    delay_ms(1000);
   }
 }
 
