1082239dc9162b12d3e3ad87087ceb9976c8d870
AFailure <89050937+AFailure@users.noreply.github.com>
2023-03-11 16:17:08 -0500

    Small fixes

--- projects/steering/test/test_steering_analog.c
+++ projects/steering/test/test_steering_analog.c
@@ -14,7 +14,7 @@ static bool s_task_started;
 // Gpio Address can be changed to different values for testing
 static const GpioAddress s_ctrl_stk_address = CTRL_STALK_GPIO;
 uint16_t control_stalk_data = UINT16_MAX;
-uint16_t control_stalk_data_verify = UINT16_MAX;
+uint16_t control_stalk_data_actual = UINT16_MAX;
 
 void setup_test(void) {
   s_task_started = false;
@@ -27,7 +27,7 @@ TASK(steering_analog_input_task, TASK_STACK_512) {
   LOG_DEBUG("steering_analog_input started\n");
   s_task_started = true;
   set_reading(s_ctrl_stk_address);
-  control_stalk_data_verify = get_reading(s_ctrl_stk_address);
+  control_stalk_data_actual = get_reading(s_ctrl_stk_address);
   steering_analog_input();
   while (true) {
   }
@@ -36,30 +36,32 @@ TASK(steering_analog_input_task, TASK_STACK_512) {
 TEST_IN_TASK
 void test_steering_analog(void) {
   adc_init(ADC_MODE_SINGLE);
+  control_stalk_data = STEERING_CONTROL_STALK_LEFT_SIGNAL_VOLTAGE_MV;
   tasks_init_task(steering_analog_input_task, TASK_PRIORITY(1), NULL);
   TEST_ASSERT_FALSE(s_task_started);
 
   delay_ms(20);
 
   TEST_ASSERT_TRUE(s_task_started);
+  TEST_ASSERT_EQUAL(g_tx_struct.steering_info_analog_input, STEERING_LIGHT_LEFT);
+  s_task_started = false;
 
-  // Since the ADC read converted is from a variable GPIO address pin, these values are not going to
-  // change from their original values, but if the pin is being used for testing, the next two lines
-  // can be uncommented
+  control_stalk_data = STEERING_CONTROL_STALK_RIGHT_SIGNAL_VOLTAGE_MV;
+  tasks_init_task(steering_analog_input_task, TASK_PRIORITY(1), NULL);
+  TEST_ASSERT_FALSE(s_task_started);
 
-  // TEST_ASSERT_NOT_EQUAL(control_stalk_data, UINT16_MAX);
-  // TEST_ASSERT_NOT_EQUAL(control_stalk_data_verify, UINT16_MAX);
-  TEST_ASSERT_EQUAL(control_stalk_data, control_stalk_data_verify);
+  delay_ms(20);
 
-  if (control_stalk_data > STEERING_CONTROL_STALK_LEFT_SIGNAL_VOLTAGE_MV - VOLTAGE_TOLERANCE_MV &&
-      control_stalk_data < STEERING_CONTROL_STALK_LEFT_SIGNAL_VOLTAGE_MV + VOLTAGE_TOLERANCE_MV) {
-    TEST_ASSERT_EQUAL(g_tx_struct.steering_info_analog_input, STEERING_LIGHT_LEFT);
-  } else if (control_stalk_data >
-                 STEERING_CONTROL_STALK_RIGHT_SIGNAL_VOLTAGE_MV - VOLTAGE_TOLERANCE_MV &&
-             control_stalk_data <
-                 STEERING_CONTROL_STALK_RIGHT_SIGNAL_VOLTAGE_MV + VOLTAGE_TOLERANCE_MV) {
-    TEST_ASSERT_EQUAL(g_tx_struct.steering_info_analog_input, STEERING_LIGHT_RIGHT);
-  } else {
-    TEST_ASSERT_EQUAL(g_tx_struct.steering_info_analog_input, STEERING_LIGHT_OFF);
-  }
+  TEST_ASSERT_TRUE(s_task_started);
+  TEST_ASSERT_EQUAL(g_tx_struct.steering_info_analog_input, STEERING_LIGHT_RIGHT);
+  s_task_started = false;
+
+  control_stalk_data = 0;
+  tasks_init_task(steering_analog_input_task, TASK_PRIORITY(1), NULL);
+  TEST_ASSERT_FALSE(s_task_started);
+
+  delay_ms(20);
+
+  TEST_ASSERT_TRUE(s_task_started);
+  TEST_ASSERT_EQUAL(g_tx_struct.steering_info_analog_input, STEERING_LIGHT_OFF);
 }
