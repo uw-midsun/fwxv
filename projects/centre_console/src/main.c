a0a2e6b9f95f958bf93723b6f1beb705b42a1a07
Kyle Chan <chankyle9@gmail.com>
2023-03-15 00:05:56 -0400

    Trying to fix main.c fsm for centre console

--- projects/centre_console/src/main.c
+++ projects/centre_console/src/main.c
@@ -27,9 +27,20 @@ void run_medium_cycle() {
 
 void run_slow_cycle() {}
 
+TASK(master_task, TASK_MIN_STACK_SIZE) {
+  int counter = 0;
+  while (true) {
+    run_fast_cycle();
+    if (!(counter % 10)) run_medium_cycle();
+    if (!(counter % 100)) run_slow_cycle();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    ++counter;
+  }
+}
+
 int main() {
-  tasks_init();
   log_init();
+  tasks_init();
   LOG_DEBUG("Welcome to TEST! \n");
 
   init_drive_fsm();
