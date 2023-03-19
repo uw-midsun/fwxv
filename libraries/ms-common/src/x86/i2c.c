81cdc93417383ba130f34227928fd7dffef1d12d
devAdhiraj <adhiraj.bal@gmail.com>
2023-03-18 23:44:47 -0400

    add getters and setters

--- libraries/ms-common/src/x86/i2c.c
+++ libraries/ms-common/src/x86/i2c.c
@@ -1,7 +1,17 @@
 #include "i2c.h"
 
+#include <stdbool.h>
+
 #include "log.h"
+#include "queues.h"
+#include "semaphore.h"
 #include "stdio.h"
+
+typedef enum I2CMode {
+  I2C_MODE_TRANSMIT = 0,
+  I2C_MODE_RECEIVE,
+  NUM_I2C_MODES,
+} I2CMode;
 
 // Used to protect I2C resources
 // All data tx'd and rx'd through queue
@@ -12,17 +22,13 @@ typedef struct {
   Mutex mutex;
 } I2CBuffer;
 
-typedef enum I2CMode {
-  I2C_MODE_TRANSMIT = 0,
-  I2C_MODE_RECEIVE,
-  NUM_I2C_MODES,
-} I2CMode;
-
 typedef struct {
-  I2CSettings settings;     // I2C Config
-  I2CBuffer i2c_buf;        // RTOS data structures
-  bool curr_mode;           // Mode used in current transxn
-  I2CAddress current_addr;  // Addr used in current transxn
+  I2CSettings settings;           // I2C Config
+  I2CBuffer i2c_tx_buf;           // RTOS data structures
+  I2CBuffer i2c_rx_buf;           // RTOS data structures
+  bool curr_mode;                 // Mode used in current transxn
+  I2CAddress current_addr;        // Addr used in current transxn
+  volatile uint8_t num_rx_bytes;  // Number of bytes left to receive in rx mode
 } I2CPortData;
 
 static I2CPortData s_port[NUM_I2C_PORTS] = {
@@ -37,27 +43,59 @@ StatusCode i2c_init(I2CPort i2c, const I2CSettings *settings) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C speed.");
   }
 
-  s_port[i2c].i2c_buf.queue.num_items = I2C_MAX_NUM_DATA;
-  s_port[i2c].i2c_buf.queue.item_size = sizeof(uint8_t);
-  s_port[i2c].i2c_buf.queue.storage_buf = s_port[i2c].i2c_buf.buf;
-  status_ok_or_return(queue_init(&s_port[i2c].i2c_buf.queue));
+  s_port[i2c].settings = *settings;
+
+  s_port[i2c].i2c_rx_buf.queue.num_items = I2C_MAX_NUM_DATA;
+  s_port[i2c].i2c_rx_buf.queue.item_size = sizeof(uint8_t);
+  s_port[i2c].i2c_rx_buf.queue.storage_buf = s_port[i2c].i2c_rx_buf.buf;
+  status_ok_or_return(queue_init(&s_port[i2c].i2c_rx_buf.queue));
+
+  s_port[i2c].i2c_tx_buf.queue.num_items = I2C_MAX_NUM_DATA;
+  s_port[i2c].i2c_tx_buf.queue.item_size = sizeof(uint8_t);
+  s_port[i2c].i2c_tx_buf.queue.storage_buf = s_port[i2c].i2c_tx_buf.buf;
+  status_ok_or_return(queue_init(&s_port[i2c].i2c_tx_buf.queue));
 
   return STATUS_CODE_OK;
 }
+
+#ifdef MS_PLATFORM_X86
+StatusCode i2c_set_data(I2CPort i2c, uint8_t *tx_data, size_t tx_len) {
+  for (size_t tx = 0; tx < tx_len; tx++) {
+    if (queue_send(&s_port[i2c].i2c_rx_buf.queue, &tx_data[tx], 0)) {
+      queue_reset(&s_port[i2c].i2c_rx_buf.queue);
+      return STATUS_CODE_RESOURCE_EXHAUSTED;
+    }
+  }
+  return STATUS_CODE_OK;
+}
+
+StatusCode i2c_get_data(I2CPort i2c, uint8_t *rx_data, size_t rx_len) {
+  for (size_t rx = 0; rx < rx_len; rx++) {
+    if (queue_receive(&s_port[i2c].i2c_tx_buf.queue, &rx_data[rx], 0)) {
+      queue_reset(&s_port[i2c].i2c_tx_buf.queue);
+      return STATUS_CODE_INTERNAL_ERROR;
+    }
+  }
+  return STATUS_CODE_OK;
+}
+#endif
 
 StatusCode i2c_read(I2CPort i2c, I2CAddress addr, uint8_t *rx_data, size_t rx_len) {
   if (i2c >= NUM_I2C_PORTS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
   }
 
+  // Set number of bytes to read
+  s_port[i2c].num_rx_bytes = rx_len;
+  // Store address for this transaction
   s_port[i2c].current_addr = addr;
   s_port[i2c].curr_mode = I2C_MODE_RECEIVE;
 
   // Receive data from queue
   // If less than requested is received, an error in the transaction has occurred
   for (size_t rx = 0; rx < rx_len; rx++) {
-    if (queue_receive(&s_port[i2c].i2c_buf.queue, &rx_data[rx], 0)) {
-      queue_reset(&s_port[i2c].i2c_buf.queue);
+    if (queue_receive(&s_port[i2c].i2c_rx_buf.queue, &rx_data[rx], 0)) {
+      queue_reset(&s_port[i2c].i2c_rx_buf.queue);
       return STATUS_CODE_INTERNAL_ERROR;
     }
   }
@@ -71,8 +109,8 @@ StatusCode i2c_write(I2CPort i2c, I2CAddress addr, uint8_t *tx_data, size_t tx_l
 
   // Copy data into queue
   for (size_t tx = 0; tx < tx_len; tx++) {
-    if (queue_send(&s_port[i2c].i2c_buf.queue, &tx_data[tx], 0)) {
-      queue_reset(&s_port[i2c].i2c_buf.queue);
+    if (queue_send(&s_port[i2c].i2c_tx_buf.queue, &tx_data[tx], 0)) {
+      queue_reset(&s_port[i2c].i2c_tx_buf.queue);
       return STATUS_CODE_RESOURCE_EXHAUSTED;
     }
   }
--- libraries/ms-drivers/test/test_pca9555_gpio_expander.c
+++ libraries/ms-drivers/test/test_pca9555_gpio_expander.c
@@ -42,24 +42,25 @@ void test_gpio_set_state(void) {
   uint8_t data = MOCK_REG;
 
   // set reg to a mock state
-  status = i2c_write(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_set_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
+  // set gpio pin to HIGH
   status = pca9555_gpio_set_state(&pca9555_address, PCA9555_GPIO_STATE_HIGH);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
-  // reading the input reg address. Reg address should be 0x01 (OUTPUT0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  // checking the output reg address. Reg address should be 0x01 (OUTPUT0) for pins 0-7
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x02, data);
 
-  // reading the output reg address. Reg address should be 0x02 (OUTPUT0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  // checking the output reg address. Reg address should be 0x02 (OUTPUT0) for pins 0-7
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x02, data);
 
-  // reading reg content. Reg content should have bit 4 turned on since mock pin is pin 4.
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  // check data written to reg. Reg content should have bit 4 turned on since mock pin is pin 4.
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL((MOCK_REG | 1 << MOCK_I2C_PIN), data);
 }
@@ -70,17 +71,17 @@ void test_gpio_get_state(void) {
   uint8_t data = 1 << MOCK_I2C_PIN;
 
   // set reg to a mock state
-  status = i2c_write(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_set_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
-  // reading reg content. Reg content should be high because bit 4 is high in mock state
+  // reading the input reg content. Reg content should be high because bit 4 is high in mock state
   // and pca address pin is 4.
   status = pca9555_gpio_get_state(&pca9555_address, &state);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(PCA9555_GPIO_STATE_HIGH, state);
 
   // reading input reg address. Should match INPUT0 (0x00)
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x00, data);
 }
@@ -90,47 +91,47 @@ void test_gpio_toggle_state(void) {
   uint8_t data = 0x00;
 
   // set reg to a mock state of 0
-  status = i2c_write(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_set_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
   status = pca9555_gpio_toggle_state(&pca9555_address);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
-  // reading the input reg address. Reg address should be 0x01 (INPUT0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  // reading the output reg address. Reg address should be 0x02 (OUTPUT0) for pins 0-7
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
-  TEST_ASSERT_EQUAL(0x00, data);
+  TEST_ASSERT_EQUAL(0x02, data);
 
   // reading the output reg address. Reg address should be 0x02 (OUTPUT0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x02, data);
 
   // reading reg content. Reg content should have bit 4 turned on since mock pin is pin 4.
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(1 << MOCK_I2C_PIN, data);
 
   // set reg content to what it was after toggling state
-  status = i2c_write(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_set_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
   // toggle same pin again
   status = pca9555_gpio_toggle_state(&pca9555_address);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
-  // reading the input reg address. Reg address should be 0x00 (INPUT0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  // reading the output reg address. Reg address should be 0x02 (OUTPUT0) for pins 0-7
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
-  TEST_ASSERT_EQUAL(0x00, data);
+  TEST_ASSERT_EQUAL(0x02, data);
 
   // reading the output reg address. Reg address should be 0x02 (OUTPUT0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x02, data);
 
   // reading reg content. Reg content should be 0 since same pin got toggled again (turns off)
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x00, data);
 }
@@ -143,25 +144,25 @@ void test_gpio_init_pin() {
   uint8_t data = MOCK_REG;
 
   // set reg to a mock state
-  status = i2c_write(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_set_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
   status = pca9555_gpio_init_pin(&pca9555_address, &pca9555_in_settings);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
   // reading the io dir reg address. Reg address should be 0x06 (IODIR0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x06, data);
 
   // reading the io dir reg address. Reg address should be 0x06 (IODIR0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x06, data);
 
   // reading reg content. Reg content should have bit 4 low for input setting on since mock pin is
   // pin 4.
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(MOCK_REG | (1 << MOCK_I2C_PIN), data);
 
@@ -170,45 +171,45 @@ void test_gpio_init_pin() {
   data = MOCK_REG | (1 << MOCK_I2C_PIN);
 
   // set reg to a mock state where the pin being intialized is turned on (state HIGH)
-  status = i2c_write(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_set_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
   // set output reg to a mock state
-  status = i2c_write(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_set_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
   status = pca9555_gpio_init_pin(&pca9555_address, &pca9555_out_settings);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
 
   // reading the io dir reg address. Reg address should be 0x06 (IODIR0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x06, data);
 
   // reading the io dir reg address. Reg address should be 0x06 (IODIR0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x06, data);
 
   // reading reg content. Reg content should have bit 4 low for output setting on since mock pin is
   // pin 4.
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(MOCK_REG, data);
 
-  // reading the input reg address. Reg address should be 0x00 (OUTPUT0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  // reading the output reg address. Reg address should be 0x00 (OUTPUT0) for pins 0-7
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x02, data);
 
   // reading the output reg address. Reg address should be 0x02 (OUTPUT0) for pins 0-7
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(0x02, data);
 
   // reading reg content. Reg content should have bit 4 turned off since default is
   // PCA9555_GPIO_STATE_LOW
-  status = i2c_read(i2c_port, MOCK_I2C_ADDRESS, &data, 1);
+  status = i2c_get_data(i2c_port, &data, 1);
   TEST_ASSERT_EQUAL(STATUS_CODE_OK, status);
   TEST_ASSERT_EQUAL(MOCK_REG, data);
 }
