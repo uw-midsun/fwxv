766c786cf5e87e85a126f7f3a630079921c45c6e
Ayo Fasan <113726657+ay0fasan@users.noreply.github.com>
2023-03-20 17:55:37 -0400

    Updated code with requested changes

--- projects/pedal/test/test_pedal_data_tx.c
+++ projects/pedal/test/test_pedal_data_tx.c
@@ -11,89 +11,72 @@
 #include "log.h"
 #include "test_helpers.h"
 #include "pedal_data.h"
+#include "pedal_calib.h"
 #include "soft_timer.h"
 #include "unity.h"
 
-// Define constants for CAN message IDs, types, and DLC
-#define PEDAL_CAN_MSG_ID 18
-#define PEDAL_CAN_MSG_TYPE CAN_MSG_TYPE_DATA
-#define PEDAL_CAN_MSG_EXTENDED true
-#define PEDAL_CAN_MSG_DLC 8
+#define THROTTLE_CHANNEL MAX11600_CHANNEL_0
+#define BRAKE_CHANNEL MAX11600_CHANNEL_2
+#define EE_PEDAL_VALUE_DENOMINATOR (1 << 12)
 
-// Define constants for throttle and brake values
-#define THROTTLE_POSITION 1
-#define BRAKE_POSITION 1
+extern g_tx_struct;
 
-
-static CanStorage can_storage = { 0 };
-const CanSettings can_settings = {
-  .device_id = 0x1,
-  .bitrate = CAN_HW_BITRATE_125KBPS,
-  .tx = { GPIO_PORT_A, 12 },
-  .rx = { GPIO_PORT_A, 11 },
-  .loopback = true,
+//Mock function for pedal calibration
+PedalCalibBlob global_calib_blob_mock = {
+  .throttle_calib = { .lower_value = 0, .upper_value = 1000 },
+  .brake_calib = { .lower_value = 0, .upper_value = 1000 },
 };
 
-// Function to send a CAN message containing the throttle and brake readings
-StatusCode transmit_pedal_data(int16_t throttle_reading, int16_t brake_reading) {
-  // Create a new CAN message with the throttle and brake readings
-  CanMessage msg = {
-    .id = { .raw = PEDAL_CAN_MSG_ID },
-    .type = PEDAL_CAN_MSG_TYPE,
-    .extended = PEDAL_CAN_MSG_EXTENDED,
-    .dlc = PEDAL_CAN_MSG_DLC,
-    .data_u16 = { (uint16_t)throttle_reading, (uint16_t)brake_reading }
-  };
-
-  // Send the CAN message
-  return can_transmit(&msg);
-}
-
-// Define a static variable to hold the sent CAN message
-static CanMessage sent_msg;
-
-// Define static variables to hold the simulated throttle and brake readings
-static int16_t simulated_throttle_reading = THROTTLE_POSITION;
-static int16_t simulated_brake_reading = BRAKE_POSITION;
-
-// Mock CAN transmit function to capture the transmitted message
-StatusCode TEST_MOCK(can_transmit)(const CanMessage *msg) {
-  sent_msg = *msg;
-  return STATUS_CODE_OK;
-}
-
-// Mock function for reading throttle data from MAX11600
-StatusCode TEST_MOCK(read_throttle_data)(int16_t *position) {
-  *position = simulated_throttle_reading;
-  return STATUS_CODE_OK;
-}
-
-// Mock function for reading brake data from MAX11600
-StatusCode TEST_MOCK(read_brake_data)(int16_t *position) {
-  *position = simulated_brake_reading;
+//Mock function for max11600_read_converted
+StatusCode TEST_MOCK(max11600_read_converted)(Max11600Storage *storage) {
+  storage->channel_readings[THROTTLE_CHANNEL] = 500; //Mock throttle value
+  storage->channel_readings[BRAKE_CHANNEL] = 600;    //Mock brake value
   return STATUS_CODE_OK;
 }
 
 void setup_test(void) {
   log_init();
-  can_init(&can_storage, &can_settings);
 }
 
 void teardown_test(void) {}
 
-void test_pedal_can_tx(void) {
-  // Send the throttle and brake values over CAN
-  TEST_ASSERT_OK(transmit_pedal_data(simulated_throttle_reading, simulated_brake_reading));
+StatusCode read_pedal_data(int16_t *reading, MAX11600Channel channel) {
+  status_ok_or_return(max11600_read_converted(&s_max11600_storage));
+  int32_t range = channel == THROTTLE_CHANNEL
+                      ? global_calib_blob_mock.throttle_calib.upper_value - global_calib_blob_mock.throttle_calib.lower_value
+                      : global_calib_blob_mock.brake_calib.upper_value - global_calib_blob_mock.brake_calib.lower_value;
 
-  // Check the sent CAN message ID, type, and DLC
-  TEST_ASSERT_EQUAL_HEX32(PEDAL_CAN_MSG_ID, sent_msg.id.raw);
-  TEST_ASSERT_EQUAL_UINT8(PEDAL_CAN_MSG_TYPE, sent_msg.type);
-  TEST_ASSERT_EQUAL_UINT8(PEDAL_CAN_MSG_EXTENDED, sent_msg.extended);
-  TEST_ASSERT_EQUAL_UINT8(PEDAL_CAN_MSG_DLC, sent_msg.dlc);
+  *reading = (int16_t)s_max11600_storage.channel_readings[channel];
+  int32_t reading_upscaled = (int32_t)*reading * EE_PEDAL_VALUE_DENOMINATOR;
+  reading_upscaled *= 100;
 
-  // Check the throttle and brake signals in the CAN message
-  int32_t throttle_sent = (int32_t)(sent_msg.data_u16[0]);
-  int32_t brake_sent = (int32_t)(sent_msg.data_u16[1]);
-  TEST_ASSERT_EQUAL_INT32(simulated_throttle_reading, throttle_sent);
-  TEST_ASSERT_EQUAL_INT32(simulated_brake_reading, brake_sent);
+  if (range != 0) {
+    reading_upscaled /= (range * EE_PEDAL_VALUE_DENOMINATOR);
+    *reading = (int16_t)reading_upscaled;
+  }
+  return STATUS_CODE_OK;
+}
+
+void test_pedal_cycle(void) {
+  int16_t throttle_reading, brake_reading;
+
+  //Read throttle and brake values
+  StatusCode status_throttle = read_pedal_data(&throttle_reading, THROTTLE_CHANNEL);
+  StatusCode status_brake = read_pedal_data(&brake_reading, BRAKE_CHANNEL);
+
+  TEST_ASSERT_OK(status_throttle);
+  TEST_ASSERT_OK(status_brake);
+
+  //Use CAN setters to set throttle and brake values
+  set_pedal_output_throttle_output(throttle_reading);
+  set_pedal_output_brake_output(brake_reading);
+
+  //Transmit the data over CAN
+  can_tx_all();
+
+  //Check the correct values for the throttle and brake signals are in the g_tx_struct
+  int32_t throttle_sent = (int32_t)(g_tx_struct.pedal_output.throttle_output);
+  int32_t brake_sent = (int32_t)(g_tx_struct.pedal_output.brake_output);
+  TEST_ASSERT_EQUAL_INT32(throttle_reading, throttle_sent);
+  TEST_ASSERT_EQUAL_INT32(brake_reading, brake_sent);
 }
