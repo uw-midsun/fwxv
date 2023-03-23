ebbc2ef400e64dccf61bdabea693bb075e25f85d
Kevin Song <kevin.kwsong@gmail.com>
2023-03-23 22:32:50 +0000

    made changes as requested

--- projects/motor_controller/inc/precharge_control.h
+++ projects/motor_controller/inc/precharge_control.h
@@ -20,19 +20,10 @@ typedef struct PrechargeControlSettings {
   GpioAddress precharge_monitor2;
 } PrechargeControlSettings;
 
-typedef struct PrechargeControlStorage {
-  GpioAddress precharge_control;
-} PrechargeControlStorage;
-
-PrechargeControlStorage *test_get_storage(void);
-
-StatusCode prv_set_precharge_control(PrechargeControlStorage *storage, const GpioState state);
+StatusCode prv_set_precharge_control(GpioAddress *address, const GpioState state);
 
 StatusCode run_precharge_rx_cycle();
 
 PrechargeState get_precharge_state();
-
-void prv_populate_storage(PrechargeControlStorage *storage,
-                          const PrechargeControlSettings *settings);
 
 StatusCode precharge_control_init(const PrechargeControlSettings *settings);
--- projects/motor_controller/src/precharge_control.c
+++ projects/motor_controller/src/precharge_control.c
@@ -9,14 +9,10 @@
 #include "motor_controller_setters.h"
 #include "status.h"
 
-static PrechargeControlStorage s_precharge_storage = { 0 };
+static GpioAddress s_precharge_control;
 
-PrechargeControlStorage *test_get_storage(void) {
-  return &s_precharge_storage;
-}
-
-StatusCode prv_set_precharge_control(PrechargeControlStorage *storage, const GpioState state) {
-  gpio_set_state(&storage->precharge_control, state);
+StatusCode prv_set_precharge_control(GpioAddress *address, const GpioState state) {
+  gpio_set_state(&s_precharge_control, state);
   return STATUS_CODE_OK;
 }
 
@@ -46,25 +42,22 @@ TASK(PRECHARGE_INTERRUPT, TASK_MIN_STACK_SIZE) {
 
 StatusCode run_precharge_rx_cycle() {
   if (get_drive_output_precharge() == MCI_PRECHARGE_CHARGED) {
-    set_mc_status_precharge_status(MCI_PRECHARGE_CHARGED);
-    return prv_set_precharge_control(&s_precharge_storage, GPIO_STATE_HIGH);
+    return prv_set_precharge_control(&s_precharge_control, GPIO_STATE_HIGH);
   } else {
-    set_mc_status_precharge_status(MCI_PRECHARGE_DISCHARGED);
-    return prv_set_precharge_control(&s_precharge_storage, GPIO_STATE_LOW);
+    return prv_set_precharge_control(&s_precharge_control, GPIO_STATE_LOW);
   }
-}
-
-void prv_populate_storage(PrechargeControlStorage *storage,
-                          const PrechargeControlSettings *settings) {
-  storage->precharge_control = settings->precharge_control;
 }
 
 StatusCode precharge_control_init(const PrechargeControlSettings *settings) {
   interrupt_init();
-  prv_populate_storage(&s_precharge_storage, settings);
-  InterruptSettings monitor_it_settings = { .type = INTERRUPT_TYPE_INTERRUPT,
-                                            .priority = INTERRUPT_PRIORITY_NORMAL,
-                                            .edge = INTERRUPT_EDGE_RISING };
+
+  s_precharge_control = settings->precharge_control;
+
+  InterruptSettings monitor_it_settings = {
+    .type = INTERRUPT_TYPE_INTERRUPT,
+    .priority = INTERRUPT_PRIORITY_NORMAL,
+    .edge = INTERRUPT_EDGE_RISING,
+  };
   status_ok_or_return(gpio_it_register_interrupt(&settings->precharge_monitor, &monitor_it_settings,
                                                  0, PRECHARGE_INTERRUPT));
   status_ok_or_return(gpio_it_register_interrupt(&settings->precharge_monitor2,
