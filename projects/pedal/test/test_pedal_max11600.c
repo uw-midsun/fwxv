dd8b47dc5500a7c98a039cf3b9ea36fed9668fe7
Louis Sun <louis.sun3570@gmail.com>
2023-04-01 13:06:44 -0400

    saving changes before rebasing

--- projects/pedal/test/test_pedal_max11600.c
+++ projects/pedal/test/test_pedal_max11600.c
@@ -4,9 +4,6 @@
 #include "unity.h"
 
 #define TEST_MAX11600_I2C_PORT I2C_PORT_2
-#define THROTTLE_CHANNEL MAX11600_CHANNEL_0
-#define STEERING_CHANNEL MAX11600_CHANNEL_1
-#define BRAKE_CHANNEL MAX11600_CHANNEL_2
 #define REFERENCE_VOLTAGE_V 3.3
 #define MAX11600_READ_ADDRESS 0xC9
 #define MAX11600_WRITE_ADDRESS 0xC8
@@ -28,20 +25,21 @@ void test_max11600_init(void) {
 }
 
 void test_max11600_read_raw(void) {
-  for (int LSB_THROTTLE = 0; LSB_THROTTLE < 256; LSB_THROTTLE++) {
-    for (int LSB_STEERING = 0; LSB_STEERING < 256; LSB_STEERING++) {
-      for (int LSB_BRAKE = 0; LSB_BRAKE < 256; LSB_BRAKE++) {
-        // Mock all possible combinations of LSD onto the I2C
-        uint8_t tx_data[] = { LSB_THROTTLE, LSB_STEERING, LSB_BRAKE };
-        i2c_write(TEST_MAX11600_I2C_PORT, MAX11600_WRITE_ADDRESS, tx_data, 3);
-        // Read the raw data from the MAX11600
-        TEST_ASSERT_OK(max11600_read_raw(&max_storage));
-        for (int channel = 0; channel < 3; channel++) {
-          TEST_ASSERT_EQUAL(tx_data[channel], max_storage.channel_readings[channel]);
-        }
-      }
-    }
-  }
+  uint8_t tx_data[3];
+  tx_data[0] = tx_data[1] = tx_data[2] = 0x00;
+  i2c_write(TEST_MAX11600_I2C_PORT, MAX11600_WRITE_ADDRESS, tx_data, 3);
+  TEST_ASSERT_OK(max11600_read_raw(&max_storage));
+  TEST_ASSERT_EQUAL(0x00, max_storage.channel_readings[0]);
+  TEST_ASSERT_EQUAL(0x00, max_storage.channel_readings[1]);
+  TEST_ASSERT_EQUAL(0x00, max_storage.channel_readings[2]);
+  tx_data[0] = 0xFF;
+  tx_data[1] = 0xFF;
+  tx_data[2] = 0xFF;
+  i2c_write(TEST_MAX11600_I2C_PORT, MAX11600_WRITE_ADDRESS, tx_data, 3);
+  TEST_ASSERT_OK(max11600_read_raw(&max_storage));
+  TEST_ASSERT_EQUAL(0xFF, max_storage.channel_readings[0]);
+  TEST_ASSERT_EQUAL(0xFF, max_storage.channel_readings[1]);
+  TEST_ASSERT_EQUAL(0xFF, max_storage.channel_readings[2]);
 }
 
 void test_max11600_read_converted(void) {
