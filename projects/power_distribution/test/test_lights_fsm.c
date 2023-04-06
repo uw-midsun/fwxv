940b125e3fae18162abb383ba44b0b79b529064c
Citrusboa <ivybao07@gmail.com>
2023-04-05 19:29:40 -0600

    Lights FSM Test Draft

---
+++ projects/power_distribution/test/test_lights_fsm.c
@@ -0,0 +1,109 @@
+#include "centre_console_setters.h"
+#include "lights_fsm.h"
+#include "steering_setters.h"
+#include "task_test_helpers.h"
+#include "unity.h"
+
+void setup_test(void) {
+  log_init();
+  init_lights();
+}
+
+void teardown_test(void) {}
+
+TEST_IN_TASK
+void test_pd_lights_fsm(void) {
+  Fsm *f = lights->context;
+
+  // In init state
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, INIT_STATE);
+
+  // Init State -> Hazard State
+  set_power_info_hazard_state(HAZARD_ON);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, HAZARD);
+
+  // Hazard State -> Init State
+  set_power_info_hazard_state(HAZARD_OFF);
+  set_steering_info_analog_input(EE_LIGHT_TYPE_OFF);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, INIT_STATE);
+
+  // Init State -> Left Signal
+  set_steering_info_analog_input(EE_LIGHT_TYPE_SIGNAL_LEFT);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, LEFT_SIGNAL);
+
+  // Left Signal ->Init State
+  set_steering_info_analog_input(EE_LIGHT_TYPE_OFF);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, INIT_STATE);
+
+  // Init State -> Right Signal
+  set_steering_info_analog_input(EE_LIGHT_TYPE_SIGNAL_RIGHT);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, RIGHT_SIGNAL);
+
+  // Right Signal -> Init State
+  set_steering_info_analog_input(EE_LIGHT_TYPE_OFF);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, INIT_STATE);
+
+  // Init State -> Left Signal
+  set_steering_info_analog_input(EE_LIGHT_TYPE_SIGNAL_LEFT);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, LEFT_SIGNAL);
+
+  // Left Signal -> Right Signal
+  set_steering_info_analog_input(EE_LIGHT_TYPE_SIGNAL_RIGHT);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, RIGHT_SIGNAL);
+
+  // Right Signal -> Left Signal
+  set_steering_info_analog_input(EE_LIGHT_TYPE_SIGNAL_LEFT);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, LEFT_SIGNAL);
+
+  // Left Signal -> Hazard State
+  set_power_info_hazard_state(HAZARD_ON);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, HAZARD);
+
+  // Hazard State -> Init State
+  set_power_info_hazard_state(HAZARD_OFF);
+  set_steering_info_analog_input(EE_LIGHT_TYPE_OFF);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, INIT_STATE);
+
+  // Init State -> Right Signal
+  set_steering_info_analog_input(EE_LIGHT_TYPE_SIGNAL_RIGHT);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, RIGHT_SIGNAL);
+
+  // Right Signal -> Hazard State
+  set_power_info_hazard_state(HAZARD_ON);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, HAZARD);
+
+  // Hazard State -> Init State
+  set_power_info_hazard_state(HAZARD_OFF);
+  set_steering_info_analog_input(EE_LIGHT_TYPE_OFF);
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(f->curr_state->id, INIT_STATE);
+}
