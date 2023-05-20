726b469db3847ad8cc2f10fd83ccbaf9a99ace80
Ivy Bao <ivybao07@gmail.com>
2023-05-20 18:21:06 +0000

    Renamed power_distribution project & test fail from FSM timeout

---
+++ projects/power_distribution_front/test/test_lights_fsm.c
@@ -0,0 +1,111 @@
+#include "lights_fsm.h"
+#include "task_test_helpers.h"
+#include "unity.h"
+#include "power_distribution_front_getters.h"
+
+#define HAZARD_SIGNAL_MSG g_rx_struct.power_info_hazard_state
+#define STEERING_ANALOG_SIGNAL_MSG g_rx_struct.steering_info_analog_input
+
+void setup_test(void) {
+  log_init();
+  init_lights();
+}
+
+void teardown_test(void) {}
+
+TEST_IN_TASK
+void test_pd_lights_fsm(void) {
+  Fsm *f = lights->context;
+
+  // In init state
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(INIT_STATE, f->curr_state->id);
+
+  // Init State -> Hazard State
+  HAZARD_SIGNAL_MSG = HAZARD_ON;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(HAZARD, f->curr_state->id);
+
+  // Hazard State -> Init State
+  HAZARD_SIGNAL_MSG = HAZARD_OFF;
+  STEERING_ANALOG_SIGNAL_MSG = EE_LIGHT_TYPE_OFF;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(INIT_STATE, f->curr_state->id);
+
+  // Init State -> Left Signal
+  STEERING_ANALOG_SIGNAL_MSG = EE_LIGHT_TYPE_SIGNAL_LEFT;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(LEFT_SIGNAL, f->curr_state->id);
+
+  // Left Signal ->Init State
+  STEERING_ANALOG_SIGNAL_MSG = EE_LIGHT_TYPE_OFF;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(INIT_STATE, f->curr_state->id);
+
+  // Init State -> Right Signal
+  STEERING_ANALOG_SIGNAL_MSG = EE_LIGHT_TYPE_SIGNAL_RIGHT;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(RIGHT_SIGNAL, f->curr_state->id);
+
+  // Right Signal -> Init State
+  STEERING_ANALOG_SIGNAL_MSG = EE_LIGHT_TYPE_OFF;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(INIT_STATE, f->curr_state->id);
+
+  // Init State -> Left Signal
+  STEERING_ANALOG_SIGNAL_MSG = EE_LIGHT_TYPE_SIGNAL_LEFT;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(LEFT_SIGNAL, f->curr_state->id);
+
+  // Left Signal -> Right Signal
+  STEERING_ANALOG_SIGNAL_MSG = EE_LIGHT_TYPE_SIGNAL_RIGHT;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(RIGHT_SIGNAL, f->curr_state->id);
+
+  // Right Signal -> Left Signal
+  STEERING_ANALOG_SIGNAL_MSG = EE_LIGHT_TYPE_SIGNAL_LEFT;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(LEFT_SIGNAL, f->curr_state->id);
+
+  // Left Signal -> Hazard State
+  HAZARD_SIGNAL_MSG = HAZARD_ON;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(HAZARD, f->curr_state->id);
+
+  // Hazard State -> Init State
+  HAZARD_SIGNAL_MSG = HAZARD_OFF;
+  STEERING_ANALOG_SIGNAL_MSG = EE_LIGHT_TYPE_OFF;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(INIT_STATE, f->curr_state->id);
+
+  // Init State -> Right Signal
+  STEERING_ANALOG_SIGNAL_MSG = EE_LIGHT_TYPE_SIGNAL_RIGHT;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(RIGHT_SIGNAL, f->curr_state->id);
+
+  // Right Signal -> Hazard State
+  HAZARD_SIGNAL_MSG = HAZARD_ON;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(HAZARD, f->curr_state->id);
+
+  // Hazard State -> Init State
+  HAZARD_SIGNAL_MSG = HAZARD_OFF;
+  STEERING_ANALOG_SIGNAL_MSG = EE_LIGHT_TYPE_OFF;
+  fsm_run_cycle(lights);
+  wait_tasks(1);
+  TEST_ASSERT_EQUAL(INIT_STATE, f->curr_state->id);
+}
