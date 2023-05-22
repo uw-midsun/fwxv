7aff519dfee058416a4e80aead79627f5be82167
AFailure <89050937+AFailure@users.noreply.github.com>
2023-05-22 12:38:27 -0400

    Made changes to display

---
+++ projects/centre_console/inc/cc_displays.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "log.h"
+// #include "seg_display.h"
+
+// Updates the 3 centre console displays with new values and will be called in the slow cycle
+void cc_displays_update(void);
