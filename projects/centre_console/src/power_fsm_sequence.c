883229b17da10be0d10d17411e182d4bf57f5ef3
Bafran <aachinoy@gmail.com>
2023-05-22 21:48:21 -0400

    Updated test and can data

--- projects/centre_console/src/power_fsm_sequence.c
+++ projects/centre_console/src/power_fsm_sequence.c
@@ -37,7 +37,6 @@ void prv_power_fsm_confirm_aux_status_input(Fsm *fsm, void *context) {
 }
 
 void prv_power_fsm_send_pd_bms_output(void *context) {
-  // Todo (Bafran): Change value to real value
   set_set_bms_power_bms_power_on_notification(SET_BMS_POWER_NOTIFY);
   LOG_DEBUG("Transitioned to send pd bms\n");
 }
@@ -72,7 +71,6 @@ void prv_power_fsm_confirm_battery_status_input(Fsm *fsm, void *context) {
 
   uint8_t status = get_bps_heartbeat_status();
 
-  // TODO(Bafran): Confirm what bps hearbeat status message looks like
   if (status == BPS_HEARTBEAT) {
     // Reset cycle counter
     cycle_timeout = 3;
@@ -90,9 +88,9 @@ void prv_power_fsm_confirm_battery_status_input(Fsm *fsm, void *context) {
 
 void prv_power_fsm_close_battery_relays_output(void *context) {
   set_set_relay_states_relay_mask(
-      SET_CLOSE_RELAY_STATE_MASK);  // Todo (Bafran): Change value to real value
+      SET_CLOSE_RELAY_STATE_MASK);
   set_set_relay_states_relay_state(
-      SET_CLOSE_RELAY_STATE_STATE);  // Todo (Bafran): Change value to real value
+      SET_CLOSE_RELAY_STATE_STATE);
   LOG_DEBUG("Transitioned to close battery relays\n");
 }
 
@@ -136,7 +134,6 @@ void prv_power_fsm_confirm_dc_dc_input(Fsm *fsm, void *context) {
 }
 
 void prv_power_fsm_turn_on_everything_output(void *context) {
-  // Todo (Bafran): Change value to real value
   set_set_power_state_turn_on_everything_notification(SET_TURN_ON_EVERYTHING_NOTIFICATION);
   LOG_DEBUG("Transitioned to turn on everything\n");
 }
@@ -154,7 +151,6 @@ void prv_power_fsm_turn_on_everything_input(Fsm *fsm, void *context) {
 }
 
 void prv_power_fsm_power_main_complete_output(void *context) {
-  // Todo (Bafran): Change value to real value
   set_ready_to_drive_ready_state(SET_READY_TO_DRIVE);
   LOG_DEBUG("Transitioned to power main complete\n");
 }
@@ -173,7 +169,6 @@ void prv_power_fsm_power_main_complete_input(Fsm *fsm, void *context) {
 // Input/outputs for going into OFF
 
 void prv_power_fsm_discharge_precharge_output(void *context) {
-  // Todo (Bafran): Change value to real value
   set_discharge_precharge_signal1(SET_DISCHARGE_PRECHARGE);
   LOG_DEBUG("Transitioned to discharge precharge\n");
 }
@@ -183,7 +178,7 @@ void prv_power_fsm_discharge_precharge_input(Fsm *fsm, void *context) {
 
   uint8_t precharge = get_precharge_completed_notification();
 
-  if (precharge) {
+  if (precharge == PRECHARGE_COMPLETED_NOTIFCIATION) {
     // Transition to next state
     fsm_transition(fsm, POWER_FSM_TURN_OFF_EVERYTHING);
   } else {
@@ -195,8 +190,6 @@ void prv_power_fsm_discharge_precharge_input(Fsm *fsm, void *context) {
 }
 
 void prv_power_fsm_turn_off_everything_output(void *context) {
-  // Maybe reuse this message somehow
-  // Todo (Bafran): Change value to real value
   set_set_power_state_turn_on_everything_notification(SET_TURN_ON_EVERYTHING_NOTIFICATION);
   LOG_DEBUG("Transitioned to turn off everything\n");
 }
@@ -208,9 +201,7 @@ void prv_power_fsm_turn_off_everything_input(Fsm *fsm, void *context) {
 }
 
 void prv_power_fsm_open_relays_output(void *context) {
-  // Todo (Bafran): Change value to real value
   set_set_relay_states_relay_mask(SET_OPEN_RELAY_STATE_MASK);
-  // Todo (Bafran): Change value to real value
   set_set_relay_states_relay_state(SET_OPEN_RELAY_STATE_STATE);
   LOG_DEBUG("Transitioned to open relays\n");
 }
@@ -222,7 +213,6 @@ void prv_power_fsm_open_relays_input(Fsm *fsm, void *context) {
   uint8_t gnd_status = get_battery_relay_state_gnd();
 
   // If both relays are open, transition to next sequence state
-  // Todo (Bafran): Change value to real value
   if (hv_status == OPEN_HV_STATUS && gnd_status == OPEN_GND_STATUS) {
     // Transition to next state
     fsm_transition(fsm, POWER_FSM_STATE_OFF);
--- projects/centre_console/test/test_power_fsm.c
+++ projects/centre_console/test/test_power_fsm.c
@@ -4,7 +4,11 @@
 #include "power_fsm.h"
 #include "power_fsm_sequence.h"
 #include "task_test_helpers.h"
+#include "power_fsm_can_data.h"
 #include "unity.h"
+
+#define PEDAL_PRESSED 0xFF
+#define PEDAL_RELEASED 0x00
 
 static const GpioAddress s_btn_start = CC_BTN_PUSH_START;
 
@@ -27,57 +31,54 @@ void test_off_to_main(void) {
 
   // Transition to CONFIRM_AUX_STATUS
   gpio_set_state(&s_btn_start, GPIO_STATE_LOW);
-  g_rx_struct.pedal_output_brake_output = 0xFF;
+  g_rx_struct.pedal_output_brake_output = PEDAL_PRESSED;
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_CONFIRM_AUX_STATUS);
   gpio_set_state(&s_btn_start, GPIO_STATE_HIGH);
 
   // Transition to POWER_FSM_SEND_PD_BMS
-  g_rx_struct.power_select_status_status = 0x02;
-  g_rx_struct.power_select_status_fault = 0x00;
+  g_rx_struct.power_select_status_status = AUX_STATUS_BITS;
+  g_rx_struct.power_select_status_fault = 0x00; // Not a fault
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_SEND_PD_BMS);
-  // g_tx_struct.
 
   // Transition to POWER_FSM_CONFIRM_BATTERY_STATUS
-  g_rx_struct.rear_pd_fault_fault_data = 0x00;
-  g_rx_struct.front_pd_fault_fault_data = 0x00;
+  g_rx_struct.rear_pd_fault_fault_data = PD_REAR_FAULT;
+  g_rx_struct.front_pd_fault_fault_data = PD_FRONT_FAULT;
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_CONFIRM_BATTERY_STATUS);
 
   // Transition to POWER_FSM_CLOSE_BATTERY_RELAYS
-  g_rx_struct.bps_heartbeat_status = 0x00;  // Todo (Bafran): Change to real value
+  g_rx_struct.bps_heartbeat_status = BPS_HEARTBEAT;
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_CLOSE_BATTERY_RELAYS);
-  TEST_ASSERT_EQUAL(g_tx_struct.set_relay_states_relay_mask, 0x01);
-  TEST_ASSERT_EQUAL(g_tx_struct.set_relay_states_relay_state, 0x01);
+  TEST_ASSERT_EQUAL(g_tx_struct.set_relay_states_relay_mask, SET_CLOSE_RELAY_STATE_MASK);
+  TEST_ASSERT_EQUAL(g_tx_struct.set_relay_states_relay_state, SET_CLOSE_RELAY_STATE_STATE);
 
   // Transition to POWER_FSM_CONFIRM_DC_DC
-  g_rx_struct.battery_relay_state_hv = 0x01;   // Todo (Bafran): Change to real value
-  g_rx_struct.battery_relay_state_gnd = 0x01;  // Todo (Bafran): Change to real value
+  g_rx_struct.battery_relay_state_hv = CLOSE_HV_STATUS;
+  g_rx_struct.battery_relay_state_gnd = CLOSE_GND_STATUS;
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_CONFIRM_DC_DC);
 
   // Transition to POWER_FSM_TURN_ON_EVERYTHING
-  g_rx_struct.power_select_status_status = 0x02;
-  g_rx_struct.power_select_status_fault = 0x00;
+  g_rx_struct.power_select_status_status = DCDC_STATUS_BITS;
+  g_rx_struct.power_select_status_fault = 0x00; // No fault
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_TURN_ON_EVERYTHING);
-  // Todo (Bafran): Change value to real value
-  TEST_ASSERT_EQUAL(g_tx_struct.set_power_state_turn_on_everything_notification, 0x00);
+  TEST_ASSERT_EQUAL(g_tx_struct.set_power_state_turn_on_everything_notification, SET_TURN_ON_EVERYTHING_NOTIFICATION);
 
   // Transition to POWER_FSM_POWER_MAIN_COMPLETE
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_POWER_MAIN_COMPLETE);
-  // Todo (Bafran): Change value to real value
-  TEST_ASSERT_EQUAL(g_tx_struct.ready_to_drive_ready_state, 0x01);
+  TEST_ASSERT_EQUAL(g_tx_struct.ready_to_drive_ready_state, SET_READY_TO_DRIVE);
 
   // Transition to POWER_FSM_STATE_MAIN
   fsm_run_cycle(power);
@@ -92,20 +93,19 @@ void test_off_to_aux(void) {
   // Transition to CONFIRM_AUX_STATUS
   gpio_set_state(&s_btn_start, GPIO_STATE_LOW);
   // Brake is not pressed
-  g_rx_struct.pedal_output_brake_output = 0x00;
+  g_rx_struct.pedal_output_brake_output = PEDAL_RELEASED;
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_CONFIRM_AUX_STATUS);
   gpio_set_state(&s_btn_start, GPIO_STATE_HIGH);
 
   // Transition to POWER_FSM_TURN_ON_EVERYTHING
-  g_rx_struct.power_select_status_status = 0x02;
-  g_rx_struct.power_select_status_fault = 0x00;
+  g_rx_struct.power_select_status_status = AUX_STATUS_BITS;
+  g_rx_struct.power_select_status_fault = 0x00; // No fault
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_TURN_ON_EVERYTHING);
-  // Todo (Bafran) : Change value to real value
-  TEST_ASSERT_EQUAL(g_tx_struct.set_power_state_turn_on_everything_notification, 0x00);
+  TEST_ASSERT_EQUAL(g_tx_struct.set_power_state_turn_on_everything_notification, SET_TURN_ON_EVERYTHING_NOTIFICATION);
 }
 
 TEST_IN_TASK
@@ -114,7 +114,7 @@ void test_aux_to_main(void) {
 
   // Transition to POWER_FSM_SEND_PD_BMS
   gpio_set_state(&s_btn_start, GPIO_STATE_LOW);
-  g_rx_struct.pedal_output_brake_output = 0xFF;
+  g_rx_struct.pedal_output_brake_output = PEDAL_PRESSED;
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_SEND_PD_BMS);
@@ -132,12 +132,10 @@ void test_power_to_off(void) {
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_DISCHARGE_PRECHARGE);
-  // Todo (Bafran): Change value to real value
-  TEST_ASSERT_EQUAL(g_tx_struct.discharge_precharge_signal1, 0x01);
+  TEST_ASSERT_EQUAL(g_tx_struct.discharge_precharge_signal1, SET_DISCHARGE_PRECHARGE);
 
   // Transition to POWER_FSM_TURN_OFF_EVERYTHING
-  // Todo (Bafran): Change value to real value
-  g_rx_struct.precharge_completed_notification = 0x01;
+  g_rx_struct.precharge_completed_notification = PRECHARGE_COMPLETED_NOTIFCIATION;
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_TURN_OFF_EVERYTHING);
@@ -146,15 +144,12 @@ void test_power_to_off(void) {
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_OPEN_RELAYS);
-  // Todo (Bafran): Change value to real value
-  TEST_ASSERT_EQUAL(g_tx_struct.set_relay_states_relay_mask, 0x00);
-  TEST_ASSERT_EQUAL(g_tx_struct.set_relay_states_relay_state, 0x00);
+  TEST_ASSERT_EQUAL(g_tx_struct.set_relay_states_relay_mask, SET_OPEN_RELAY_STATE_MASK);
+  TEST_ASSERT_EQUAL(g_tx_struct.set_relay_states_relay_state, SET_OPEN_RELAY_STATE_STATE);
 
   // Transition to POWER_FSM_STATE_OFF
-  // Todo (Bafran): Change value to real value
-  g_rx_struct.battery_relay_state_hv = 0x01;
-  // Todo (Bafran): Change value to real value
-  g_rx_struct.battery_relay_state_gnd = 0x01;
+  g_rx_struct.battery_relay_state_hv = OPEN_HV_STATUS;
+  g_rx_struct.battery_relay_state_gnd = OPEN_GND_STATUS;
   fsm_run_cycle(power);
   wait_tasks(1);
   TEST_ASSERT_EQUAL(power_fsm->curr_state->id, POWER_FSM_STATE_OFF);
