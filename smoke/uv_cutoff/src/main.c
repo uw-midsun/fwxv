9f410136218f52976dfa6d1e72aa855383a6cac7
Taksh-041102 <takshak_041102@yahoo.com>
2023-06-14 15:44:21 -0400

    uv_cutoff smoke tests ready for eval

---
+++ smoke/uv_cutoff/src/main.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+
+#include "can.h"
+#include "gpio.h"
+#include "can_board_ids.h"
+#include "delay.h"
+#include "interrupt.h"
+#include "log.h"
+#include "misc.h"
+#include "soft_timer.h"
+#include "tasks.h"
+#include "uv_cutoff.h"
+#include "uv_cutoff_getters.h"
+#include "uv_cutoff_setters.h"
+
+// TODO(devAdhiraj): update with actual ports and pins
+#define UV_CUTOFF_PORT GPIO_PORT_A
+#define UV_CUTOFF_PIN 5
+
+#define HORN_PORT GPIO_PORT_A
+#define HORN_PIN 5
+
+#define LIGHTS_PORT GPIO_PORT_A
+#define LIGHTS_PIN 5
+
+static const GpioAddress uv_status = { .port = UV_CUTOFF_PORT, .pin = UV_CUTOFF_PIN };
+
+static const GpioAddress horn = { .port = HORN_PORT, .pin = HORN_PIN };
+
+static const GpioAddress lights = { .port = LIGHTS_PORT, .pin = LIGHTS_PIN };
+
+
+static UVCutoffState state = UV_CUTOFF_ACTIVE;
+
+void uv_smoke_logic() {
+  GpioState uv_value;
+
+  if (gpio_get_state(&uv_status, &uv_value) != STATUS_CODE_OK) {
+    LOG_CRITICAL("Error reading UV_cutoff pin!\n");
+    return;
+  }
+  if (uv_value == GPIO_STATE_LOW) {
+    state = UV_CUTOFF_DISCONNECTED;
+  } else { 
+    state = UV_CUTOFF_ACTIVE;
+  }
+
+  set_uv_cutoff_notification_signal1(state);
+
+  if (state == UV_CUTOFF_DISCONNECTED) {
+    return;
+  }
+  StatusCode status;
+  GpioState state_val;
+
+  if (get_horn_and_lights_horn_state()) {
+    assert ( status = gpio_set_state(&horn, GPIO_STATE_HIGH) );
+    if(status != STATUS_CODE_OK) {
+      LOG_DEBUG("GPIO Horn State could not be set to high\n");
+    } 
+    else if (status == STATUS_CODE_OK) {
+      gpio_get_state(&horn, &state_val);
+      if( state_val != GPIO_STATE_HIGH) {
+        LOG_DEBUG("Horn State not set correctly\n")
+      } else if (state_val == GPIO_STATE_HIGH) {
+        LOG_DEBUG("Horn State set correctly\n");
+      }
+    }
+  } else {
+    assert ( status = gpio_set_state(&horn, GPIO_STATE_LOW) );
+    if(status != STATUS_CODE_OK) {
+      LOG_DEBUG("GPIO Horn State could not be set to low\n");
+    }
+    else if (status == STATUS_CODE_OK) {
+      gpio_get_state(&horn, &state_val);
+      if( state_val != GPIO_STATE_LOW) {
+        LOG_DEBUG("Horn State not set correctly\n")
+      } else if (state_val == GPIO_STATE_LOW) {
+        LOG_DEBUG("Horn state set correctly\n");
+      }
+    }
+    
+  }
+
+  if (get_horn_and_lights_lights_state()) {
+    assert ( status = gpio_set_state(&lights, GPIO_STATE_HIGH) );
+    if(status != STATUS_CODE_OK) {
+      LOG_DEBUG("GPIO Lights State could not be set to high\n");  
+    }
+    else if (status == STATUS_CODE_OK) {
+      gpio_get_state(&lights, &state_val);
+      if(state_Val != GPIO_STATE_HIGH) {
+        LOG_DEBUG("Lights State not set correctly\n");
+      } else if (state_val == GPIO_STATE_HIGH) {
+        LOG_DEBUG("Lights State set correctly\n")
+      }
+    }
+    
+  } else {
+    assert ( status = gpio_set_state(&lights, GPIO_STATE_LOW) );
+    if(status != STATUS_CODE_OK) {
+      LOG_DEBUG("GPIO Lights State could not be set to low\n");
+    }
+    else if (status == STATUS_CODE_OK) {
+      gpio_get_state(&lights, &state_val);
+      if(state_val != GPIO_STATE_LOW) {
+        LOG_DEBUG("Lights State not set correctly\n");
+      } else if (state_val == GPIO_STATE_LOW) {
+        LOG_DEBUG("Lights State set correctly\n");
+      }
+    }
+  }
+}
+
+TASK(smoke_task, TASK_MIN_STACK_SIZE) {
+  uv_smoke_logic();
+}
+
+int main() {
+  tasks_init();
+  log_init();
+  
+  gpio_init();
+
+  gpio_init_pin(&uv_status, GPIO_INPUT_PULL_UP, GPIO_STATE_HIGH);
+  gpio_init_pin(&horn, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
+  gpio_init_pin(&lights, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
+
+  tasks_init_task(smoke_task, TASK_PRIORITY(1), NULL);
+
+  tasks_start();
+
+  LOG_DEBUG("UV cutoff task!\n");
+  return 0;
+}
