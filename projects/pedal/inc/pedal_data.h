4d4b1923a90f93493758ed085b76416c70bb895d
Bafran <48925842+Bafran@users.noreply.github.com>
2023-07-04 11:39:11 -0400

    Refactor/Update Pedal (#183)
    * Refacor driver files
    
    * Format
    
    * Update function name
    
    * Updated calib
    
    * Updated pedal to 32 bit unsigned value
    
    * Move merged files
    
    * Format
    
    * Move pedal files
    
    * Rename calib and fix build issue
    
    * Move files
    
    * Move MAX test
    
    * Init i2c in max test
    
    * Format

--- projects/pedal/inc/pedal_data.h
+++ projects/pedal/inc/pedal_data.h
@@ -1,12 +1,10 @@
 #pragma once
 #include "max11600.h"
 
-extern Max11600Storage s_max11600_storage;
-
 // Read the trottle value in mV
 // Requires the MAX11600 to be initialized
-StatusCode read_throttle_data(int16_t *position);
+StatusCode read_throttle_data(uint32_t *position);
 
 // Read the trottle value in mV from the MAX11600
 // Requires the MAX11600 to be initialized
-StatusCode read_brake_data(int16_t *position);
+StatusCode read_brake_data(uint32_t *position);
---
+++ projects/pedal/src/pedal_data.c
@@ -0,0 +1,47 @@
+#include "pedal_data.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pedal_calib.h"
+#include "pedal_shared_resources_provider.h"
+#include "status.h"
+
+#define THROTTLE_CHANNEL MAX11600_CHANNEL_0
+#define BRAKE_CHANNEL MAX11600_CHANNEL_2
+#define EE_PEDAL_VALUE_DENOMINATOR ((1 << 12))
+
+static PedalCalibBlob *s_calib_blob;
+static Max11600Storage *s_max11600_storage;
+
+void pedal_data_init() {
+  s_max11600_storage = get_shared_max11600_storage();
+  s_calib_blob = get_shared_pedal_calib_blob();
+}
+
+StatusCode read_pedal_data(uint32_t *reading, MAX11600Channel channel) {
+  status_ok_or_return(max11600_read_raw(s_max11600_storage));
+  int32_t range = s_calib_blob->brake_calib.upper_value - s_calib_blob->brake_calib.lower_value;
+
+  int32_t reading_upscaled =
+      (int32_t)s_max11600_storage->channel_readings[channel] * EE_PEDAL_VALUE_DENOMINATOR;
+  reading_upscaled -= s_calib_blob->brake_calib.lower_value * EE_PEDAL_VALUE_DENOMINATOR;
+  reading_upscaled *= 100;
+
+  if (range != 0) {
+    reading_upscaled /= (range * EE_PEDAL_VALUE_DENOMINATOR);
+    // Return a reading between 0 and INT32_MAX, representing a percentage between 0 and 100
+    *reading = (uint32_t)((reading_upscaled / 100.0) * UINT32_MAX);
+  }
+  return STATUS_CODE_OK;
+}
+
+StatusCode read_throttle_data(uint32_t *reading) {
+  return read_pedal_data(reading, THROTTLE_CHANNEL);
+}
+
+StatusCode read_brake_data(uint32_t *reading) {
+  return read_pedal_data(reading, BRAKE_CHANNEL);
+}
