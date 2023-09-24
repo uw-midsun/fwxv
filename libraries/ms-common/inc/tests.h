6fa441957eb4c01d23ab7804c216e8e7ad4c19e8
ShiCheng Lu <shichenglu0708@gmail.com>
2023-09-24 03:59:57 +0000

    tests.h docs

--- libraries/ms-common/inc/tests.h
+++ libraries/ms-common/inc/tests.h
@@ -4,37 +4,48 @@
 
 #include "status.h"
 
-// Initializes test end and test start semaphore
+// Initializes test end and test start semaphore.
 void tests_init(void);
 
 /**
- * @brief get the count for the test_start semaphore
+ * @brief get the count for the test_start semaphore.
  *
- * @return uint8_t, semaphore count
+ * @return uint8_t: semaphore count.
  */
 uint8_t test_start_get(void);
 /**
- * @brief get the count for the test_end semaphore
+ * @brief get the count for the test_end semaphore.
  *
- * @return uint8_t, semaphore count
+ * @return uint8_t: semaphore count.
  */
 uint8_t test_end_get(void);
 
 /**
- * @brief release the test_start semaphore
+ * @brief release the test_start semaphore.
  *
  * @return StatusCode: STATUS_CODE_OK if semaphore was released successfully,
- * STATUS_CODE_INTERNAL_ERROR otherwise
+ * otherwise STATUS_CODE_INTERNAL_ERROR.
  */
 StatusCode test_start_give(void);
 /**
- * @brief release the test_end semaphore
+ * @brief release the test_end semaphore.
  *
- * @return StatusCode : STATUS_CODE_OK if semaphore was released successfully,
- * STATUS_CODE_INTERNAL_ERROR otherwise
+ * @return StatusCode: STATUS_CODE_OK if semaphore was released successfully,
+ * otherwise STATUS_CODE_INTERNAL_ERROR.
  */
 StatusCode test_end_give(void);
 
-// Take for the test start anbd test end semaphores
+/**
+ * @brief obtain the test_start semaphore.
+ *
+ * @return StatusCode: STATUS_CODE_OK if semaphore was obtained successfully,
+ * otherwise STATUS_CODE_INTERNAL_ERROR.
+ */
 StatusCode test_start_take(void);
+/**
+ * @brief obtain the test_end semaphore.
+ *
+ * @return StatusCode: STATUS_CODE_OK if semaphore was obtained successfully,
+ * otherwise STATUS_CODE_INTERNAL_ERROR.
+ */
 StatusCode test_end_take(void);
