87e5665dd47c4fb67b8b2347698b5673d8621b4d
Adhiraj Singh <75645547+devAdhiraj@users.noreply.github.com>
2023-09-28 17:58:54 -0400

    FWXV-219: pd current sense broadcast (#208)
    * pd current sense functions
    
    * minor fix
    
    * minor change

---
+++ projects/power_distribution_front/inc/output_current_sense.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "bts_load_switch.h"
+#include "gpio.h"
+#include "i2c.h"
+#include "outputs.h"
+#include "pca9555_gpio_expander.h"
+#include "pin_defs.h"
+
+// initializes sense pin and mux sel pins, must be called before adc_init()
+StatusCode pd_sense_init(void);
+
+// reads adc value of all pins in output group and stores the result in reading_out attr
+StatusCode pd_sense_output_group(OutputGroup group);
