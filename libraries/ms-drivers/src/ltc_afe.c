de1397213f974ac84c3dfd170ada612d8794a8b2
AFailure <89050937+AFailure@users.noreply.github.com>
2023-10-05 20:09:02 -0400

    Removed all AFE related stuff

--- libraries/ms-drivers/src/ltc_afe.c
+++
@@ -1,8 +0,0 @@
-#include "ltc_afe.h"
-
-#include "ltc_afe_impl.h"
-
-StatusCode ltc_afe_init(LtcAfeStorage *afe, const LtcAfeSettings *settings) {
-  status_ok_or_return(ltc_afe_impl_init(afe, settings));
-  return STATUS_CODE_OK;
-}
--- projects/bms_carrier/inc/bms.h
+++ projects/bms_carrier/inc/bms.h
@@ -2,7 +2,6 @@
 
 #include <stdint.h>
 
-#include "cell_sense.h"
 #include "current_sense.h"
 #include "i2c.h"
 #include "status.h"
