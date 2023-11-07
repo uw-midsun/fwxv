a8dfea4e48ca596c3020cabbdfcd4534ecd523e0
cnscwong <105528854+cnscwong@users.noreply.github.com>
2023-11-07 17:38:35 -0500

    fwxv 228 - temp sense (#214)
    Temperature sense for solar sense project
    
    * Pushing fwxv 228 for review
    
    * Ran scons format
    
    * added newline at end of file for lint errors
    
    * Completed requested changes
    
    * Final changes
    
    * format and lint
    
    * Fixed function call names

--- projects/solar_sense/src/main.c
+++ projects/solar_sense/src/main.c
@@ -4,6 +4,7 @@
 #include "master_task.h"
 #include "solar_sense_getters.h"
 #include "tasks.h"
+#include "temp_sense.h"
 
 void run_fast_cycle() {}
 
@@ -11,10 +12,23 @@ void run_medium_cycle() {}
 
 void run_slow_cycle() {}
 
+static CanStorage s_can_storage = { 0 };
+const CanSettings can_settings = {
+  .device_id = 0x1,
+  .bitrate = CAN_HW_BITRATE_500KBPS,
+  .tx = { GPIO_PORT_A, 12 },
+  .rx = { GPIO_PORT_A, 11 },
+  .loopback = true,
+};
+
 int main() {
   tasks_init();
   log_init();
+  gpio_init();
   LOG_DEBUG("Welcome to TEST!");
+  adc_init();
+  can_init(&s_can_storage, &can_settings);
+  temp_sense_adc_init();
 
   init_master_task();
 
