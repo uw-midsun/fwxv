13642a3b46b281a9ad42904221c49db372ff2237
Elbert Chen <86504165+elbertchen1@users.noreply.github.com>
2023-11-08 15:49:36 -0500

    FWXV-226 Added MPPT Task (#228)
    * FWXV-226 Added MPPT Task
    
    * Changing mppt selection logic
    
    * Fix format
    
    * Moving main code temporarily
    
    * Lint
    
    * Fixed build?

--- projects/solar_sense/src/main.c
+++ projects/solar_sense/src/main.c
@@ -2,13 +2,15 @@
 
 #include "log.h"
 #include "master_task.h"
-#include "solar_sense_getters.h"
+#include "mppt.h"
 #include "tasks.h"
 #include "temp_sense.h"
 
 void run_fast_cycle() {}
 
-void run_medium_cycle() {}
+void run_medium_cycle() {
+  read_mppts();
+}
 
 void run_slow_cycle() {}
 
@@ -21,14 +23,24 @@ const CanSettings can_settings = {
   .loopback = true,
 };
 
+static const SpiSettings spi_settings = {
+  .baudrate = 1000000,
+  .mode = SPI_MODE_3,
+  .mosi = { .port = GPIO_PORT_B, .pin = 15 },
+  .miso = { .port = GPIO_PORT_B, .pin = 14 },
+  .sclk = { .port = GPIO_PORT_B, .pin = 13 },
+  .cs = { .port = GPIO_PORT_B, .pin = 12 },
+};
+
 int main() {
   tasks_init();
   log_init();
   gpio_init();
-  LOG_DEBUG("Welcome to TEST!");
+  LOG_DEBUG("Welcome to Solar Sense!");
   adc_init();
   can_init(&s_can_storage, &can_settings);
   temp_sense_adc_init();
+  mppt_init(&spi_settings, SPI_PORT_2);
 
   init_master_task();
 
