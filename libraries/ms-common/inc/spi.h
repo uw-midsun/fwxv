8bc53ca27cdeb7f227500eda2631a699942a7169
ShiCheng Lu <shichenglu0708@gmail.com>
2023-11-12 04:42:32 +0000

    change x86 implementation of spi to match arm version

--- libraries/ms-common/inc/spi.h
+++ libraries/ms-common/inc/spi.h
@@ -35,25 +35,11 @@ StatusCode spi_init(SpiPort spi, const SpiSettings *settings);
 
 // This method will send |tx_len| bytes from |tx_data| to the spi port |spi|. It
 // will not change the CS line state. The response bytes will be discarded.
-StatusCode spi_tx(SpiPort spi, uint8_t *tx_data, size_t tx_len);
+#define spi_tx(spi, tx_data, tx_len) spi_exchange(spi, tx_data, tx_len, NULL, 0)
 
-// This method will receive |rx_len| bytes and place it into |rx_data| from the
-// spi port |spi|. It will not change the CS line state. In order to receive
-// data this method will send the byte specified by the |placeholder| parameter.
-StatusCode spi_rx(SpiPort spi, uint8_t *rx_data, size_t rx_len, uint8_t placeholder);
-
-// This method will set the state of the CS line for a given spi port
-StatusCode spi_cs_set_state(SpiPort spi, GpioState state);
-
-// Gets the CS state of a given spi port and assigns it to the state that is
-// passed in.
-StatusCode spi_cs_get_state(SpiPort spi, GpioState *input_state);
-
-// This method is a wrapper for |spi_tx| and |spi_rx|. First it will call
-// |spi_tx|, then |spi_rx|. Before tx and rx, CS will be pulled low. After,
-// CS will be pulled high. As a rule of thumb, this method should be used in
-// simple transactions where at most one TX is required, followed by at most one
-// RX.
+// This method will initiate a spi transaction on the |spi| port.
+// First, it will pull CS low, then transmit |tx_len| number of bytes from |tx_data|,
+// then receive |rx_len| number of bytes into |tx_data|, Finally pull CS high.
 StatusCode spi_exchange(SpiPort spi, uint8_t *tx_data, size_t tx_len, uint8_t *rx_data,
                         size_t rx_len);
 
