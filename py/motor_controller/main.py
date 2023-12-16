d97ff97b733e9c1b2f2990c804056a8a527cbb5e
ShiCheng Lu <shichenglu0708@gmail.com>
2023-12-16 17:29:20 +0000

    clean up script a little

--- py/motor_controller/main.py
+++ py/motor_controller/main.py
@@ -4,39 +4,18 @@ Testing motor controller interface
 import subprocess
 import os
 import time
-import re
 import struct
 import sys
 from collections import defaultdict
 from can import can
+from can.util import set_output, parse_line
 
-CANDUMP_PATTERN = re.compile(r"(\w*)  (\w*)   \[([0-9])\]  ((?:\w\w ?)+)")
 DRIVE_STATE_STR = ["NEUTRAL", "DRIVE", "REVERSE"]
 
 
-def set_output(output):
-    '''set the terminal output to the output string'''
-    # 'static' variable
-    if "output" not in set_output.__dict__:
-        set_output.output = ""
-    if output != set_output.output:
-        print("\033c", end="")
-        print(output)
-        set_output.output = output
-
-
-def parse_line(line):
-    '''get the can id and data from a candump line'''
-    _, can_id, length, data = CANDUMP_PATTERN.match(line).groups()
-    can_id = int(can_id, base=16)
-    length = int(length)
-    data = bytes.fromhex(data)
-
-    return can_id, data
-
-
-def handle_input(state):
+def handle_stdin():
     '''handle stdin input, updates velocity, drive state, etc.'''
+    state = {}
     line = sys.stdin.readline().strip()
     if line:
         # output_str = ""
@@ -52,6 +31,64 @@ def handle_input(state):
                 state["brake"] = float(val)
         except BaseException:
             pass
+    return state
+
+
+def update_display(state, rx):
+    '''update terminal output'''
+    set_output(f"""
+state: {DRIVE_STATE_STR[state['state']]}, vel: {state['vel']:>5}, throttle: {state['throttle']}, brake: {state['brake']}
+            left      right
+velocity:    {rx["vel_l"]:<8d}  {rx["vel_r"]:<8d}
+voltage:     {rx["vol_l"]:<8d}  {rx["vol_r"]:<8d}
+current:     {rx["cur_l"]:<8d}  {rx["cur_r"]:<8d}
+motor temp:  {rx["mtt_l"]:<8d}  {rx["mtt_r"]:<8d}
+sink temp:   {rx["hst_l"]:<8d}  {rx["hst_r"]:<8d}
+dsp temp:    {rx["dsp_l"]:<8d}  {rx["dsp_r"]:<8d}
+motor stat:  {rx["mts_l"]:<8d}  {rx["mts_r"]:<8d}
+limit:       {rx["lbs_l"]:0<8b}  {rx["lbs_r"]:0<8b}
+error:       {rx["ebs_l"]:0<8b}  {rx["ebs_r"]:0<8b}
+board fault: {rx["bfalt"]}   overtemp: {rx["ovtmp"]:}    precharge: {rx["prchg"]}
+""")
+
+
+def rx_all(proc):
+    '''rx all msgs from can'''
+    rx = {}
+    line = proc.stdout.readline().decode().strip()
+    while line:
+        can_id, data = parse_line(line)
+
+        if can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_MOTOR_CONTROLLER_VC:
+            rx["vol_l"] = struct.unpack("h", data[0:2])[0]
+            rx["cur_l"] = struct.unpack("h", data[2:4])[0]
+            rx["vol_r"] = struct.unpack("h", data[4:6])[0]
+            rx["cur_r"] = struct.unpack("h", data[6:8])[0]
+        elif can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_MOTOR_VELOCITY:
+            rx["vel_l"] = struct.unpack("h", data[0:2])[0]
+            rx["vel_r"] = struct.unpack("h", data[2:4])[0]
+        elif can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_MOTOR_STATUS:
+            rx["mts_l"] = struct.unpack("i", data[0:4])[0]
+            rx["mts_r"] = struct.unpack("i", data[4:8])[0]
+        elif can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_MOTOR_SINK_TEMPS:
+            rx["mtt_l"] = struct.unpack("h", data[0:2])[0]
+            rx["hst_l"] = struct.unpack("h", data[2:4])[0]
+            rx["mtt_r"] = struct.unpack("h", data[4:6])[0]
+            rx["hst_r"] = struct.unpack("h", data[6:8])[0]
+        elif can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_DSP_BOARD_TEMPS:
+            rx["dsp_l"] = struct.unpack("i", data[0:4])[0]
+            rx["dsp_r"] = struct.unpack("i", data[4:8])[0]
+        elif can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_MC_STATUS:
+            rx["lbs_l"] = int(data[0])
+            rx["ebs_l"] = int(data[1])
+            rx["lbs_r"] = int(data[2])
+            rx["ebs_r"] = int(data[3])
+            rx["bfalt"] = int(data[4])
+            rx["ovtmp"] = int(data[5])
+            rx["prchg"] = int(data[6])
+
+        line = proc.stdout.readline().decode().strip()
+    return rx
 
 
 def main():
@@ -72,58 +109,11 @@ def main():
 
         while True:
             # std input
-            handle_input(state)
-
+            state |= handle_stdin()
             # Can RX
-            line = proc.stdout.readline().decode().strip()
-            while line:
-                can_id, data = parse_line(line)
+            rx |= rx_all(proc)
 
-                if can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_MOTOR_CONTROLLER_VC:
-                    rx["vol_l"] = struct.unpack("h", data[0:2])[0]
-                    rx["cur_l"] = struct.unpack("h", data[2:4])[0]
-                    rx["vol_r"] = struct.unpack("h", data[4:6])[0]
-                    rx["cur_r"] = struct.unpack("h", data[6:8])[0]
-                elif can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_MOTOR_VELOCITY:
-                    rx["vel_l"] = struct.unpack("h", data[0:2])[0]
-                    rx["vel_r"] = struct.unpack("h", data[2:4])[0]
-                elif can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_MOTOR_STATUS:
-                    rx["mts_l"] = struct.unpack("i", data[0:4])[0]
-                    rx["mts_r"] = struct.unpack("i", data[4:8])[0]
-                elif can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_MOTOR_SINK_TEMPS:
-                    rx["mtt_l"] = struct.unpack("h", data[0:2])[0]
-                    rx["hst_l"] = struct.unpack("h", data[2:4])[0]
-                    rx["mtt_r"] = struct.unpack("h", data[4:6])[0]
-                    rx["hst_r"] = struct.unpack("h", data[6:8])[0]
-                elif can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_DSP_BOARD_TEMPS:
-                    rx["dsp_l"] = struct.unpack("i", data[0:4])[0]
-                    rx["dsp_r"] = struct.unpack("i", data[4:8])[0]
-                elif can_id == can.SYSTEM_CAN_MESSAGE_MOTOR_CONTROLLER_MC_STATUS:
-                    rx["lbs_l"] = int(data[0])
-                    rx["ebs_l"] = int(data[1])
-                    rx["lbs_r"] = int(data[2])
-                    rx["ebs_r"] = int(data[3])
-                    rx["bfalt"] = int(data[4])
-                    rx["ovtmp"] = int(data[5])
-                    rx["prchg"] = int(data[6])
-
-                line = proc.stdout.readline().decode().strip()
-
-            output = f"""
-state: {DRIVE_STATE_STR[state['state']]}, vel: {state['vel']:>5}, throttle: {state['throttle']}, brake: {state['brake']}
-            left      right
-velocity:    {rx["vel_l"]:<8d}  {rx["vel_r"]:<8d}
-voltage:     {rx["vol_l"]:<8d}  {rx["vol_r"]:<8d}
-current:     {rx["cur_l"]:<8d}  {rx["cur_r"]:<8d}
-motor temp:  {rx["mtt_l"]:<8d}  {rx["mtt_r"]:<8d}
-sink temp:   {rx["hst_l"]:<8d}  {rx["hst_r"]:<8d}
-dsp temp:    {rx["dsp_l"]:<8d}  {rx["dsp_r"]:<8d}
-motor stat:  {rx["mts_l"]:<8d}  {rx["mts_r"]:<8d}
-limit:       {rx["lbs_l"]:0<8b}  {rx["lbs_r"]:0<8b}
-error:       {rx["ebs_l"]:0<8b}  {rx["ebs_r"]:0<8b}
-board fault: {rx["bfalt"]}   overtemp: {rx["ovtmp"]:}    precharge: {rx["prchg"]}
-"""
-            set_output(output)
+            update_display(state, rx)
 
             # every 200 ms
             can.send_centre_console_drive_output(
--- py/wavesculptor/main.py
+++ py/wavesculptor/main.py
@@ -3,25 +3,13 @@ Simulate a wavesculptor 22's can messages
 '''
 import time
 import subprocess
-import re
 import struct
 import os
+from can.util import set_output, parse_line
 
 DRIVER_CONTROL_BASE = 0x01
 MOTOR_CONTROLLER_BASE_L = 0x40
 MOTOR_CONTROLLER_BASE_R = 0x80
-
-candump_pattern = re.compile(r"(\w*)  (\w*)   \[([0-9])\]  ((?:\w\w ?){8})")
-
-
-def parse_line(line):
-    '''get the can id and data from a candump line'''
-    _, can_id, length, data = candump_pattern.match(line).groups()
-    can_id = int(can_id, base=16)
-    length = int(length)
-    data = bytes.fromhex(data)
-
-    return can_id, data
 
 
 def pack(num, size):
@@ -54,6 +42,53 @@ def pack_status(rx_err, tx_err, active, err_flg, lim_flg):
         pack(err_flg, 16) + pack(lim_flg, 16)
 
 
+def handle_stdin():
+    '''handle stdin commands'''
+
+
+def send_200ms_messages(current, velocity):
+    '''send can messages that are sent every 200ms'''
+    for i, base_addrs in enumerate([MOTOR_CONTROLLER_BASE_L, MOTOR_CONTROLLER_BASE_R]):
+        send_status(base_addrs + 0x01,
+                    pack_status(0, 0, 0, 0, 0))  # status
+        send_message(base_addrs + 0x02, current + i,
+                     current * 2 + i)  # bus current, bus voltage
+        send_message(base_addrs + 0x03, velocity + i,
+                     velocity * 2 + i)  # vehicle vel, motor rpm
+
+        # these are all ignored so values doens't matter,
+        # just make sure they don't break anything
+        # phase B cur, phase C cur
+        send_message(base_addrs + 0x04, 0.2, 0.2)
+        send_message(base_addrs + 0x05, 0.2, 0.2)  # vol vec Vd, Vq
+        send_message(base_addrs + 0x06, 0.2, 0.2)  # cur vec Id, Iq
+        send_message(base_addrs + 0x07, 0.1, 0.1)  # backEmf d, q
+
+
+def send_1s_messages(offset=0):
+    '''send can messages that are sent every 1s'''
+    for i, base_addrs in enumerate([MOTOR_CONTROLLER_BASE_L, MOTOR_CONTROLLER_BASE_R]):
+        send_message(base_addrs + 0x00, 0, 0)  # id
+        send_message(base_addrs + 0x08, 15.0 + offset + i, 0)  # 15V rail V
+        send_message(base_addrs + 0x09, 3.3 + offset + i,
+                     1.9 + offset + i)  # 3.3 rail V, 1.9 rail V
+        send_message(base_addrs + 0x0b, 30.0 + offset + i,
+                     31.0 + offset + i)  # heatsink temp, motor temp
+        send_message(base_addrs + 0x0c, 0,
+                     25.0 + offset + i)  # ---,  dsp temp
+        send_message(base_addrs + 0x0e, 45.0 + offset + i,
+                     15.0 + offset + i)  # DC bus amp hours, odometer
+
+
+def update_display(motor_velocity):
+    '''update terminal output'''
+    set_output(f'''
+WAVESCULPTOR:
+motor_vel: {motor_velocity}
+
+''')
+
+
 def main():
     '''main'''
     with subprocess.Popen(['candump', 'vcan0'], stdout=subprocess.PIPE) as proc:
@@ -67,7 +102,11 @@ def main():
 
         motor_velocity = 0
 
+        state = {}
+
         while True:
+            state |= handle_stdin()
+
             line = proc.stdout.readline().decode().strip()
             while line:
                 can_id, data = parse_line(line)
@@ -84,47 +123,18 @@ def main():
             if motor_velocity > velocity:
                 motor_velocity -= min(current, motor_velocity - velocity)
 
-            print(motor_velocity)
-
             # every 200 ms
-            for i, base_addrs in enumerate([MOTOR_CONTROLLER_BASE_L, MOTOR_CONTROLLER_BASE_R]):
-                send_status(base_addrs + 0x01,
-                            pack_status(0, 0, 0, 0, 0))  # status
-                send_message(base_addrs + 0x02, current + i,
-                             current * 2 + i)  # bus current, bus voltage
-                send_message(base_addrs + 0x03, motor_velocity + i,
-                             motor_velocity * 2 + i)  # vehicle vel, motor rpm
-
-                # these are all ignored so values doens't matter,
-                # just make sure they don't break anything
-                # phase B cur, phase C cur
-                send_message(base_addrs + 0x04, 0.2, 0.2)
-                send_message(base_addrs + 0x05, 0.2,
-                             0.2)  # voltage vec Vd, Vq
-                send_message(base_addrs + 0x06, 0.2,
-                             0.2)  # current vec Id, Iq
-                send_message(base_addrs + 0x07, 0.1, 0.1)  # backEmf d, q
+            send_200ms_messages(current, motor_velocity)
+            update_display(motor_velocity)
 
             counter += 1
             if counter % 5:
                 # every 1 second
-                offset = counter_2 * 0.1
                 counter_2 += 1
                 if counter_2 % 10:
                     counter_2 = 0
 
-                for i, base_addrs in enumerate([MOTOR_CONTROLLER_BASE_L, MOTOR_CONTROLLER_BASE_R]):
-                    send_message(base_addrs + 0x00, 0, 0)  # id
-                    send_message(base_addrs + 0x08, 15.0 +
-                                 offset + i, 0)  # 15V rail voltage
-                    send_message(base_addrs + 0x09, 3.3 + offset + i,
-                                 1.9 + offset + i)  # 3.3 rail V, 1.9 rail V
-                    send_message(base_addrs + 0x0b, 30.0 + offset + i,
-                                 31.0 + offset + i)  # heatsink temp, motor temp
-                    send_message(base_addrs + 0x0c, 0,
-                                 25.0 + offset + i)  # ---,  dsp temp
-                    send_message(base_addrs + 0x0e, 45.0 + offset + i,
-                                 15.0 + offset + i)  # DC bus amp hours, odometer
+                send_1s_messages(counter_2 * 0.1)
 
                 counter = 0
 
