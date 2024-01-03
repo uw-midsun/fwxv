7bfeccfa5f64c8142155874328ca9dbd4eaebf5f
thiendan <98007308+thiendan@users.noreply.github.com>
2024-01-03 15:23:06 -0500

    Update Output Config spec for PD based on new rev (#238)
    * Complete task 1
    
    * Update Output Config spec for PD based on new rev
    
    * lint
    
    * lint
    
    ---------
    
    Co-authored-by: Mitchell Ostler <mlostler@uwaterloo.ca>

--- projects/power_distribution/inc/outputs.h
+++ projects/power_distribution/inc/outputs.h
@@ -20,58 +20,29 @@
 // Add any new outputs at the end, and if a spare gets a proper name, just rename the spare output.
 typedef enum {
   // Outputs for front power distribution
-  FRONT_OUTPUT_CENTRE_CONSOLE = 0,
-  FRONT_OUTPUT_PEDAL,
-  FRONT_OUTPUT_STEERING,
-  FRONT_OUTPUT_UV_CUTOFF,
-  FRONT_OUTPUT_LEFT_FRONT_TURN_LIGHT,
-  FRONT_OUTPUT_RIGHT_FRONT_TURN_LIGHT,
-  FRONT_OUTPUT_DAYTIME_RUNNING_LIGHTS,
-  FRONT_OUTPUT_CAMERA_DISPLAY,  // aka main display
+  SOLAR_SENSE_1 = 0,
+  SOLAR_SENSE_2,
+  PEDAL,
+  STEERING,
+  LEFT_TURN,
+  RIGHT_TURN,
+  DRL_LIGHT,
+  BRAKE_LIGHT,
 
-  FRONT_OUTPUT_LEFT_CAMERA,
-  FRONT_OUTPUT_RIGHT_CAMERA,
-  FRONT_OUTPUT_MAIN_PI,  // driver display + telemetry pi
-  FRONT_OUTPUT_REAR_DISPLAY,
-  FRONT_OUTPUT_LEFT_DISPLAY,
-  FRONT_OUTPUT_RIGHT_DISPLAY,
-  FRONT_OUTPUT_SPEAKER,  // If not using speaker
+  CENTER_CONSOLE,
+  BMS_DCDC,
+  MCI,
+  BPS_LIGHT,
 
-  FRONT_OUTPUT_5V_SPARE_1,
-  FRONT_OUTPUT_5V_SPARE_2,
-  FRONT_OUTPUT_SPARE_1,  // Spare_1_Telemetry_PWR
-  FRONT_OUTPUT_SPARE_2,  // MCI_PWR
-  FRONT_OUTPUT_SPARE_3,
-  FRONT_OUTPUT_SPARE_4,  // on rear fan 1's BTS7200 channel
-  FRONT_OUTPUT_SPARE_5,  // on rear fan 2's BTS7200 channel
+  SPARE_12V_1,
+  SPARE_12V_2,
+  BMS_AUX,
+  DRIVER_FAN,
+  REAR_CAM_LCD,
+  SPARE_5V_DCDC,
 
-  // Outputs for rear power distribution
-  REAR_OUTPUT_BMS,
-  REAR_OUTPUT_MCI,
-  REAR_OUTPUT_SOLAR_SENSE,
-  REAR_OUTPUT_BPS_STROBE_LIGHT,
-  REAR_OUTPUT_FAN_1,
-  LEFT_REAR_CAMERA,
-  RIGHT_CAMERA,
-  // REAR_OUTPUT_FAN_2,
-  // REAR_OUTPUT_LEFT_REAR_TURN_LIGHT,
-  // REAR_OUTPUT_RIGHT_REAR_TURN_LIGHT,
-  // REAR_OUTPUT_BRAKE_LIGHT,
-  // REAR_OUTPUT_REAR_CAMERA,
-
-  // REAR_OUTPUT_5V_SPARE_1,
-  // REAR_OUTPUT_5V_SPARE_2,
-  // REAR_OUTPUT_SPARE_1,   // on Telemetry
-  // REAR_OUTPUT_SPARE_2,   // on Left Display
-  // REAR_OUTPUT_SPARE_3,
-  // REAR_OUTPUT_SPARE_4,   // on pedal's BTS7200 channel
-  // REAR_OUTPUT_SPARE_5,   // on steering's BTS7200 channel
-  // REAR_OUTPUT_SPARE_6,   // on right camera's BTS7200 channel
-  // REAR_OUTPUT_SPARE_7,   // on main pi's BTS7200 channel
-  // REAR_OUTPUT_SPARE_8,   // on driver display's BTS7200 channel
-  // REAR_OUTPUT_SPARE_9,   // on centre console's BTS7200 channel
-  // REAR_OUTPUT_SPARE_10,  // on rear display's BTS7200 channel
-  // REAR_OUTPUT_SPARE_11,  // UV_cutoff_Pwr
+  TELEMETRY,
+  SPARE_5V_AUX,
 
   NUM_OUTPUTS,
 } Output;
