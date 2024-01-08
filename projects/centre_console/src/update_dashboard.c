70b411885d0cfa515830deb78f41bb2a8dc3b536
vagrant <vagrant@midsunbox>
2024-01-08 20:10:22 +0000

    Reformatted pd_fault check + indicator on centre console

--- projects/centre_console/src/update_dashboard.c
+++ projects/centre_console/src/update_dashboard.c
@@ -28,19 +28,23 @@ typedef enum DriveLeds {
   RIGHT_LED,
   CRUISE_LED,
   LIGHTS_LED,
+  AUX_WARNING_LED,
   NUM_DRIVE_LED,
 } DriveLeds;
 
 static Pca9555GpioAddress s_output_leds[NUM_DRIVE_LED] = {
-  [POWER_LED] = POWER_LED_ADDR,   [HAZARD_LED] = HAZARD_LED_ADDR, [LEFT_LED] = LEFT_LED_ADDR,
-  [RIGHT_LED] = RIGHT_LED_ADDR,   [CRUISE_LED] = CRUISE_LED_ADDR, [REGEN_LED] = REGEN_LED_ADDR,
+  [POWER_LED] = POWER_LED_ADDR,
+  [HAZARD_LED] = HAZARD_LED_ADDR,
+  [LEFT_LED] = LEFT_LED_ADDR,
+  [RIGHT_LED] = RIGHT_LED_ADDR,
+  [CRUISE_LED] = CRUISE_LED_ADDR,
+  [REGEN_LED] = REGEN_LED_ADDR,
   [AUX_WARNING_LED] = AUX_WARNING_LED_ADDR,
 };
 
 void update_indicators(uint32_t notif) {
   // Update hazard light
   if (notify_check_event(&notif, HAZARD_BUTTON_EVENT)) {
-
     if (!s_hazard_state) {
       s_hazard_state = true;
       pca9555_gpio_set_state(&s_output_leds[HAZARD_LED], PCA9555_GPIO_STATE_HIGH);
