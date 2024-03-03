794eb6da53a11bc239707844d4064850a923bf22
akashem06 <aryankashem@gmail.com>
2024-03-03 19:15:47 +0000

    doc files added

--- libraries/ms-common/scripts/manager.py
+++ libraries/ms-common/scripts/manager.py
@@ -8,15 +8,15 @@ import socket
 import time
 import subprocess
 
-################################################################################
+###########################################################
 # INITIALIZATION
+###########################################################
 
 PROJECTS = [
     "bms_carrier", "centre_console",
     # "motor_controller", "pedal", KNOWN ISSUES WITH THIS? SEGMENTATION FAULT
     "power_distribution", "steering"
 ]
-# PROJECTS = ["pedal"]
 PROJECT_DIR = os.path.join(os.getcwd(), 'projects')
 BUILD_DIR = os.path.join(os.getcwd(), 'build/x86/bin/projects')
 
@@ -26,10 +26,17 @@ try:
 except Exception as error:
     print(f"Error: {error}")
 
+###########################################################
+# MANAGER CLASS FOR EACH PROJECT
+###########################################################
+
+
 class ProjectManager:
     '''
-    Docfile
+    This class is a wrapper for each project. It has has a unique
+    port number used to communicate to the project
     '''
+
     def __init__(self, project, socket_num):
         self.project = project
         self.socket_num = socket_num
@@ -37,7 +44,9 @@ class ProjectManager:
 
     def start_project(self):
         '''
-        Docfile
+        This function builds the x86 binary and starts the project.
+        It also passes a port number into the binary to initialize
+        the socket on both sides to start communication
         '''
         if os.path.exists(os.path.join(PROJECT_DIR, self.project)):
             program = os.path.join(BUILD_DIR, self.project)
@@ -64,7 +73,7 @@ class ProjectManager:
 
     def find_available_port(self):
         '''
-        Find an available port starting from the given port number.
+        Find an available port starting from the given port number
         '''
         while self.socket_num < 65535:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -77,7 +86,8 @@ class ProjectManager:
 
     def call_operation(self, operations):
         '''
-        Docfile
+        Handles socket TX. Typically sends operation requests by
+        sending each character to the binary, through the socket
         '''
         try:
             for char in operations:
@@ -87,7 +97,7 @@ class ProjectManager:
 
     def read_operation(self):
         '''
-        Docfile
+        Handles socket RX. Receives a message in a 2048 buffer
         '''
         try:
             msg = (self.manager_socket.recv(2048)).decode()
@@ -97,19 +107,23 @@ class ProjectManager:
 
     def __del__(self):
         '''
-        Docfile
+        Handles project being shutdown by freeing ports
         '''
         if self.manager_socket:
             self.manager_socket.close()
 
-################################################################################
+###########################################################
 # CAR CLASS
+###########################################################
 
 
 class Car:
     '''
-    Docfile
+    This class acts as a wrapper for the entire vehicle's system.
+    It holds each project and their respective port number. Interacts
+    through the ProjectManager class
     '''
+
     def __init__(self, projects):
         self.managers = {}
         minimum_port = 1024
@@ -118,7 +132,8 @@ class Car:
 
     def start_projects(self):
         '''
-        Docfile
+        This establishes the socket communication between each project
+        in the projects list
         '''
         socket_nums = {}
         for project, manager in self.managers.items():
@@ -127,13 +142,13 @@ class Car:
 
     def operate(self, project, operations):
         '''
-        Docfile
+        Handles socket TX for respective project
         '''
         self.managers[project].call_operation(operations)
 
     def read(self, project):
         '''
-        Docfile
+        Handles socket RX for respective project
         '''
         self.managers[project].read_operation()
 
@@ -141,79 +156,93 @@ class Car:
         for manager in self.managers.values():
             del manager
 
-
-################################################################################
+###########################################################
 # MAKE CAR
+###########################################################
+
+
 CAR = Car(PROJECTS)
-# Wrappers
+
+###########################################################
+# Wrapper Functions (User Interface)
+###########################################################
+
 
 def start_car():
     '''
-    Docfile
+    Establishes all sockets between client and x86 binaries
     '''
     CAR.start_projects()
 
+
 def gpio_set_state(project):
     '''
-    Docfile
+    Allows user to modify a pins state (Only if it's push/pull)
     '''
     pin_letter = input("Select your GPIO pin letter (0-2): ")
     pin_num = input("Select your GPIO pin number (0-15): ")
     state = input("Select if it is high or low (0/1): ")
     CAR.operate(str(project), f"0: 3, {pin_letter}, {pin_num}, {state}\n")
 
+
 def gpio_toggle(project):
     '''
-    Docfile
+    Allows user to toggle a pins state (Only if it's push/pull)
     '''
     pin_letter = input("Select your GPIO pin letter (0-2): ")
     pin_num = input("Select your GPIO pin number (0-15): ")
     CAR.operate(str(project), f"1: 2, {pin_letter}, {pin_num}\n")
 
+
 def gpio_toggle_it(project):
     '''
-    Docfile
+    Allows user to trigger a gpio interrupt (Not tested yet)
     '''
     pin_letter = input("Select your GPIO pin letter (0-3): ")
     pin_num = input("Select your GPIO pin number (0-15): ")
     CAR.operate(str(project), f"2: 2, {pin_letter}, {pin_num}\n")
 
+
 def adc_set_reading(project):
     '''
-    Docfile
+    Allows user to set an ADC reading (Not tested yet)
     '''
     pin_letter = input("Select your GPIO pin letter (0-3): ")
     pin_num = input("Select your GPIO pin number (0-15): ")
     adc_reading = input("Enter your ADC reading (x-y): ")
     CAR.operate(str(project), f"3: 3, {pin_letter}, {pin_num}, {adc_reading}\n")
 
+
 def i2c_set_reading(project):
     '''
-    Docfile
+    Allows user to set an I2C read (Not tested yet)
     '''
     i2c_port = input("Select your I2C port (0/1): ")
     i2c_data_size = input("Enter the size of your message: ")
     i2c_data = input("Enter the data to write: ")
     CAR.operate(str(project), f"4: 3, {i2c_port}, {i2c_data_size}, {i2c_data}\n")
 
+
 def spi_set_rx(project):
     '''
-    Docfile
+    Allows user to set a SPI read (Not tested yet)
     '''
     spi_port = input("Select your SPI port (0/1): ")
     spi_data_size = input("Enter the size of your message")
     spi_data = input("Enter the data to write: ")
     CAR.operate(str(project), f"5: 3, {spi_port}, {spi_data_size}, {spi_data}\n")
 
+
 def uart(project):
     '''
-    Docfile
+    Allows user to do something with UART (No x86 Infrastructure yet)
     '''
     print("x86 sim portion must be made")
 
+
 def gpio_read(project):
     '''
-    Docfile
+    Allows user to view all GPIO pin states/modes
     '''
     CAR.operate(str(project), f"7: 0\n")
     time.sleep(1)
