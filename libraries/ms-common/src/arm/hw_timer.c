7bac614c0fb91f632b4fc55b5cff84fcc04255ec
Akashem06 <aryankashem@gmail.com>
2024-03-15 01:34:37 +0000

    formatting

--- libraries/ms-common/src/arm/hw_timer.c
+++ libraries/ms-common/src/arm/hw_timer.c
@@ -24,18 +24,18 @@ void hw_timer_init(void) {
 }
 
 void hw_timer_delay_us(uint32_t us) {
-    TIM_SetCounter(TIM2, 0);
-    TIM_CCxCmd(TIM2, TIM_Channel_1, TIM_CCx_Enable);
-    TIM_SetCompare1(TIM2, us);
-    TIM_Cmd(TIM2, ENABLE);
+  TIM_SetCounter(TIM2, 0);
+  TIM_CCxCmd(TIM2, TIM_Channel_1, TIM_CCx_Enable);
+  TIM_SetCompare1(TIM2, us);
+  TIM_Cmd(TIM2, ENABLE);
 
-    while (!TIM_GetFlagStatus(TIM2, TIM_FLAG_CC1)) {
-        LOG_DEBUG("DELAYING\n");
-    }
+  while (!TIM_GetFlagStatus(TIM2, TIM_FLAG_CC1)) {
+    LOG_DEBUG("DELAYING\n");
+  }
 
-    TIM_ClearFlag(TIM2, TIM_FLAG_CC1);
-    TIM_CCxCmd(TIM2, TIM_Channel_1, TIM_CCx_Disable);
-    TIM_Cmd(TIM2, DISABLE);
+  TIM_ClearFlag(TIM2, TIM_FLAG_CC1);
+  TIM_CCxCmd(TIM2, TIM_Channel_1, TIM_CCx_Disable);
+  TIM_Cmd(TIM2, DISABLE);
 }
 
 void hw_timer_callback_us(uint32_t us, TimerCallback callback_function) {
