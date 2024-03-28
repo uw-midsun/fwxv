075e9a644907f62e6d12db548fc9c2ed1668cda8
Ernest Tang <tang.ernest@yahoo.ca>
2024-03-28 19:02:40 +0000

    Ernest completed fw103 task 1

--- projects/gpio/src/main.c
+++ projects/gpio/src/main.c
@@ -1,23 +1,55 @@
 #include <stdio.h>
 
+#include "delay.h"
+#include "gpio.h"
 #include "log.h"
 #include "master_task.h"
 #include "tasks.h"
 
-void pre_loop_init() {}
+GpioAddress addr1 = {
+  .port = GPIO_PORT_B,
+  .pin = 5,
+};
 
-void run_fast_cycle() {}
+GpioAddress addr2 = {
+  .port = GPIO_PORT_B,
+  .pin = 4,
+};
 
-void run_medium_cycle() {}
+GpioAddress addr3 = {
+  .port = GPIO_PORT_B,
+  .pin = 3,
+};
 
-void run_slow_cycle() {}
+GpioAddress addr4 = {
+  .port = GPIO_PORT_A,
+  .pin = 15,
+};
+
+TASK(GPIO_LED, TASK_STACK_512) {
+  LOG_DEBUG("GPIO_LED TASK INITIALIZED");
+  while (true) {
+    // Toggle LED state
+    gpio_toggle_state(&addr1);
+    gpio_toggle_state(&addr2);
+    gpio_toggle_state(&addr3);
+    gpio_toggle_state(&addr4);
+    delay_ms(1000);
+  }
+}
 
 int main() {
   tasks_init();
   log_init();
-  LOG_DEBUG("Welcome to TEST!");
+  LOG_DEBUG("Program start...\n");
 
-  init_master_task();
+  StatusCode gpio_init(void);
+  gpio_init_pin(&addr1, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
+  gpio_init_pin(&addr2, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
+  gpio_init_pin(&addr3, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
+  gpio_init_pin(&addr4, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
+
+  tasks_init_task(GPIO_LED, TASK_PRIORITY(1), NULL);
 
   tasks_start();
 
