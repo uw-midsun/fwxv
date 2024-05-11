6439a442abf641af9e240050c0aec45b180d779d
Your Name <youremail@example.com>
2024-05-11 18:28:59 +0000

    finish 103 bootcamp

--- projects/gpio_interrupts/src/main.c
+++ projects/gpio_interrupts/src/main.c
@@ -6,6 +6,10 @@
 #include "gpio.h"
 #include "log.h"
 #include "tasks.h"
+#include "interrupt.h"
+#include "gpio_it.h"
+
+#define BTN_INT_EVENT 5
 
 GpioAddress addr1 = {
   .port = GPIO_PORT_B,
@@ -27,29 +31,51 @@ GpioAddress addr4 = {
   .pin = 15,
 };
 
+GpioAddress buttonAddress = {
+  .port = GPIO_PORT_A,
+  .pin = 7
+};
+
+InterruptSettings led_interrupt = {
+  .priority = INTERRUPT_PRIORITY_NORMAL,
+  .type = INTERRUPT_TYPE_INTERRUPT,
+  .edge = INTERRUPT_EDGE_RISING
+};
+
 TASK(GPIO_LED, TASK_STACK_512) {
+  gpio_init_pin(&buttonAddress, GPIO_INPUT_PULL_DOWN , GPIO_STATE_LOW);
   gpio_init_pin(&addr1, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
   gpio_init_pin(&addr2, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
   gpio_init_pin(&addr3, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
   gpio_init_pin(&addr4, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
 
   while (true) {
-    gpio_toggle_state(&addr1);
-    vTaskDelay(pdMS_TO_TICKS(100));
-    gpio_toggle_state(&addr2);
-    vTaskDelay(pdMS_TO_TICKS(100));
-    gpio_toggle_state(&addr3);
-    vTaskDelay(pdMS_TO_TICKS(100));
-    gpio_toggle_state(&addr4);
-    vTaskDelay(pdMS_TO_TICKS(100));
+    uint32_t notification = 0;
+    notify_wait(&notification, BLOCK_INDEFINITELY);
 
+    if (notification & (1 << BTN_INT_EVENT)) {
+      gpio_toggle_state(&addr1);
+      vTaskDelay(pdMS_TO_TICKS(100));
+      gpio_toggle_state(&addr2);
+      vTaskDelay(pdMS_TO_TICKS(100));
+      gpio_toggle_state(&addr3);
+      vTaskDelay(pdMS_TO_TICKS(100));
+      gpio_toggle_state(&addr4);
+      vTaskDelay(pdMS_TO_TICKS(100));
+    }
   }
 }
 
 int main() {
   log_init();
   tasks_init();
+  interrupt_init();
   gpio_init();
+
+  gpio_it_register_interrupt(&buttonAddress, &led_interrupt, BTN_INT_EVENT, GPIO_LED);
+
+  tasks_init_task(GPIO_LED, TASK_PRIORITY(1), NULL);
+  tasks_start();
 
   return 0;
 }
