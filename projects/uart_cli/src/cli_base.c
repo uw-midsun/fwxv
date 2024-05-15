c16a1ceef359f11b951e37f7c9266ce4566dba3b
Ernest Tang <tang.ernest@yahoo.ca>
2024-05-15 16:30:58 +0000

    Fixed build error for UART CLI

--- projects/uart_cli/src/cli_base.c
+++ projects/uart_cli/src/cli_base.c
@@ -1,15 +1,16 @@
 #include "cli_base.h"
 
 #include <stdio.h>
-
-#include "string.h"
+#include <string.h>
 
 // Helper function to separate first argument from rest of string
 void tok_cmd(char *cmd_in, char *tok_out) {
   char tmp[MAX_CMD_LEN + 1] = { 0 };
   const char delim[2] = " ";
   strncpy(tmp, cmd_in, sizeof(tmp));
-  char *tmp_tok = strtok_r(tmp, delim, &tmp_tok);
+  char *tmp_tok;
+  tmp_tok = strtok(tmp, delim);  // NOLINT
+
   snprintf(tok_out, MAX_CMD_LEN + 1, "%s", tmp_tok);
   snprintf(cmd_in, MAX_CMD_LEN + 1, "%s", cmd_in + strlen(tmp_tok) + 1);
 }
