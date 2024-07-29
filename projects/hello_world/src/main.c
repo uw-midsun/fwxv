ad30ae1cee2797ccde5a53de57aae498f6f02cdb
Herman Gahra <hermangahra48@gmail.com>
2024-07-29 16:45:47 +0000

    Addressed comments

--- projects/hello_world/src/main.c
+++ projects/hello_world/src/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
+#define BUF_SIZE 10        // Any defined constants or preprocessor directives
+static int buf[BUF_SIZE];  // Any static/global declarations
 
-#define BUF_SIZE 10 // Any defined constants or preprocessor directives
-static int buf[BUF_SIZE]; // Any static/global declarations
-
-static int prv_my_func(int *input) { // Function declarations/definitions
+static int prv_my_func(int *input) {  // Function declarations/definitions
   (*input)++;
-  return *input; // Increments the value passed and returns it 
+  return *input;  // Increments the value passed and returns it
 }
 
 int main(void) {
