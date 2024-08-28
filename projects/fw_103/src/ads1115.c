0916948629e345cf15f11d424087a1e9607f700c
Bafran <aachinoy@gmail.com>
2024-08-28 10:49:20 -0700

    Removed the partial solution

--- projects/fw_103/src/ads1115.c
+++ projects/fw_103/src/ads1115.c
@@ -14,27 +14,23 @@ StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAd
   uint16_t cmd;
 
   // Write Config register
-  cmd = 0x8084; /* TODO: make setting this to continuous conversion an onboarding task */
+  /* TODO: fill out this value */
+  cmd = 0x0000;
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
 
-  /* TODO make this onboarding task*/
+  /* TODO (optional) */
   // Set low thresh to zero
   cmd = 0x0000;
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
 
-  /* TODO make this onboarding task*/
+  /* TODO (optional) */
   // Set high thresh to 1V
-  cmd = 1 / (6.25E-5);
+  cmd = 0x0000;
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
 
   // Register the ALRT pin
-  /* TODO make filling out the settings an onboarding task */
-  const InterruptSettings settings = {
-    .priority = INTERRUPT_PRIORITY_NORMAL,
-    .type = INTERRUPT_TYPE_INTERRUPT,
-    .edge = INTERRUPT_EDGE_FALLING,
-  };
-  gpio_it_register_interrupt(config->ready_pin, &settings, ALRT_EVENT, config->handler_task);
+  /* TODO (optional) */
+
   return STATUS_CODE_OK;
 }
 
@@ -46,20 +42,17 @@ StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channe
   uint16_t cmd;
 
   // Write Config register
-  cmd = 0x8084; /* TODO: make setting this to continuous conversion an onboarding task */
+  cmd = 0x0000;
   i2c_write_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
   return STATUS_CODE_OK;
 }
 
 StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, int16_t *reading) {
-  status_ok_or_return(i2c_read_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONVERSION,
-                                   (uint8_t *)reading, 2));
+  /* TODO: complete function */
   return STATUS_CODE_OK;
 }
 
 StatusCode ads1115_read_converted(ADS1115_Config *config, ADS1115_Channel channel, float *reading) {
-  int16_t tmp;
-  status_ok_or_return(ads1115_read_raw(config, channel, &tmp));
-  *reading = tmp * (6.25E-5);
+  /* TODO: complete function */
   return STATUS_CODE_OK;
 }
--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -11,46 +11,14 @@
 
 #include <stdio.h>
 
-#include "ads1115.h"
-#include "delay.h"
-#include "gpio.h"
 #include "log.h"
 #include "tasks.h"
-
-// Task to handle our ADC reads
-TASK(adc_task, TASK_STACK_256) {
-  /* TODO: Make filling this out part of onboarding */
-  GpioAddress ready_pin = {
-    .port = GPIO_PORT_B,
-    .pin = GPIO_Pin_0,
-  };
-
-  /* TODO: Make filling this out part of onboarding */
-  ADS1115_Config config = {
-    .handler_task = adc_task,
-    .i2c_addr = ADS1115_ADDR_GND,
-    .i2c_port = ADS1115_I2C_PORT,
-    .ready_pin = &ready_pin,
-  };
-
-  while (true) {
-    /* TODO: Make filling this out part of onboarding */
-    LOG_DEBUG("Running ADC read\n");
-    // Read ADC
-    float reading;
-    ads1115_read_converted(&config, ADS1115_CHANNEL_0, &reading);
-    LOG_DEBUG("ADC read %f\n", reading);
-    delay_ms(100);
-  }
-}
 
 int main() {
   tasks_init();
   log_init();
   gpio_init();
   LOG_DEBUG("Welcome to FW 103!\n");
-
-  tasks_init_task(adc_task, TASK_PRIORITY(2), NULL);
 
   tasks_start();
 
