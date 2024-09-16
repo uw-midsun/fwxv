64bb5fd125314405961ffdfdd275a3d0a3430538
Anmeet Sekhon <anmeet31@gmail.com>
2024-09-16 04:44:47 +0000

    Part 1 of 103 Hw Completed

--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -14,10 +14,31 @@
 #include "log.h"
 #include "tasks.h"
 
+#include "gpio.h"
+#include "delay.h"
+
+// GpioAddress struct
+GpioAddress led_addr = {
+  .port = GPIO_PORT_B,
+  .pin = 3,
+};
+
+TASK(led_gpio_task, TASK_STACK_512) {
+  gpio_init_pin(&led_addr, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
+  while(true) {
+    gpio_toggle_state(&led_addr);
+    LOG_DEBUG("Toggling LED\n"); // Added this line to see if something is even happening in the scons build
+    delay_ms(1000);
+  }
+}
+
 int main() {
   tasks_init();
   log_init();
   gpio_init();
+
+  tasks_init_task(led_gpio_task, TASK_PRIORITY(1), NULL);
+
   LOG_DEBUG("Welcome to FW 103!\n");
 
   tasks_start();
