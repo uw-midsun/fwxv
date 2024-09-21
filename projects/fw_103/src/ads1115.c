efbf8837661c432a2c66d4a47b6ff3d398464a22
Kien Nguyen <nguyenhuutrungkien2105@gmail.com>
2024-09-21 16:23:15 -0400

    Finished FW 103

--- projects/fw_103/src/ads1115.c
+++ projects/fw_103/src/ads1115.c
@@ -1,6 +1,8 @@
 #include "ads1115.h"
 
 #include "gpio_it.h"
+#include "log.h"
+
 #include "i2c.h"
 #include "status.h"
 
@@ -15,7 +17,7 @@ StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAd
 
   // Write Config register
   /* TODO: fill out this value */
-  cmd = 0x0000;
+  cmd = 0x0011;
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
 
   /* TODO (optional) */
@@ -49,10 +51,21 @@ StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channe
 
 StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, int16_t *reading) {
   /* TODO: complete function */
+  i2c_read_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONVERSION, (uint8_t *) reading, 8);
   return STATUS_CODE_OK;
 }
 
 StatusCode ads1115_read_converted(ADS1115_Config *config, ADS1115_Channel channel, float *reading) {
   /* TODO: complete function */
+  uint16_t reader ;
+  
+  ads1115_read_raw(config, channel,(int16_t *) &reader);
+
+  if (reader <= 32768) {
+    *reading = -1 * (reader/ 65535)*4.096;
+  } else {
+    *reading = (reader/ 65535)*4.096;
+  }
+
   return STATUS_CODE_OK;
 }
--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -14,12 +14,55 @@
 #include "log.h"
 #include "tasks.h"
 
+#include "gpio.h"
+#include "delay.h"
+
+#include "ads1115.h"
+
+static GpioAddress led_addr = {
+  .port = GPIO_PORT_B,
+  .pin = 3
+};
+
+
+
+TASK(gpioTask, TASK_STACK_512) {
+  GpioAddress ready_pin = {
+    .port = GPIO_PORT_B,
+    .pin = GPIO_Pin_0,
+  };
+
+  ADS1115_Config config = {
+    .handler_task = gpioTask,
+    .i2c_addr = ADS1115_ADDR_GND,
+    .i2c_port = ADS1115_I2C_PORT,
+    .ready_pin = &ready_pin,
+  };
+
+  gpio_init_pin(&led_addr, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_HIGH);
+  GpioState temp_state;
+  float reader;
+  while (true) {
+    gpio_toggle_state(&led_addr);
+
+    gpio_get_state(&led_addr, &temp_state);
+
+    // LOG_DEBUG("State: %d\n", temp_state);
+    ads1115_read_converted(&config, ADS1115_CHANNEL_1, &reader);
+
+    LOG_DEBUG("reader value: %f\n", reader);
+
+    delay_s(1);
+  }
+}
+
 int main() {
   tasks_init();
   log_init();
   gpio_init();
   LOG_DEBUG("Welcome to FW 103!\n");
 
+  tasks_init_task(gpioTask, TASK_PRIORITY(2), NULL);
   tasks_start();
 
   LOG_DEBUG("exiting main?\n");
