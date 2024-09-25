39fd0fc17d1051b214312d97d02f7eb332bccde2
brendan <tambrendan05@gmail.com>
2024-09-25 22:17:51 +0000

    h

--- projects/fw_103/src/ads1115.c
+++ projects/fw_103/src/ads1115.c
@@ -15,7 +15,8 @@ StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAd
 
   // Write Config register
   /* TODO: fill out this value */
-  cmd = 0x0000;
+  // 0000 0100 1000 0011
+  cmd = 0x0483;
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
 
   /* TODO (optional) */
@@ -49,10 +50,19 @@ StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channe
 
 StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, int16_t *reading) {
   /* TODO: complete function */
+
+  uint8_t data;
+  i2c_read_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONVERSION, &data, 2);
+  *reading = data;
+
   return STATUS_CODE_OK;
 }
 
 StatusCode ads1115_read_converted(ADS1115_Config *config, ADS1115_Channel channel, float *reading) {
-  /* TODO: complete function */
+  
+  int16_t raw_reading;	
+  ads1115_read_raw(config, channel, &raw_reading);
+  *reading = ((raw_reading/65535.0)*4.096) - 2.048;
+
   return STATUS_CODE_OK;
 }
--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -11,8 +11,44 @@
 
 #include <stdio.h>
 
+#include "ads1115.h"
+#include "delay.h"
 #include "log.h"
 #include "tasks.h"
+#include "gpio.h"
+
+GpioAddress led_addr = {
+  .port = GPIO_PORT_B,
+  .pin = 3,
+};
+
+ADS1115_Config config = {
+  .handler_task = t1,
+  .i2c_addr = ADS1115_ADDR_GND,
+  .i2c_port = ADS1115_I2C_PORT,
+  .ready_pin = &ready_pin,
+};
+
+TASK(t1, TASK_STACK_512) {
+  gpio_init_pin(&led_addr, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_HIGH);
+  
+  while(true) {
+    gpio_toggle_state(&led_addr);
+    GpioState pin_state;
+    gpio_get_state(&led_addr, &pin_state);
+    if (pin_state == GPIO_STATE_HIGH) {
+      LOG_DEBUG("on ");
+    } else {
+      LOG_DEBUG("off ");
+    }
+
+    float reading;
+    ads1115_read_converted(&config, ADS1115_CHANNEL_0, &reading);
+    LOG_DEBUG("%.6f\n", reading);
+
+    delay_ms(1000);
+  }
+}
 
 int main() {
   tasks_init();
@@ -20,6 +56,8 @@ int main() {
   gpio_init();
   LOG_DEBUG("Welcome to FW 103!\n");
 
+  tasks_init_task(t1, TASK_PRIORITY(2), NULL);
+  
   tasks_start();
 
   LOG_DEBUG("exiting main?\n");
