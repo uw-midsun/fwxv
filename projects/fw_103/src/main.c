0adcfc82a14891f77213caecb20bf3306c0ea757
Harman <harmanshienh01@gmail.com>
2024-09-29 17:45:54 +0000

    Onboarding 103

--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -11,16 +11,46 @@
 
 #include <stdio.h>
 
+#include "ads1115.h"
+#include "delay.h"
+#include "gpio.h"
 #include "log.h"
 #include "tasks.h"
+
+GpioAddress led_addr = {
+  .port = GPIO_PORT_B,
+  .pin = 3,
+};
+
+GpioAddress ready_pin = {
+  .port = GPIO_PORT_B,
+  .pin = GPIO_Pin_0,
+};
+
+ADS1115_Config config = {
+  .handler_task = "fw_103",
+  .i2c_addr = ADS1115_ADDR_GND,
+  .i2c_port = ADS1115_I2C_PORT,
+  .ready_pin = &ready_pin,
+};
 
 int main() {
   tasks_init();
   log_init();
   gpio_init();
+  gpio_init_pin(&led_addr, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
   LOG_DEBUG("Welcome to FW 103!\n");
 
   tasks_start();
+
+  GpioState out_state;
+  while (true) {
+    // Sets the value of out_state to current state of input pin at my_address
+    gpio_get_state(&led_addr, &out_state);
+    gpio_toggle_state(&led_addr);
+    delay_ms(1000);
+    ads1115_read_converted(&config, &config.i2c_addr, 65535);
+  };
 
   LOG_DEBUG("exiting main?\n");
   return 0;
