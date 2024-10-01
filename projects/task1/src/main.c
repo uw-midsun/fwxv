d44ee4b43505442b636590054584f803745e09de
Adityya Kaushal <adityyakaushal@gmail.com>
2024-09-30 21:26:29 -0400

    Completed queues task

---
+++ projects/task1/src/main.c
@@ -0,0 +1,74 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "delay.h"
+#include "log.h"
+#include "misc.h"
+#include "queues.h"
+#include "status.h"
+#include "tasks.h"
+
+#define ITEM_SZ 6
+#define QUEUE_LEN 5
+#define BUF_SIZE (QUEUE_LEN * ITEM_SZ)
+
+static char s_list[QUEUE_LEN][ITEM_SZ] = { "Item1", "Item2", "Item3", "Item4", "Item5" };
+
+// Task static entities
+static uint8_t s_queue1_buf[BUF_SIZE];
+static Queue s_queue1 = {
+  .num_items = QUEUE_LEN,
+  .item_size = ITEM_SZ,
+  .storage_buf = s_queue1_buf,
+};
+
+TASK(task1, TASK_STACK_512) {
+  LOG_DEBUG("Task 1 initialized!\n");
+  StatusCode ret = STATUS_CODE_OK;
+  int index = 0;
+  while (true) {
+    if (index < QUEUE_LEN) {
+      ret = queue_send(&s_queue1, &s_list[index], 0);
+    } else {
+      index = 0;
+    }
+    index++;
+    if (ret != STATUS_CODE_OK) {
+      LOG_DEBUG("write to queue failed");
+    }
+    LOG_DEBUG("Sent %s\n", s_list[index]);
+    delay_ms(100);
+  }
+}
+
+TASK(task2, TASK_STACK_512) {
+  LOG_DEBUG("Task 2 initialized!\n");
+  char outstr[ITEM_SZ];
+  StatusCode ret = STATUS_CODE_OK;
+  while (true) {
+    // Your code goes here
+    ret = queue_receive(&s_queue1, &outstr, 1000);
+    if (ret != STATUS_CODE_OK) {
+      LOG_DEBUG("read from queue failed");
+    }
+    else {
+      LOG_DEBUG("%s\n", outstr);
+    }
+  }
+}
+
+int main(void) {
+  log_init();
+  // Initialize queues here
+  tasks_init();
+  queue_init(&s_queue1);
+
+  tasks_init_task(task1, TASK_PRIORITY(2), NULL);
+  tasks_init_task(task2, TASK_PRIORITY(2), NULL);
+
+  LOG_DEBUG("Program start...\n");
+  tasks_start();
+
+  return 0;
+}
