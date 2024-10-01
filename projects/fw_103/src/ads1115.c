0ebeaf907a6d7dafca50d9ea4abecc3ac3e77bec
vagrant <vagrant@midsunbox>
2024-10-01 21:28:22 +0000

    completed task 103

--- projects/fw_103/src/ads1115.c
+++ projects/fw_103/src/ads1115.c
@@ -1,5 +1,5 @@
 #include "ads1115.h"
-
+#include "log.h"
 #include "gpio_it.h"
 #include "i2c.h"
 #include "status.h"
@@ -14,19 +14,22 @@ StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAd
   uint16_t cmd;
 
   // Write Config register
-  /* TODO: fill out this value */
-  cmd = 0x0000;
+  /* TODO: fill out this value 
+  1,000,010,0,100,0,0,0,11*/ 
+  
+
+  cmd = 0x8483;
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
 
   /* TODO (optional) */
   // Set low thresh to zero
-  cmd = 0x0000;
+  cmd = 0x8000;
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
 
   /* TODO (optional) */
   // Set high thresh to 1V
-  cmd = 0x0000;
-  i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
+  cmd = 0x7FFF;
+  i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_HI_THRESH, (uint8_t *)(&cmd), 2);
 
   // Register the ALRT pin
   /* TODO (optional) */
@@ -38,21 +41,24 @@ StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channe
   if (config == NULL) {
     return status_code(STATUS_CODE_INVALID_ARGS);
   }
-
   uint16_t cmd;
+  i2c_read_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
+  cmd &= ~(0b111 << 12); 
+  cmd |= (channel << 12);
 
-  // Write Config register
-  cmd = 0x0000;
   i2c_write_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
   return STATUS_CODE_OK;
 }
 
-StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, int16_t *reading) {
-  /* TODO: complete function */
-  return STATUS_CODE_OK;
+StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, uint16_t *reading) {
+  ads1115_select_channel(config, ADS1115_CHANNEL_0);
+  return i2c_read_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONVERSION, (uint8_t*)reading, 2);
 }
 
 StatusCode ads1115_read_converted(ADS1115_Config *config, ADS1115_Channel channel, float *reading) {
-  /* TODO: complete function */
+  uint16_t raw_reading;
+  StatusCode status = ads1115_read_raw(config, channel, &raw_reading);  
+  float converted_reading = (raw_reading * 4.096/65536) - 2.048;
+  LOG_DEBUG("%.8f", converted_reading);
   return STATUS_CODE_OK;
 }
--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -10,9 +10,40 @@
 */
 
 #include <stdio.h>
-
+#include "gpio.h"
 #include "log.h"
 #include "tasks.h"
+#include "delay.h"
+#include"ads1115.h"
+
+ADS1115_Config config;
+
+TASK(read, TASK_STACK_512){
+  float converted_reading;
+  while(true){
+    StatusCode status = ads1115_read_converted(&config, ADS1115_CHANNEL_0, &converted_reading);
+    LOG_DEBUG("%.6f", converted_reading);
+    delay_ms(1000);
+  }
+   
+};
+
+TASK(setup, TASK_STACK_512){
+  GpioAddress ready_pin = {
+  .port = GPIO_PORT_B,
+  .pin = GPIO_Pin_0,
+};
+
+  config.handler_task = read;
+  config.i2c_addr = ADS1115_ADDR_GND;
+  config.i2c_port = ADS1115_I2C_PORT;
+  config.ready_pin = &ready_pin;
+StatusCode status = ads1115_init(&config, config.i2c_addr, &ready_pin);
+}
+
+
+
+
 
 int main() {
   tasks_init();
@@ -20,6 +51,8 @@ int main() {
   gpio_init();
   LOG_DEBUG("Welcome to FW 103!\n");
 
+  tasks_init_task(setup, TASK_PRIORITY(1), NULL);
+  tasks_init_task(read, TASK_PRIORITY(2), NULL);
   tasks_start();
 
   LOG_DEBUG("exiting main?\n");
