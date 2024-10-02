1cd45d0631a79100d379d2397b5758b2b8eb0c0f
Benjamin Dong <b34dong@uwaterloo.ca>
2024-10-02 22:47:59 +0000

    Benjamin Dong FW 102 Task 1

---
+++ projects/task1/src/main.c
@@ -0,0 +1,46 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "delay.h"
+#include "FreeRTOS.h"
+#include "tasks.h"
+
+#include "gpio.h"
+#include "log.h"
+#include "misc.h"
+#include "delay.h"
+
+// Non blocking delay. Simply consumes cpu cycles until a given time has passed
+static void prv_delay(const TickType_t delay_ms) {
+  TickType_t curr_tick = xTaskGetTickCount();
+  while(xTaskGetTickCount() - curr_tick < pdMS_TO_TICKS(delay_ms))
+  {}
+}
+
+TASK(task1, TASK_STACK_512) {
+  int counter1 = 0;
+  while (true) {
+    LOG_DEBUG("task1 %d\n", counter1);
+    counter1++;
+    delay_ms(125);
+  }
+}
+
+TASK(task2, TASK_STACK_512) {
+  int counter2 = 0;
+  while (true) {
+    LOG_DEBUG("task2 %d\n", counter2);
+    counter2++;
+    prv_delay(125);
+  }
+}
+
+int main(void) {
+    log_init();
+    tasks_init();
+    tasks_init_task(task1, TASK_PRIORITY(2), NULL);
+    tasks_init_task(task2, TASK_PRIORITY(1), NULL);
+    LOG_DEBUG("Program start...\n");
+    tasks_start();
+
+    return 0;
+}
