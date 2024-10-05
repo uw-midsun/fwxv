2951d1edb811d0ce0b6a2cdc9c1e32cfe2c91f28
Joelle Lee <j947lee@uwaterloo.ca>
2024-10-05 19:24:07 +0000

    writing metadata

--- projects/bootloader/scripts/ping_application.py
+++ projects/bootloader/scripts/ping_application.py
@@ -1,4 +1,12 @@
-from bootloader import *
+from bootloader_id import *
+from can_datagram import Datagram, DatagramSender
+
+def string_to_bytearray(self, string: str) -> bytearray:
+    data = []
+    for i in range(len(string)):
+        data.append((string[i] >> (i * 8)) & 0xff)
+        # [(something[i] >> (i * 8)) & 0xff for i in range(len(something))]
+    return bytearray(data)
 
 class Ping_Application:
     def __init__(self, sender=None) -> None:
@@ -7,34 +15,35 @@ class Ping_Application:
         else:
             self._sender = DatagramSender()
 
-    def string_to_bytearray(self, string: str) -> bytearray:
-        data = []
-        for i in range(len(string)):
-            data.append((string[i] >> (i * 8)) & 0xff)
-            # [(something[i] >> (i * 8)) & 0xff for i in range(len(something))]
-        return bytearray(data)
-
     def ping_application(self, **kwargs):
-        node_id = kwargs["node_id"]
+        node_id = kwargs.get("node_id")
 
         # Represents the ping_type, along with branch and project names
-        ping_type = kwargs["ping_type"]
-        branch = kwargs["branch"]
-        project = kwargs["project"]
+        ping_type = kwargs.get("ping_type")
+        branch = kwargs.get("branch")
+        project = kwargs.get("project")
 
-        print(f"Starting ping application process for boards {node_ids}...")
+        meta_datagram = Datagram(
+            datagram_type_id=METADATA,
+            node_ids=0,
+            data=string_to_bytearray(len(branch) if (len(branch)) else len(project))
+        )
+
+        print(f"Starting ping application process for boards {node_id}...")
 
         byte_data = None
         datagram_type = None
 
         if ping_type == "branch":
+            self._sender.send(meta_datagram)
             byte_data = string_to_bytearray(branch)
-            datagram_type = bootloader_id.BRANCH
+            datagram_type = BRANCH
         elif ping_type == "project":
+            self._sender.send(meta_datagram)
             byte_data = string_to_bytearray(project)
-            datagram_type = bootloader_id.PROJECT
+            datagram_type = PROJECT
         elif ping_type == "node_id":
-            datagram_type = bootloader_id.NODE_ID
+            datagram_type = NODE_ID
 
         datagram = Datagram(
             datagram_type_id=datagram_type,
@@ -42,5 +51,5 @@ class Ping_Application:
             data=byte_data
         )
         self._sender.send(datagram)
-        print(f"Ping application completed for boards {node_ids}")
+        print(f"Ping application completed for boards {node_id}")
 
