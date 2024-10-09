6e9d8538ab353b89a384d20fe7e88b6ecec21357
benji-do <ben.y.dong.2019@gmail.com>
2024-10-09 20:10:22 +0000

    Benjamin Dong Fw 103 tasks commit

--- projects/fw_103/src/ads1115.c
+++ projects/fw_103/src/ads1115.c
@@ -1,5 +1,5 @@
 #include "ads1115.h"
-
+#include "i2c.h"
 #include "gpio_it.h"
 #include "i2c.h"
 #include "status.h"
@@ -12,20 +12,20 @@ StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAd
   config->i2c_addr = i2c_addr;
 
   uint16_t cmd;
-
+  
   // Write Config register
   /* TODO: fill out this value */
-  cmd = 0x0000;
+  cmd = 0b0000010010000011; //check if this is right
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
 
   /* TODO (optional) */
   // Set low thresh to zero
-  cmd = 0x0000;
+  cmd = 0x8000;
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
 
   /* TODO (optional) */
   // Set high thresh to 1V
-  cmd = 0x0000;
+  cmd = 0x7FFF;
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
 
   // Register the ALRT pin
@@ -42,17 +42,20 @@ StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channe
   uint16_t cmd;
 
   // Write Config register
-  cmd = 0x0000;
+  cmd = 0b0000010010000011;
   i2c_write_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
   return STATUS_CODE_OK;
 }
 
 StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, int16_t *reading) {
   /* TODO: complete function */
+  i2c_read_reg(config->i2c_port, config->i2c_addr, 0x00, *reading, 2); // my read raw function
   return STATUS_CODE_OK;
 }
 
 StatusCode ads1115_read_converted(ADS1115_Config *config, ADS1115_Channel channel, float *reading) {
   /* TODO: complete function */
+  float converted_voltage = *reading * 4.096 / 65535;
+  i2c_read_reg(config->i2c_port, config->i2c_addr, 0x00, &converted_voltage, 2); // NO CLUE IF THIS WORKS
   return STATUS_CODE_OK;
 }
--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -8,18 +8,44 @@
     - ADC task to periodically measure the voltage of channel 0
     - Overvoltage interrupt (configure the interrupt to be on channel 0 with thresholds of 0V - 1V)
 */
-
+#include "ads1115.h"
 #include <stdio.h>
-
+#include "gpio.h"
 #include "log.h"
 #include "tasks.h"
+#include "delay.h"
+
+GpioAddress led_addr = {
+  .port = GPIO_PORT_B,
+  .pin = 3,
+};
+TASK(task1, TASK_STACK_512) {
+  gpio_init_pin(&led_addr, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
+  GpioAddress ready_pin = {
+    .port = GPIO_PORT_B,
+    .pin = GPIO_Pin_0,
+  };
+  ADS1115_Config config = {
+    .handler_task = task1,
+    .i2c_addr = ADS1115_ADDR_GND,
+    .i2c_port = ADS1115_I2C_PORT,
+    .ready_pin = &ready_pin,
+  };
+  while (true) {
+    gpio_toggle_state(&led_addr);
+    LOG_DEBUG("\nblink!");
+    delay_ms(1000);
+
+  }
+}
+
 
 int main() {
   tasks_init();
   log_init();
   gpio_init();
   LOG_DEBUG("Welcome to FW 103!\n");
-
+  tasks_init_task(task1, TASK_PRIORITY(1), NULL);
   tasks_start();
 
   LOG_DEBUG("exiting main?\n");
