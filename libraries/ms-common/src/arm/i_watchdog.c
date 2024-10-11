4afffef90425a79bc07c06a514843d271ffbc7ff
Adityya Kaushal <adityyakaushal@gmail.com>
2024-10-11 13:46:31 -0400

    Fixed compilation errors

--- libraries/ms-common/src/arm/i_watchdog.c
+++ libraries/ms-common/src/arm/i_watchdog.c
@@ -3,20 +3,20 @@
 #include "stm32f10x_iwdg.h"
 
 // This is the time that we want the clock to reset after
-#define RELOAD_TIME ((uint16_t)3000)
+#define RELOAD_TIME 3000
 
 // This is a prescaler value that scales a 32 kHz clock (LSI)
-#define PRESCALER_VALUE (IWDG_Prescaler_32)
+#define PRESCALER_VALUE IWDG_Prescaler_32
 
 // To get the reload value based on how the prescaler is defined
-#define RELOAD_VALUE ((uint16_t)(REALOAD_TIME(32000 / PRESCALER_VALUE))
+#define RELOAD_VALUE (uint16_t)(RELOAD_TIME * (32000 / PRESCALER_VALUE))
 
 void IWDG_Start(void) {
   IWDG_Enable();
   IWDG_SetPrescaler(PRESCALER_VALUE);
-  IWDG_SetReload(SET_RELOAD_VALUE);
+  IWDG_SetReload(RELOAD_VALUE);
 }
 
-void IWDG_Reload() {
+void IWDG_Reload(void) {
   IWDG_ReloadCounter();
 }
