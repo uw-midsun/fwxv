bd422b3128656ab537ed240f71361474ca2b90c4
Brandon Ling <79050338+bzzling@users.noreply.github.com>
2024-10-17 02:27:08 +0000

    formatting

--- projects/task1/src/queues1.c
+++ projects/task1/src/queues1.c
@@ -8,13 +8,10 @@
 #include "queues.h"
 #include "status.h"
 #include "tasks.h"
-
 #define ITEM_SZ 6
 #define QUEUE_LEN 5
 #define BUF_SIZE (QUEUE_LEN * ITEM_SZ)
-
 static const char s_list[QUEUE_LEN][ITEM_SZ] = { "Item1", "Item2", "Item3", "Item4", "Item5" };
-
 // Task static entities
 static uint8_t s_queue1_buf[BUF_SIZE];
 static Queue s_queue1 = {
@@ -22,7 +19,6 @@ static Queue s_queue1 = {
   .item_size = ITEM_SZ,
   .storage_buf = s_queue1_buf,
 };
-
 TASK(task1, TASK_STACK_512) {
   LOG_DEBUG("Task 1 initialized!\n");
   StatusCode ret;
@@ -42,7 +38,6 @@ TASK(task1, TASK_STACK_512) {
     }
   }
 }
-
 TASK(task2, TASK_STACK_512) {
   LOG_DEBUG("Task 2 initialized!\n");
   const char outstr[ITEM_SZ];
@@ -58,19 +53,14 @@ TASK(task2, TASK_STACK_512) {
     delay_ms(100);
   }
 }
-
 int main(void) {
   log_init();
   tasks_init();
   queue_init(&s_queue1);
-
   // Initialize queues here
-
   tasks_init_task(task1, TASK_PRIORITY(2), NULL);
   tasks_init_task(task2, TASK_PRIORITY(2), NULL);
-
   LOG_DEBUG("Program start...\n");
   tasks_start();
-
   return 0;
 }
