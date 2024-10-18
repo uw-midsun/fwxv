73c243e66ebf618986fe4b433183037bd6cc6c6f
Adityya Kaushal <adityyakaushal@gmail.com>
2024-10-18 19:40:30 -0400

    simplifying runtime calculation

--- libraries/ms-common/src/arm/i_watchdog.c
+++ libraries/ms-common/src/arm/i_watchdog.c
@@ -10,7 +10,9 @@ void IWDG_SetValues(IWDGPrescalerValue prescaler_value, uint16_t reload_period_m
   IWDG_SetPrescaler((uint8_t)prescaler_value);
 
   // Set the reload value using #define from above and write it into IWDG_WLR register
-  IWDG_SetReload(((reload_period_ms * 40000) / ((4 * (1 << prescaler_value)) * 1000)));
+  // ((reload_period_ms * 40000) / ((4 * (1 << prescaler_value)) * 1000)) <- The calculation used
+  // but then simplified
+  IWDG_SetReload((reload_period_ms / (1 << prescaler_value)) * 10);
 
   // Reload the IWDG with the value set previously in IWDG_SetReload
   IWDG_ReloadCounter();
