4cfffc4cd824bc68089d5b1ab57efb6af35f9950
vagrant <vagrant@midsunbox>
2024-10-30 21:53:06 +0000

    get accel data & accel offset gain

--- projects/imu/inc/bmi323.h
+++ projects/imu/inc/bmi323.h
@@ -7,11 +7,55 @@
 #define READ 0x80
 #define WRITE 0x7F
 #define DUMMY_BYTE = 0x00
+#define BMI_SET_LOW_BYTE = UINT16_C(0x00FF)
+#define BMI_SET_HIGH_BYTE = UINT16_C(0xFF00)
+
+
+/*
+this might be 13 or 14 idk
+#define BMI3_ACC_DP_OFF_XYZ_13_BIT_MASK              UINT16_C(0x1FFF)
+#define BMI3_ACC_DP_OFF_XYZ_14_BIT_MASK              UINT16_C(0x3FFF)
+*/
+#define BMI3_ACC_DP_DOFFSET_X_MASK UINT16_C(0xFFFF)
+#define BMI3_ACC_DP_DOFFSET_Y_MASK UINT16_C(0xFFFF)
+#define BMI3_ACC_DP_DOFFSET_Z_MASK UINT16_C(0xFFFF)
+
+#define BMI3_ACC_DP_DGAIN_X_MASK UINT16_C(0x00FF)
+#define BMI3_ACC_DP_DGAIN_Y_MASK UINT16_C(0x00FF)
+#define BMI3_ACC_DP_DGAIN_Z_MASK UINT16_C(0x00FF)
 
 typedef enum{
   REG_BANK_SEL = 0x7F,
-  GYRO_REG_ADDR = 0x06
+  GYRO_REG_ADDR = 0x06,
+  ACCEL_REG_ADDR = 0x03
 }bmi323_registers;
+
+typedef enum {
+    ACC_DP_OFF_X = 0x60,
+    ACC_DP_DGAIN_X = 0x61,
+    ACC_DP_OFF_Y = 0x62,
+    ACC_DP_DGAIN_Y = 0x63,
+    ACC_DP_OFF_Z = 0x64,
+    ACC_DP_DGAIN_Z = 0x65
+}accel_og;
+
+typedef enum {
+    GYR_DP_OFF_X = 0x66,
+    GYR_DP_DGAIN_X = 0x67,
+    GYR_DP_OFF_Y = 0x68,
+    GYR_DP_DGAIN_Y = 0x69,
+    GYR_DP_OFF_Z = 0x6A,
+    GYR_DP_DGAIN_Z = 0x6B
+}gyro_og;
+
+struct accel_gain_offset{
+  uint16_t accel_offset_x;
+  uint16_t accel_offset_y;
+  uint16_t accel_offset_z;
+  uint16_t accel_gain_x;
+  uint16_t accel_gain_y;
+  uint16_t accel_gain_z;
+};
 
 typedef struct {
   SpiPort spi_port;
@@ -25,7 +69,7 @@ typedef struct {
 
 typedef struct {
   bmi323_settings *settings;
-
+  axes accel;
   axes gyro;
 } bmi323_storage;
 
--- projects/imu/src/bmi323.c
+++ projects/imu/src/bmi323.c
@@ -1,4 +1,5 @@
 #include "bmi323.h"
+
 static bmi323_storage *s_storage;
 
 
@@ -47,18 +48,92 @@ static StatusCode set_multi_register(uint16_t reg_addr, uint16_t *value, uint16_
 }
 
 static StatusCode get_gyroscope_data(axes *gyro) {
-    uint16_t temp[6];
+    uint16_t data[6];
 
-    StatusCode status = get_multi_register(GYRO_REG_ADDR, &temp, 6);
+    StatusCode status = get_multi_register(GYRO_REG_ADDR, &data, 6);
 
     if (status == STATUS_CODE_OK) {
         //(int16_t)((msb << 8) | lsb);
-        gyro->x = (int16_t)((temp[0] << 8) | temp[1]);
-        gyro->y = (int16_t)((temp[2] << 8) | temp[3]);
-        gyro->z = (int16_t)((temp[4] << 8) | temp[5]);
+        gyro->x = (int16_t)((data[0] << 8) | data[1]);
+        gyro->y = (int16_t)((data[2] << 8) | data[3]);
+        gyro->z = (int16_t)((data[4] << 8) | data[5]);
 
         return STATUS_CODE_OK;
     }
 
     return status;
+}
+
+static StatusCode get_accel_data(axes *accel){
+    uint16_t data[6];
+    StatusCode status = get_multi_register(ACCEL_REG_ADDR, &data, 6);
+
+    if(status = STATUS_CODE_OK){
+        accel->x = (int16_t)((data[0] << 8) | data[1]);
+        accel->y = (int16_t)((data[2] << 8) | data[3]);
+        accel->z = (int16_t)((data[4] << 8) | data[5]);
+    }
+
+
+    return status;
+}
+
+static StatusCode get_accel_offset_gain(struct accel_gain_offset *accel_go){
+    StatusCode result;
+    uint8_t data[12] = {0};
+
+    uint16_t accel_off_x, accel_off_y, accel_off_z;
+    uint8_t accel_gain_x, accel_gain_y, accel_gain_z;
+
+    if(accel_go != NULL){
+        result = get_multi_register(ACC_DP_OFF_X, data, 12);
+
+        if(result == STATUS_CODE_OK){
+            accel_off_x = (uint16_t)(data[0] << 8 | data[1]);
+            accel_gain_x = (uint16_t)data[2];
+            accel_off_y = (uint16_t)(data[4] << 8 | data[5]);
+            accel_gain_y = (uint16_t)data[6];
+            accel_off_z = (uint16_t)(data[8] << 8 | data[9]);
+            accel_gain_z = (uint16_t)data[10];
+
+            accel_go -> accel_offset_x = (accel_off_x & BMI3_ACC_DP_DOFFSET_X_MASK);
+            accel_go -> accel_gain_x = (accel_gain_x & BMI3_ACC_DP_DGAIN_X_MASK);
+            accel_go -> accel_offset_y = (accel_off_y & BMI3_ACC_DP_DOFFSET_Y_MASK);
+            accel_go -> accel_gain_y = (accel_gain_y & BMI3_ACC_DP_DGAIN_Y_MASK);
+            accel_go -> accel_offset_z = (accel_off_z & BMI3_ACC_DP_DOFFSET_Z_MASK);
+            accel_go -> accel_gain_z = (accel_gain_z & BMI3_ACC_DP_DGAIN_Z_MASK);
+        }
+    }
+
+    return result;
+}
+
+static StatusCode set_accel_offset_gain(struct accel_gain_offset *accel_go){
+    StatusCode result;
+
+    uint8_t data[12] = {0};
+    
+    uint16_t accel_off_x, accel_off_y, accel_off_z;
+    uint8_t accel_gain_x, accel_gain_y, accel_gain_z;
+
+    accel_off_x = accel_go -> accel_offset_x & BMI3_ACC_DP_DOFFSET_X_MASK;
+    accel_gain_x = accel_go -> accel_gain_x & BMI3_ACC_DP_DGAIN_X_MASK;
+    accel_off_y = accel_go -> accel_offset_y & BMI3_ACC_DP_DOFFSET_Y_MASK;
+    accel_gain_y = accel_go -> accel_gain_y & BMI3_ACC_DP_DGAIN_Y_MASK;
+    accel_off_z = accel_go -> accel_offset_z & BMI3_ACC_DP_DOFFSET_Z_MASK;
+    accel_gain_z = accel_go -> accel_gain_z & BMI3_ACC_DP_DGAIN_Z_MASK;
+
+    data[0] = (uint8_t)(accel_off_x & 0x00FF);
+    data[1] = (uint8_t)(accel_off_x & 0xFF00) >> 8;
+    data[2] = (uint8_t)(accel_gain_x);
+    data[4] = (uint8_t)(accel_off_y & 0x00FF);
+    data[5] = (uint8_t)(accel_off_y & 0xFF00) >> 8;
+    data[6] = (uint8_t)(accel_gain_y);
+    data[8] = (uint8_t)(accel_off_z & 0x00FF);
+    data[9] = (uint8_t)(accel_off_z & 0xFF00) >> 8;
+    data[10] = (uint8_t)(accel_gain_z);
+
+    result = set_multi_register(ACC_DP_OFF_X, data, 12);
+
+    return result;
 }
