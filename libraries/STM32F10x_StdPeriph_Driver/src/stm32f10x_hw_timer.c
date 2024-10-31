bcdf7cac915b3af3a7b75cce0fae49c4fddcb985
Hugh Ding <hzfmd2006@gmail.com>
2024-10-31 01:32:09 +0000

    got clock freq

--- libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_hw_timer.c
+++ libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_hw_timer.c
@@ -5,6 +5,9 @@
 
 StatusCode hardware_timer_init_and_start(uint32_t duration_us, HardwareTimerCallback func, TIM_TypeDef *TIMX){
     //RCC
+    RCC_ClocksTypeDef *clock;
+    RCC_GetClocksFreq(clock);
+    uint16_t ClocksFreq = clock->SYSCLK_Frequency;
 
     //Base init of TimeBaseInitTypeDef
 }
