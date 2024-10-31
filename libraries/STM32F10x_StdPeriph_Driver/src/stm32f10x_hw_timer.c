27775f4316adfd0c6c94f834409940708ba194fb
M. Wasif Kamran <muwasifk@hotmail.com>
2024-10-31 01:24:28 +0000

    fmt change

--- libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_hw_timer.c
+++ libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_hw_timer.c
@@ -6,10 +6,13 @@
 StatusCode hardware_timer_init_and_start(uint32_t duration_us, HardwareTimerCallback func, TIM_TypeDef *TIMX){
     //RCC
 
-    struct TIM_TimeBaseInitTypeDef config = {
-        prescaler, 
-        TIM_CounterMode_Up, 
-        duration_us - 1, 
-        TIM_CLOCKDIVISION_DIV1, 
-        0};
+        struct TIM_TimeBaseInitTypeDef config = {
+                prescaler, 
+                TIM_CounterMode_Up, 
+                duration_us - 1, 
+                TIM_CLOCKDIVISION_DIV1, 
+                0};
+
+        
+
 }
