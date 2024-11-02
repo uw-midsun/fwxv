05c7e2073cd8c0c18bb1acf02dddb0127f68a33f
Aryan Kashem <130705280+Akashem06@users.noreply.github.com>
2024-11-02 12:53:45 -0400

    Updated UART (#339)
    * Updated UART
    
    * formatting
    
    * modified cpplint

--- libraries/ms-common/inc/arm/uart_mcu.h
+++ libraries/ms-common/inc/arm/uart_mcu.h
@@ -1,14 +1,10 @@
 #pragma once
-// NOTE(Mitch): Since we are only using uart for logs on port 1, make others
-// unavailable to save space
-#define MULTI_PORT_UART 0
-#define NUM_UART_PORTS 1
+
+#define NUM_UART_PORTS 3
 
 typedef enum {
   UART_PORT_1 = 0,
-#ifdef MULTI_PORT_UART
   UART_PORT_2,
   UART_PORT_3,
   UART_PORT_4,
-#endif
 } UartPort;
