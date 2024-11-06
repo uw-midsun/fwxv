8442fa0f20eef4e25c99e522b9b11bf9c587085a
Megan Lee <leemegan722@gmail.com>
2024-11-06 01:32:06 +0000

    commit 102

--- projects/task_1/src/tasks/tasks.c
+++ projects/task_1/src/tasks/tasks.c
@@ -19,9 +19,9 @@ static void prv_delay(const TickType_t delay_ms) {
 TASK(task1, TASK_STACK_512) {
   int counter1 = 0;
   while (true) {
-    LOG_DEBUG("Task 1 - %d \n", counter1);
+    LOG_DEBUG("Task 1 - %d\n", counter1);
     counter1++;
-    prv_delay(1000);
+    delay_ms(1000);
   }
 }
 
@@ -30,7 +30,7 @@ TASK(task2, TASK_STACK_512) {
   while (true) {
     LOG_DEBUG("Task 2 - %d\n", counter2);
     counter2++;
-    prv_delay(1000);
+    delay_ms(1000);
   }
 }
 
@@ -39,7 +39,7 @@ int main(void) {
     tasks_init();
 
     tasks_init_task(task1, TASK_PRIORITY(1), NULL);
-    tasks_init_task(task2, TASK_PRIORITY(1), NULL);
+    tasks_init_task(task2, TASK_PRIORITY(2), NULL);
 
     LOG_DEBUG("Program start...\n");
 
