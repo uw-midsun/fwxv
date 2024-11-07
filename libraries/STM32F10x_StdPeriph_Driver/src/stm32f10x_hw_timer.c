4078987a9576e168b445f36571e1df6fed9bffa3
Roshan Hegde <roshanhegde@outlook.com>
2024-11-06 19:20:02 -0500

    refactored and changed clock line to ABP1/2 instead of system

--- libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_hw_timer.c
+++ libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_hw_timer.c
@@ -3,26 +3,22 @@
 
 
 
-StatusCode hardware_timer_init(uint32_t duration_us, TIM_TypeDef *TIMX){
-    //RCC
-    RCC_ClocksTypeDef *clock;
-    RCC_GetClocksFreq(clock);
-    
+StatusCode hardware_timer_init_and_start(uint32_t duration_us, TIM_TypeDef *TIMX){
+    //Configure ABP1 / ABP2 functionality
+
+    TIM_Cmd(TIMX, ENABLE);
 
 
     //Base init of TimeBaseInitTypeDef
     TIM_TimeBaseInitTypeDef config = {
-            clock->SYSCLK_Frequency/1000000, // replace this using clock speed / 1000000
+            72 - 1, // replace this using clock speed / 1000000
             TIM_CounterMode_Up, 
             duration_us - 1, 
             TIM_CKD_DIV1, 
             0
-            };
+        };
+    
 
     TIM_TimeBaseInit(TIMX, &config); // configs timer
 
-}
-
-StatusCode hardware_timer_start(HardwareTimerCallback *func, TIM_TypeDef *TIMX){
-    
 }
