6de3bda08aa9d452c2fa3ade0fb671adc024fabd
Hugh Ding <hzfmd2006@gmail.com>
2024-11-07 01:41:56 +0000

    fixed header file

--- libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_hw_timer.h
+++ libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_hw_timer.h
@@ -1,23 +1,11 @@
+#pragma once
 #include "stm32f10x_tim.h"
 #include "stm32f10x_rcc.h"
 #include "status.h"
 #include <stdint.h>
 
 
-typedef void (* HardwareTimerCallback) (TIM_TypeDef *TIMx);
+typedef void (* HardwareTimerCallback) (TIM_TypeDef *TIMX);
 
 
-
-
-
-
-
-
-
-
-
-
-
-StatusCode hardware_timer_init(uint32_t duration_us, TIM_TypeDef *TIMX);
-
-StatusCode hardware_timer_start(HardwareTimerCallback *func, TIM_TypeDef *TIMX);
+StatusCode hardware_timer_init_and_start(uint32_t duration_us);
