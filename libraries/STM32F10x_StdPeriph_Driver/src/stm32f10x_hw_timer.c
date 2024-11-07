bb8a9dba10c2f41aac39cb67ba808c0744bf1ff8
Roshan Hegde <roshanhegde@outlook.com>
2024-11-06 20:54:54 -0500

    Changed so that only 3 timers are used at a time and TIMX isnt passed

--- libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_hw_timer.c
+++ libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_hw_timer.c
@@ -3,8 +3,36 @@
 
 
 
-StatusCode hardware_timer_init_and_start(uint32_t duration_us, TIM_TypeDef *TIMX){
-    //Configure ABP1 / ABP2 functionality
+StatusCode hardware_timer_init_and_start(uint32_t duration_us){
+    //Timer array for which timers are in use.
+    static bool timers[3] = {false, false, false};
+
+    TIM_TypeDef *TIMX;
+    uint_32t ABP_periph;
+
+
+    if (!timers[0]){
+        TIMX = TIM2;
+        timers[0] = true;
+        ABP_periph = RCC_APB1Periph_TIM2;
+        
+    } else if (!timers[1]){
+        TIMX = TIM3;
+        timers[1] = true;
+        ABP_periph = RCC_APB1Periph_TIM2;
+    } else if (!timers[2]){
+        TIMX = TIM4; 
+        timers[2] = true;
+        ABP_periph = RCC_APB1Periph_TIM2;
+    } else {
+        //All timers are in use
+        return STATUS_CODE_RESOURCE_EXHAUSTED;
+    }
+
+    
+    
+    
+
 
     TIM_Cmd(TIMX, ENABLE);
 
@@ -17,8 +45,6 @@ StatusCode hardware_timer_init_and_start(uint32_t duration_us, TIM_TypeDef *TIMX
             TIM_CKD_DIV1, 
             0
         };
-    
 
     TIM_TimeBaseInit(TIMX, &config); // configs timer
-
 }
