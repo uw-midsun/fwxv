33f061b6f0687ebd57c4c86a5d7ecf69a2cec928
Jarvis <jarvis@uwmidsun.com>
2024-11-09 20:05:07 +0000

    Fixed CAN message decode issues

--- projects/telemetry/inc/datagram.h
+++ projects/telemetry/inc/datagram.h
@@ -2,21 +2,26 @@
 
 #include "can.h"
 
+#pragma pack (push, 1)
 typedef struct {
   // Initialize start_frame as 0xAA
   uint8_t start_frame;
-  uint16_t id;
+  uint32_t id;
   size_t dlc;
 
-  union {
-    uint64_t data_u64;
-    uint32_t data_u32[2];
-    uint16_t data_u16[4];
-    uint8_t data_u8[8];
-  } data;
+  // Only use dlc number of bytes in data, plus 1 byte for end_of_frame
+  uint8_t data[9];
+
+
+  // union {
+  //   uint64_t data_u64;
+  //   uint32_t data_u32[2];
+  //   uint16_t data_u16[4];
+  //   uint8_t data_u8[8];
+  // } data;
 
   // Initialize end_of_frame as 0xBB
-  uint8_t end_of_frame;
 } Datagram;
+#pragma pack(pop)
 
-Datagram decode_can_message(Datagram datagram, CanMessage msg);
+size_t decode_can_message(Datagram *datagram, CanMessage *msg);
--- projects/telemetry/src/main.c
+++ projects/telemetry/src/main.c
@@ -11,7 +11,7 @@
 static CanStorage s_can_storage = { 0 };
 
 static const CanSettings can_settings = {
-  .device_id = SYSTEM_CAN_DEVICE_TELEMETRY,
+  .device_id = SYSTEM_CAN_DEVICE_CAN_COMMUNICATION,
   .bitrate = CAN_HW_BITRATE_500KBPS,
   .tx = { GPIO_PORT_A, 12 },
   .rx = { GPIO_PORT_A, 11 },
@@ -24,27 +24,28 @@ static UartSettings uart_settings = { .tx = { .port = GPIO_PORT_A, .pin = 2 },
 
 TASK(CAN_TO_UART, TASK_STACK_256) {
   while (true) {
-    uint8_t test = 0xFF;
-    size_t test_len = sizeof(test);
-    uart_tx(UART_PORT_2, &test, &test_len);
+    // uint8_t test = 0xFF;
+    // size_t test_len = sizeof(test);
+    // uart_tx(UART_PORT_2, &test, &test_len);
     CanMessage msg = { 0 };
-    Datagram datagram = { .start_frame = 0xAA, .end_of_frame = 0xBB };
+    Datagram datagram = { .start_frame = 0xAA };
 
     // Polling can_storage queue
-    StatusCode ret = can_queue_pop(&s_can_storage.rx_queue, &msg);
+    while(can_queue_pop(&s_can_storage.rx_queue, &msg) != STATUS_CODE_OK) {
+      // LOG_DEBUG("can_queue_pop failed\n");  
+    }
+    size_t data_len = decode_can_message(&datagram, &msg);
 
-    if (ret == STATUS_CODE_OK) {
-      decode_can_message(datagram, msg);
-
-      // Typecast datagram object to pass into uart_tx
-      uint8_t *uint8_datagram = (uint8_t *) &datagram; 
-      size_t len = sizeof(uint8_datagram);
-      StatusCode uart_ret = uart_tx(UART_PORT_2, uint8_datagram, &len);
-      if (uart_ret != STATUS_CODE_OK) {
-        LOG_DEBUG("UART_TX FAILED\n");
-      }
-    } else {
-      // LOG_DEBUG("can_queue_pop failed\n");
+    // Typecast datagram object to pass into uart_tx
+    uint8_t *uint8_datagram = (uint8_t *) &datagram; 
+    size_t tx_len = sizeof(uint8_t) + sizeof(uint32_t) + sizeof(size_t) + data_len;
+    StatusCode uart_ret = uart_tx(UART_PORT_2, uint8_datagram, &tx_len);
+    for (unsigned int i = 0; i < sizeof(Datagram); ++i) {
+      LOG_DEBUG("TX: %x\n", uint8_datagram[i]);
+    }
+    
+    if (uart_ret != STATUS_CODE_OK) {
+      LOG_DEBUG("UART_TX FAILED\n");
     }
   }
 }
@@ -57,13 +58,18 @@ void run_fast_cycle() {
   wait_tasks(1);
 }
 
-Datagram decode_can_message(Datagram datagram, CanMessage msg) {
+// Return size of data
+size_t decode_can_message(Datagram *datagram, CanMessage *msg) {
   // Populate datagram object with relevant data
-  datagram.id = msg.id.raw;
-  datagram.dlc = msg.dlc;
-  datagram.data.data_u64 = msg.data;
+  datagram->id = msg->id.raw;
+  // LOG_DEBUG("%x\n", (uint32_t) msg.id.raw);
+  datagram->dlc = msg->dlc;
+  for (size_t i = 0; i < msg->dlc; ++i) {
+    datagram->data[i] = msg->data_u8[i];
+  }
+  datagram->data[msg->dlc] = 0xBB;  // End of frame
 
-  return datagram;
+  return msg->dlc;
 }
 
 int main() {
