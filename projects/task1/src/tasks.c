76c1362c591715355d7ada2997685a5b3a9ad28e
vagrant <vagrant@midsunbox>
2024-11-20 16:08:46 +0000

    Tamir Polyakov queues commit

---
+++ projects/task1/src/tasks.c
@@ -0,0 +1,41 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "tasks.h"
+
+#include "gpio.h"
+#include "log.h"
+#include "misc.h"
+#include "delay.h"
+#include "queues.h"
+
+TASK(task1, TASK_STACK_512) {
+  int counter1 = 0;
+  while (true) {
+  LOG_DEBUG("Task 1: %d\n", counter1);
+  counter1++;
+  vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+
+TASK(task2, TASK_STACK_512) {
+  int counter2 = 0;
+  while (true) {
+  LOG_DEBUG("Task 2: %d\n", counter2);
+  counter2++;
+  vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+
+int main(void) {
+    log_init();
+    // Create tasks here
+    tasks_init();
+    tasks_init_task(task1, TASK_PRIORITY(2), NULL);
+    tasks_init_task(task2, TASK_PRIORITY(1), NULL);
+    LOG_DEBUG("Program start...\n");
+    // Start the scheduler
+    tasks_start();
+    return 0;
+}
