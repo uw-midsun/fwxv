9e6523060a87740cdf339a0267849492403fc706
Darun <d3kanesa@uwaterloo.ca>
2024-12-20 21:52:50 +0000

    Finished FW 103

--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -13,15 +13,49 @@
 
 #include "log.h"
 #include "tasks.h"
+#include "gpio.h"
+#include <ads1115.h>
+#include "delay.h"
+
+// GpioAddress struct
+GpioAddress led_addr = {
+  .port = GPIO_PORT_B,
+  .pin = 3,
+};
+
+TASK(toggle, TASK_STACK_512) {
+
+  GpioAddress ready_pin = {
+    .port = GPIO_PORT_B,
+    .pin = GPIO_Pin_0,
+  };
+
+  ADS1115_Config config = {
+    .handler_task = toggle,
+    .i2c_addr = ADS1115_ADDR_GND,
+    .i2c_port = ADS1115_I2C_PORT,
+    .ready_pin = &ready_pin,
+  };
+
+  float res;
+
+  while (true) {
+    gpio_toggle_state(&led_addr);
+    ads1115_read_converted(&config, ADS1115_CHANNEL_0, &res);
+    LOG_DEBUG("Voltage: %f\n", res);
+    delay_ms(1000);
+  }
+
+}
 
 int main() {
   tasks_init();
   log_init();
   gpio_init();
-  LOG_DEBUG("Welcome to FW 103!\n");
+  gpio_init_pin(&led_addr, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
 
+  tasks_init_task(toggle, TASK_PRIORITY(1), NULL);
   tasks_start();
 
-  LOG_DEBUG("exiting main?\n");
   return 0;
 }
