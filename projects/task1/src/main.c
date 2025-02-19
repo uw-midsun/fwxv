934f41ea19982d38e218c1dd220587e08b6c4002
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-02-19 03:06:03 +0000

    Complete task 1

--- projects/task1/src/main.c
+++ projects/task1/src/main.c
@@ -10,11 +10,11 @@
 #include "delay.h"
 
 // Non blocking delay. Simply consumes cpu cycles until a given time has passed
-static void prv_delay(const TickType_t delay_ms) {
-  TickType_t curr_tick = xTaskGetTickCount();
-  while(xTaskGetTickCount() - curr_tick < pdMS_TO_TICKS(delay_ms))
-  {}
-}
+// static void prv_delay(const TickType_t delay_ms) {
+//  TickType_t curr_tick = xTaskGetTickCount();
+//   while(xTaskGetTickCount() - curr_tick < pdMS_TO_TICKS(delay_ms))
+//   {}
+// }
 
 TASK(task1, TASK_STACK_512) {
   int counter1 = 0;
@@ -22,7 +22,7 @@ TASK(task1, TASK_STACK_512) {
   while (true) {
     // Your code here
     LOG_DEBUG("Task 1: %d\n", counter1++);
-    prv_delay(1000);
+    delay_ms(1000);
   }
 }
 
@@ -32,7 +32,7 @@ TASK(task2, TASK_STACK_512) {
   while (true) {
     // Your code here
     LOG_DEBUG("Task 2: %d\n", counter2++);
-    prv_delay(1000);
+    delay_ms(1000);
   }
 }
 
@@ -41,7 +41,7 @@ int main(void) {
     tasks_init();
 
     // Create tasks here
-    tasks_init_task(task1, TASK_PRIORITY(1), NULL);
+    tasks_init_task(task1, TASK_PRIORITY(2), NULL);
     tasks_init_task(task2, TASK_PRIORITY(1), NULL);
 
     LOG_DEBUG("Program start...\n");
