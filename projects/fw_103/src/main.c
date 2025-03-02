aa99453479490b8ffec661ad33fdabbea8ce380e
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-03-02 03:27:04 +0000

    completed fw 102 part 1

--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -13,12 +13,47 @@
 
 #include "log.h"
 #include "tasks.h"
+#include "delay.h"
+#include "gpio.h"
+
+
+
+// Define ADC task
+TASK(run_leds, TASK_STACK_512) {
+  // Define LED pin
+  GpioAddress led_addr = {
+    // PB3_LED_RED
+    .port = GPIO_PORT_B,
+    .pin = 3,
+  };
+  // Track current LED pin state
+  GpioState current_led_state;
+
+  // Initialize LED pin
+  gpio_init_pin(&led_addr, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
+
+  while(true) {
+    gpio_toggle_state(&led_addr);
+    delay_ms(1000);
+
+    gpio_get_state(&led_addr, &current_led_state);
+
+    if (current_led_state == GPIO_STATE_LOW) {
+      LOG_DEBUG("LED off\n");
+    } else if (current_led_state == GPIO_STATE_HIGH) {
+      LOG_DEBUG("LED on\n");
+    }
+  }
+}
 
 int main() {
   tasks_init();
   log_init();
   gpio_init();
   LOG_DEBUG("Welcome to FW 103!\n");
+
+  // Create ADC task
+  tasks_init_task(run_leds, TASK_PRIORITY(1), NULL);
 
   tasks_start();
 
