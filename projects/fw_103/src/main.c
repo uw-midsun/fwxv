7429f3364f816a239664162f0aa2455408529dd6
Daiyan Zubaier <daiyan102345@gmail.com>
2025-03-08 05:15:02 +0000

    Commiting adc and gpio implementation

--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -13,11 +13,56 @@
 
 #include "log.h"
 #include "tasks.h"
+#include "gpio.h"
+#include "delay.h"
+#include "FreeRTOS.h"
+#include "ads1115.h"
 
+
+
+
+TASK(led_task, TASK_STACK_512){
+
+  //Initializing led GPIO address
+  const GpioAddress led_addr = {
+    .port = GPIO_PORT_A,
+    .pin = 15
+  };
+
+  //Initializing ADS1115 config and ready pin
+  GpioAddress ready_pin = {
+    .port = GPIO_PORT_B,
+    .pin = GPIO_Pin_0,
+  };
+  ADS1115_Config config = {
+    .handler_task = led_task,
+    .i2c_addr = ADS1115_ADDR_GND,
+    .i2c_port = ADS1115_I2C_PORT,
+    .ready_pin = &ready_pin,
+  };
+
+  // Initializing gpio pins
+  gpio_init();
+  gpio_init_pin(&led_addr);
+  
+  //Initializing I2C
+  ads1115_init(&config, ADS1115_ADDR_GND, &ready_pin);
+
+  while(1){
+    gpio_toggle_state(&led_addr);
+    delay_s(1);
+    ads1115_read_converted(&config, ADS1115_CHANNEL_0, 0);
+  }
+}
 int main() {
   tasks_init();
   log_init();
-  gpio_init();
+  
+  
+  
+  //Initializing task
+  tasks_init_task(led_task, TASK_PRIORITY(1), NULL);
+  
   LOG_DEBUG("Welcome to FW 103!\n");
 
   tasks_start();
