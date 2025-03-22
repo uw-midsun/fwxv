aaf43088add96a740904f89f80f40c96a8d04075
Jarvis <jarvis@uwmidsun.com>
2025-03-22 18:50:26 +0000

    Task 103 completed

--- projects/fw_103/src/ads1115.c
+++ projects/fw_103/src/ads1115.c
@@ -1,5 +1,6 @@
 #include "ads1115.h"
 
+#include "i2c.h"
 #include "gpio_it.h"
 #include "i2c.h"
 #include "status.h"
@@ -15,7 +16,7 @@ StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAd
 
   // Write Config register
   /* TODO: fill out this value */
-  cmd = 0x0000;
+  cmd = 0x1483;   // 1000 0100 1000 0011
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
 
   /* TODO (optional) */
@@ -49,10 +50,14 @@ StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channe
 
 StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, uint16_t *reading) {
   /* TODO: complete function */
+  i2c_read_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONVERSION, (uint8_t *)(reading), 2);
   return STATUS_CODE_OK;
 }
 
 StatusCode ads1115_read_converted(ADS1115_Config *config, ADS1115_Channel channel, float *reading) {
   /* TODO: complete function */
+  uint16_t raw_reading;
+  ads1115_read_raw(config, channel, &raw_reading);
+  *reading = 4.095f * (raw_reading / 65535.0f);
   return STATUS_CODE_OK;
 }
--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -13,15 +13,58 @@
 
 #include "log.h"
 #include "tasks.h"
+#include "gpio.h"
+
+#include "ads1115.h"
+
+#include "delay.h"
+
+TASK(ads1115, TASK_STACK_512) {
+  GpioAddress ready_pin = {
+    .port = GPIO_PORT_B,
+    .pin = GPIO_Pin_0,
+  };
+  ADS1115_Config config = {
+    .handler_task = ads1115,
+    .i2c_addr = ADS1115_ADDR_GND,
+    .i2c_port = ADS1115_I2C_PORT,
+    .ready_pin = &ready_pin,
+  };
+
+  while (true) {
+    float voltage;
+    ads1115_read_converted(&config, ADS1115_CHANNEL_0, &voltage);
+    LOG_DEBUG("%f\n", voltage);
+    delay_ms(500);
+  }
+}
+
+TASK(led_blink, TASK_STACK_512) {
+  GpioAddress led_addr = {
+    .port = GPIO_PORT_B,
+    .pin = 3,
+  };
+
+  gpio_init_pin(&led_addr, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_HIGH);
+
+  while (true) {
+    gpio_toggle_state(&led_addr);
+    LOG_DEBUG("blink\n");
+    delay_ms(1000);
+  }
+}
 
 int main() {
+  gpio_init();
   tasks_init();
   log_init();
-  gpio_init();
-  LOG_DEBUG("Welcome to FW 103!\n");
+
+  LOG_DEBUG("Welcome to TEST!");
+
+  tasks_init_task(ads1115, TASK_PRIORITY(2), NULL);
 
   tasks_start();
 
-  LOG_DEBUG("exiting main?\n");
+  LOG_DEBUG("exiting main?");
   return 0;
 }
