d54ea63683be3a2f29125e80e26d58bd51a8c589
Ethan-Gan <ethanganca@gmail.com>
2025-07-06 05:43:15 +0000

    Completed FW103 ethangan

--- projects/fw_103/src/ads1115.c
+++ projects/fw_103/src/ads1115.c
@@ -4,6 +4,7 @@
 #include "i2c.h"
 #include "status.h"
 
+// Completed by Ethan Gan 
 StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAddress *ready_pin) {
   if (config == NULL) {
     return status_code(STATUS_CODE_INVALID_ARGS);
@@ -15,7 +16,8 @@ StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAd
 
   // Write Config register
   /* TODO: fill out this value */
-  cmd = 0x0000;
+  cmd = 0x0483;
+
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
 
   /* TODO (optional) */
@@ -42,17 +44,38 @@ StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channe
   uint16_t cmd;
 
   // Write Config register
-  cmd = 0x0000;
+  cmd = 0x0483;
   i2c_write_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
   return STATUS_CODE_OK;
 }
 
 StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, uint16_t *reading) {
   /* TODO: complete function */
+  // i2c already initialized somewhere else
+  uint16_t cmd = 0x0483;
+  // read 2 bytes
+    StatusCode status =  i2c_read_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONVERSION, (uint8_t *)reading, 2);
+    if(status!=STATUS_CODE_OK) {
+    return status;
+  }
+    // this info is stored in reading
   return STATUS_CODE_OK;
 }
 
 StatusCode ads1115_read_converted(ADS1115_Config *config, ADS1115_Channel channel, float *reading) {
   /* TODO: complete function */
+  uint16_t raw_reading;
+  uint16_t cmd = 0x0483;
+  // the reason why it is mapped to int8_t other than the header file calling for it
+  // is because the rx_data really only cares about pointing to the start of the memory address to deal with the data byte by byte which is why we enter 2 bytes as rx_len.
+  StatusCode status = i2c_read_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONVERSION, (uint8_t *)&raw_reading, 2);
+  
+  if(status!=STATUS_CODE_OK) {
+    return status;
+  }
+
+  //voltage = (raw/max)*range - (max magnitude)
+  // remember to avoid integer division
+  *reading = ((raw_reading/65535.0f)*4.096f)-2.048; 
   return STATUS_CODE_OK;
 }
--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -8,17 +8,70 @@
     - ADC task to periodically measure the voltage of channel 0
     - Overvoltage interrupt (configure the interrupt to be on channel 0 with thresholds of 0V - 1V)
 */
+// Completed by Ethan Gan 
+#include "gpio.h"
+#include "delay.h"
+
+#include "ads1115.h"
 
 #include <stdio.h>
-
 #include "log.h"
 #include "tasks.h"
+
+
+GpioAddress led_addr = {
+  // PB5 = Port B, Pin 5
+  // Green LED from the FW103 picture
+  .port = GPIO_PORT_B,
+  .pin = 5,
+};
+
+TASK(task1, TASK_STACK_512) {
+    GpioAddress ready_pin = {
+    .port = GPIO_PORT_B,
+    .pin = GPIO_Pin_0,
+  };
+  ADS1115_Config config = {
+    // ig handler task is not "FW103 Final Task: Blink Green LED 1 blink per second",
+    .handler_task = (Task *)xTaskGetCurrentTaskHandle(),
+    .i2c_addr = ADS1115_ADDR_GND,
+    .i2c_port = ADS1115_I2C_PORT,
+    .ready_pin = &ready_pin,
+  };
+  float voltage;
+  StatusCode status = ads1115_init(&config, config.i2c_addr, config.ready_pin); // start the ads program
+  // review struct syntax later
+  if (status!=STATUS_CODE_OK) {
+    LOG_DEBUG("Failed to start ADS1115\n");
+      vTaskDelete(NULL);
+  }
+
+  LOG_DEBUG("blink started\n");
+
+  while(true) {
+    ads1115_read_converted(&config, ADS1115_CHANNEL_0, &voltage); 
+    // I still don't get the channel parameter, it looks like it is unused
+  
+    LOG_DEBUG("Voltage reading: %f V\n", voltage);
+    gpio_toggle_state(&led_addr);
+    LOG_DEBUG("blink\n");
+
+    delay_ms(1000);
+  }
+  vTaskDelete(NULL);
+  
+}
 
 int main() {
   tasks_init();
   log_init();
-  gpio_init();
+  gpio_init(); // already done
+  
+  tasks_init_task(task1, TASK_PRIORITY(2), NULL);
+
   LOG_DEBUG("Welcome to FW 103!\n");
+
+
 
   tasks_start();
 
